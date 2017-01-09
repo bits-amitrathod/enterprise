27907c9e850d65297d2f63612eceec88e6c43cc5
Luis Torres <luis_t@vauxoo.com>
2017-01-09 20:14:23 +0000

    [REF] l10n_mx_edi: Refactoring test
      - Add fiscal position for testing
      - Using MXN currency for test and remove serie,TipoCambio,descuento
      - Added test with XML expected with node "Emitido en"
      - Removed prefix in company VAT, is not necessary.
      - Removed assign VAT in partner, like country is different to Mexico, is
        assigned the generic VAT to foreign suppliers
      - Using just a test method unified
      - Get attachment lxml xml instead of just the attachment
      - Force invoice.number to test expected xml.folio
      - Add method to merge dynamic xml items
      - Testing using invoice.l10n_mx_edi_xml_signed method
      - Moving xml standard method to common.py
      - Add testing for UUID Cancelled
      - Add testing for PAC cancel process
      - Deleting pac login info since that is extracted by default from pac_infos methods
      - Add test re-sign process to recovery a previous signed xml (This case is TODO yet)
      - Add testing for sf (is equal to fk)
      - Add assert to check encoding xml declaration
      - Using odoo.tools.misc.file_open in unittest

--- l10n_mx_edi/tests/common.py
+++ l10n_mx_edi/tests/common.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+import time
+
+from lxml import objectify
+
 from odoo.tests.common import TransactionCase
 
 
@@ -24,19 +28,34 @@ class InvoiceTransactionCase(TransactionCase):
             'amount_type': 'percent',
             'amount': -10,
         })
-        self.tax_zero = self.tax_model.create({
-            'name': 'IVA Exento',
-            'description': 'iva',
-            'amount_type': 'percent',
-            'amount': 0,
+        self.product.taxes_id = [self.tax_positive.id, self.tax_negative.id]
+        self.fiscal_position_model = self.env['account.fiscal.position']
+        self.fiscal_position = self.fiscal_position_model.create({
+            'name': 'Personas morales del régimen general'})
+        self.payment_method_cash = self.env.ref(
+            'l10n_mx_edi.payment_method_efectivo')
+        self.account_payment = self.env['res.partner.bank'].create({
+            'acc_number': '123456789',
         })
-        self.product.taxes_id = [self.tax_positive.id, self.tax_negative.id,
-                                 self.tax_zero.id]
+        self.rate_model = self.env['res.currency.rate']
+        self.mxn = self.env.ref('base.MXN')
+        self.usd = self.env.ref('base.USD')
 
-    def create_invoice(self, inv_type='out_invoice'):
+    def set_currency_rates(self, mxn_rate, usd_rate):
+        self.mxn.rate_ids = self.rate_model.create({
+            'rate': mxn_rate, 'name': time.strftime('%Y-%m-%d')})
+        self.usd.rate_ids = self.rate_model.create({
+            'rate': usd_rate, 'name': time.strftime('%Y-%m-%d')})
+
+    def create_invoice(self, inv_type='out_invoice', currency_id=None):
+        if currency_id is None:
+            currency_id = self.usd.id
         invoice = self.invoice_model.create({
             'partner_id': self.partner_agrolait.id,
             'type': inv_type,
+            'currency_id': currency_id,
+            'l10n_mx_edi_payment_method_id': self.payment_method_cash.id,
+            'l10n_mx_edi_partner_bank_id': self.account_payment.id,
         })
         self.create_invoice_line(invoice)
         invoice.compute_taxes()
@@ -51,11 +70,35 @@ class InvoiceTransactionCase(TransactionCase):
         invoice_line._onchange_product_id()
         invoice_line_dict = invoice_line._convert_to_write({
             name: invoice_line[name] for name in invoice_line._cache})
+        invoice_line_dict['price_unit'] = 450
         self.invoice_line_model.create(invoice_line_dict)
 
-    def attachments(self, invoice):
-        domain = [
-            ('res_id', '=', invoice.id),
-            ('res_model', '=', invoice._name),
-            ('name', '=', invoice.l10n_mx_edi_cfdi_name)]
-        return self.env['ir.attachment'].search(domain)
+    def get_invoice_xml(self, invoice):
+        xml_str = invoice.l10n_mx_edi_xml_signed()
+        xml = objectify.fromstring(xml_str)
+        return xml
+
+    def xml_merge_dynamic_items(self, xml, xml_expected):
+        xml_expected.attrib['fecha'] = xml.attrib['fecha']
+        xml_expected.attrib['sello'] = xml.attrib['sello']
+        xml_expected.Complemento = xml.Complemento
+
+    def xml2dict(self, xml):
+        """Receive 1 lxml etree object and return a dict string.
+        This method allow us have a precise diff output"""
+        def recursive_dict(element):
+            return (element.tag,
+                    dict(map(recursive_dict, element.getchildren()),
+                         **element.attrib))
+        return dict([recursive_dict(xml)])
+
+    def assertEqualXML(self, xml_real, xml_expected):
+        """Receive 2 objectify objects and show a diff assert if exists."""
+        xml_expected = self.xml2dict(xml_expected)
+        xml_real = self.xml2dict(xml_real)
+        # "self.maxDiff = None" is used to get a full diff from assertEqual method
+        # This allow us get a precise and large log message of where is failing
+        # expected xml vs real xml More info:
+        # https://docs.python.org/2/library/unittest.html#unittest.TestCase.maxDiff
+        self.maxDiff = None
+        self.assertEqual(xml_real, xml_expected)
