bits-amitrathod/enterprise

commit fd14281223b79cbd06fb2ba42d4cffdc446e4930

[IMP] web_studio: update field_widgets and kanban view
These changes are needed to be able to edit the kanban
view with Studio.

These changes are inspired from the branch with the new views.
Do not forward port them with the new views.

web_studio/static/src/js/renderers/field_widgets/field_registry.js
@@ -1,11 +1,18 @@
 odoo.define('web.field_registry', function (require) {
 "use strict";
 
-var basic_fields = require('web.basic_fields');
 var Registry = require('web.Registry');
-var relational_fields = require('web.relational_fields');
 
-var registry = new Registry();
+return new Registry();
+});
+
+odoo.define('web._field_registry', function(require) {
+"use strict";
+
+var basic_fields = require('web.basic_fields');
+var relational_fields = require('web.relational_fields');
+var registry = require('web.field_registry');
+
 
 // Basic fields
 registry
@@ -22,12 +29,15 @@ registry
     .add('char', basic_fields.FieldChar)
     .add('handle', basic_fields.HandleWidget)
     .add('email', basic_fields.EmailWidget)
+    .add('phone', basic_fields.FieldPhone)
     .add('url', basic_fields.UrlWidget)
     .add('image', basic_fields.FieldBinaryImage)
     .add('binary', basic_fields.FieldBinaryFile)
     .add('monetary', basic_fields.FieldMonetary)
     .add('priority', basic_fields.PriorityWidget)
-    .add('kanban_state_selection', basic_fields.KanbanStateWidget)
+    .add('attachment_image', basic_fields.AttachmentImage)
+    .add('label_selection', basic_fields.LabelSelection)
+    .add('state_selection', basic_fields.StateSelectionWidget)
     .add('statusbar', basic_fields.StatusBar)
     .add('boolean_button', basic_fields.FieldBooleanButton)
     .add('id', basic_fields.FieldID)
@@ -42,13 +52,12 @@ registry
     .add('many2one', relational_fields.FieldMany2One)
     .add('form.many2one', relational_fields.FormFieldMany2One)
     .add('list.many2one', relational_fields.ListFieldMany2One)
+    .add('kanban.many2one', relational_fields.KanbanFieldMany2One)
     .add('many2many', relational_fields.FieldMany2Many)
     .add('many2many_tags', relational_fields.FieldMany2ManyTags)
     .add('many2many_checkboxes', relational_fields.FieldMany2ManyCheckBoxes)
     .add('form.many2many_tags', relational_fields.FormFieldMany2ManyTags)
     .add('one2many', relational_fields.FieldOne2Many)
     .add('one2many_list', relational_fields.FieldOne2Many);
-
-return registry;
 
 });

web_studio/static/src/js/renderers/field_widgets/abstract_field.js
@@ -71,7 +71,7 @@ return Widget.extend({
 
         // Widget can often be configured in the 'options' attribute in the
         // xml 'field' tag.  These options are saved (and evaled) in node_options
-        this.node_options = pyeval.py_eval(this.field.__attrs.options || '{}');
+        this.node_options = pyeval.py_eval(this.field.__attrs.options || '{}', this.record_data);
 
         // local_id is the id corresponding to the current record in the model.
         // Its intended use is to be able to tag any messages going upstream,

web_studio/static/src/js/renderers/field_widgets/relational_fields.js
@@ -530,6 +530,19 @@ var ListFieldMany2One = FieldMany2One.extend({
     },
 });
 
+var KanbanFieldMany2One = AbstractRelationalField.extend({
+    tagName: 'span',
+    init: function() {
+        this._super.apply(this, arguments);
+        this.m2o_value = field_utils.format_many2one(this.value, this.field, this.record_data, {
+            relational_data: this.record.relational_data,
+        });
+    },
+    render: function() {
+        this.$el.text(this.m2o_value);
+    },
+});
+
 var FormFieldMany2One = FieldMany2One.extend({
     events: _.extend({}, FieldMany2One.prototype.events, {
         'click': function(event) {
@@ -747,6 +760,7 @@ return {
     FieldMany2One: FieldMany2One,
     FormFieldMany2One: FormFieldMany2One,
     ListFieldMany2One: ListFieldMany2One,
+    KanbanFieldMany2One: KanbanFieldMany2One,
     FieldOne2Many: FieldOne2Many,
 };
 

web_studio/static/src/js/renderers/kanban_column.js
@@ -107,22 +107,17 @@ var KanbanColumn = Widget.extend({
                 update: function (event, ui) {
                     var record = ui.item.data('record');
                     var index = self.records.indexOf(record);
-                    var test2 = $.contains(self.$el[0], record.$el[0]);
                     record.$el.removeAttr('style');  // jqueryui sortable add display:block inline
-                    if (index >= 0 && test2) {
-                        // resequencing records
-                        self.trigger_up('kanban_column_resequence', {ids: self.get_ids()});
-                    } else if (index >= 0 && !test2) {
-                        // removing record from this column
-                        self.records.splice(self.records.indexOf(record), 1);
+                    ui.item.addClass('o_updating');
+                    if (index >= 0) {
+                        if ($.contains(self.$el[0], record.$el[0])) {
+                            // resequencing records
+                            self.trigger_up('kanban_column_resequence', {ids: self.get_ids()});
+                        }
                     } else {
                         // adding record to this column
-                        self.records.push(record);
-                        record.setParent(self);
-                        ui.item.addClass('o_updating');
                         self.trigger_up('kanban_column_add_record', {record: record, ids: self.get_ids()});
                     }
-                    self.update_column();
                 }
             });
         }
@@ -170,12 +165,7 @@ var KanbanColumn = Widget.extend({
         this.$header.find('.o-kanban-count').text(this.records.length);
 
         this.$el.toggleClass('o_column_folded', this.folded);
-        var tooltip;
-        if (this.remaining) {
-            tooltip = this.records.length + '/' + this.size + _t(' records');
-        } else {
-            tooltip = this.records.length + _t(' records');
-        }
+        var tooltip = this.size + _t(' records');
         tooltip = '<p>' + tooltip + '</p>' + this.tooltip_info;
         this.$header.tooltip({html: true}).attr('data-original-title', tooltip);
         if (!this.remaining) {

web_studio/static/src/js/renderers/kanban_record.js
@@ -3,16 +3,16 @@ odoo.define('web.KanbanRecord', function (require) {
 
 var core = require('web.core');
 var data = require('web.data');
+var field_registry = require('web.field_registry');
+var field_utils = require('web.field_utils');
 var formats = require('web.formats');
 var framework = require('web.framework');
-var kanban_widgets = require('web.kanban_widgets');
 var session = require('web.session');
 var time = require('web.time');
 var utils = require('web.utils');
 var Widget = require('web.Widget');
 
 var QWeb = core.qweb;
-var fields_registry = kanban_widgets.registry;
 
 var KanbanRecord = Widget.extend({
     template: 'KanbanView.record',
@@ -32,6 +32,7 @@ var KanbanRecord = Widget.extend({
         this.fields = state.fields;
         this.record_data = state.data;
         this.relational_data = state.relational_data;
+        this.options = options;
         this.editable = options.editable;
         this.deletable = options.deletable;
         this.draggable = options.draggable;
@@ -57,11 +58,13 @@ var KanbanRecord = Widget.extend({
     start: function() {
         this.add_widgets();
         this.render_m2m_tags();
+        this.attach_tooltip();
         return this._super.apply(this, arguments);
     },
 
     init_content: function (state) {
         var self = this;
+        this.state = state;
         this.id = state.res_id;
         this.db_id = state.id;
         this.values = {};
@@ -83,6 +86,7 @@ var KanbanRecord = Widget.extend({
                 qweb_context[p] = _.bind(this[p], this);
             }
         }
+        this.qweb_context = qweb_context;
         this.content = this.qweb.render('kanban-box', qweb_context);
     },
 
@@ -90,10 +94,12 @@ var KanbanRecord = Widget.extend({
         var self = this;
         this.$("field").each(function() {
             var $field = $(this);
-            var field = self.record[$field.attr("name")];
-            var type = $field.attr("widget") || field.type;
-            var Widget = fields_registry.get(type);
-            var widget = new Widget(self, field, $field, self.record_data, self.relational_data);
+            var field_name = $field.attr("name");
+            var field = self.record[field_name];
+            var field_widget = $field.attr("widget");
+            var widget_keys = (field_widget ? [field_widget] : []).concat(['kanban.' + field.type, field.type]);
+            var Widget = field_registry.get_any(widget_keys);
+            var widget = new Widget(self, field_name, self.state, self.options);
             widget.replace($field);
             self.sub_widgets.push(widget);
         });
@@ -106,7 +112,7 @@ var KanbanRecord = Widget.extend({
             var rel_ids = self.record[field_name].raw_value;
             var $m2m_tags = self.$('.o_form_field_many2manytags[name=' + field_name + ']');
             _.each(rel_ids, function (id) {
-                var m2m = _.findWhere(values, {id: id});
+                var m2m = _.findWhere(values.data, {res_id: id}).data;
                 if (typeof m2m.color !== 'undefined' && m2m.color !== 10) { // 10th color is invisible
                     $('<span>')
                         .addClass('o_tag o_tag_color_' + m2m.color)
@@ -122,14 +128,23 @@ var KanbanRecord = Widget.extend({
     transform_record: function(record) {
         var self = this;
         var new_record = {};
-        _.each(_.extend(_.object(_.keys(this.fields), []), record), function(value, name) {
+        _.each(Object.keys(this.state.fields), function(name) {
+            var value = record[name];
             var r = _.clone(self.fields[name] || {});
+
             if ((r.type === 'date' || r.type === 'datetime') && value) {
                 r.raw_value = time.auto_str_to_date(value);
             } else {
                 r.raw_value = value;
             }
-            r.value = formats.format_value(value, r);
+
+            if (r.type) {
+                var formatter = field_utils['format_' + r.type];
+                r.value = formatter(value, self.fields[name], record, self.state);
+            } else {
+                r.value = value;
+            }
+
             new_record[name] = r;
         });
         return new_record;
@@ -142,6 +157,21 @@ var KanbanRecord = Widget.extend({
         this.renderElement();
         this.add_widgets();
         this.render_m2m_tags();
+        this.attach_tooltip();
+    },
+
+    attach_tooltip: function () {
+        var self = this;
+        this.$('[tooltip]').each(function () {
+            var $el = $(this);
+            var tooltip = $el.attr('tooltip');
+            if (tooltip) {
+                $el.tooltip({
+                    'html': true,
+                    'title': self.qweb.render(tooltip, self.qweb_context)
+                });
+            }
+        });
     },
 
     kanban_image: function(model, field, id, cache, options) {
@@ -199,14 +229,15 @@ var KanbanRecord = Widget.extend({
                 if (elem === ev.currentTarget) {
                     ischild = false;
                 }
+                var test_event = events && events.click && (events.click.length > 1 || events.click[0].namespace !== "tooltip");
                 if (ischild) {
                     children.push(elem);
-                    if (events && events.click) {
+                    if (test_event) {
                         // do not trigger global click if one child has a click event registered
                         trigger = false;
                     }
                 }
-                if (trigger && events && events.click) {
+                if (trigger && test_event) {
                     _.each(events.click, function(click_event) {
                         if (click_event.selector) {
                             // For each parent of original target, check if a

web_studio/static/src/js/renderers/kanban_renderer.js
@@ -3,16 +3,15 @@ odoo.define('web.KanbanRenderer', function (require) {
 
 var AbstractRenderer = require('web.AbstractRenderer');
 var core = require('web.core');
+var field_registry = require('web.field_registry');
 var KanbanColumn = require('web.KanbanColumn');
 var KanbanRecord = require('web.KanbanRecord');
 var quick_create = require('web.kanban_quick_create');
-var kanban_widgets = require('web.kanban_widgets');
 var QWeb = require('web.QWeb');
 var session = require('web.session');
 var utils = require('web.utils');
 
 var ColumnQuickCreate = quick_create.ColumnQuickCreate;
-var fields_registry = kanban_widgets.registry;
 
 var qweb = core.qweb;
 
@@ -52,7 +51,7 @@ function transform_qweb_template(node, fields) {
             if (ftype === 'many2many') {
                 node.tag = 'div';
                 node.attrs['class'] = (node.attrs['class'] || '') + ' oe_form_field o_form_field_many2manytags o_kanban_tags';
-            } else if (fields_registry.contains(ftype)) {
+            } else if (field_registry.contains(ftype)) {
                 // do nothing, the kanban record will handle it
             } else {
                 node.tag = qweb.prefix;
@@ -77,7 +76,15 @@ function transform_qweb_template(node, fields) {
                 } else {
                     node.attrs.type = 'button';
                 }
-                node.attrs['class'] = (node.attrs['class'] || '') + ' oe_kanban_action oe_kanban_action_' + node.tag;
+
+                var action_classes = " oe_kanban_action oe_kanban_action_" + node.tag;
+                if (node.attrs['t-attf-class']) {
+                    node.attrs['t-attf-class'] += action_classes;
+                } else if (node.attrs['t-att-class']) {
+                    node.attrs['t-att-class'] += " + '" + action_classes + "'";
+                } else {
+                    node.attrs['class'] = (node.attrs['class'] || '') + action_classes;
+                }
             }
             break;
     }
@@ -90,7 +97,7 @@ function transform_qweb_template(node, fields) {
 
 return AbstractRenderer.extend({
     className: "o_kanban_view",
-    init: function (parent, arch, fields, data, widgets_registry, record_options, column_options) {
+    init: function (parent, arch, fields, state, widgets_registry, options) {
         this._super.apply(this, arguments);
 
         this.qweb = new QWeb(session.debug, {_s: session.origin});
@@ -98,16 +105,37 @@ return AbstractRenderer.extend({
         transform_qweb_template(templates, fields);
         this.qweb.add_template(utils.json_node_to_xml(templates));
 
-        this.record_options = _.extend({}, record_options, { qweb: this.qweb });
-        this.column_options = _.extend({}, column_options, { qweb: this.qweb });
+        this.record_options = _.extend({}, options.record_options, { qweb: this.qweb });
+        this.column_options = _.extend({}, options.column_options, { qweb: this.qweb });
     },
     update: function (state, fields) {
         this.fields = fields;
         return this._super(state);
     },
-    update_column: function (column, data, record_options) {
+    update_record: function(record_state) {
+        var is_grouped = !!this.state.grouped_by.length;
+        var record;
+
+        if (is_grouped) {
+            // if grouped, this.widgets are kanban columns
+            // so we need to find the kanban record inside
+            _.each(this.widgets, function(widget) {
+                record = record || _.findWhere(widget.records, {db_id: record_state.id});
+            });
+        } else {
+            record = _.findWhere(this.widgets, {db_id: record_state.id});
+        }
+
+        if (record) {
+            record.update(record_state);
+        }
+    },
+    update_column: function (column_db_id, data, record_options) {
+        var column = _.findWhere(this.widgets, {db_id: column_db_id});
+        this.widgets.splice(_.indexOf(this.widgets, column), 1); // remove column from widgets' list
         record_options = _.extend({}, record_options, { qweb: this.qweb });
         var new_column = new KanbanColumn(this, data, this.column_options, record_options);
+        this.widgets.push(new_column);
         return new_column.insertAfter(column.$el).then(column.destroy.bind(column));
     },
     _render: function () {
@@ -141,7 +169,7 @@ return AbstractRenderer.extend({
     },
     _render_grouped: function (fragment) {
         var self = this;
-        var group_by_field_attrs = this.fields[this.state.grouped_by[0]];
+        var group_by_field_attrs = this.state.fields[this.state.grouped_by[0]];
         // Deactivate the drag'n'drop if the grouped_by field:
         // - is a date or datetime since we group by month or
         // - is readonly

web_studio/static/src/js/renderers/kanban_widgets.js
@@ -1,293 +1,0 @@
-odoo.define('web.kanban_widgets', function (require) {
-"use strict";
-
-var core = require('web.core');
-var field_utils = require('web.field_utils');
-var formats = require('web.formats');
-var Priority = require('web.Priority');
-var ProgressBar = require('web.ProgressBar');
-var pyeval = require('web.pyeval');
-var Registry = require('web.Registry');
-var Widget = require('web.Widget');
-var QWeb = core.qweb;
-var _t = core._t;
-/**
- * Interface to be implemented by kanban fields.
- *
- */
-var FieldInterface = {
-    /**
-        Constructor.
-        - parent: The widget's parent.
-        - field: A dictionary giving details about the field, including the current field's value in the
-            raw_value field.
-        - $node: The field <field> tag as it appears in the view, encapsulated in a jQuery object.
-    */
-    init: function(parent, field, $node) {},
-};
-
-/**
- * Abstract class for classes implementing FieldInterface.
- *
- * Properties:
- *     - value: useful property to hold the value of the field. By default, the constructor
- *     sets value property.
- *
- */
-var AbstractField = Widget.extend(FieldInterface, {
-    /**
-        Constructor that saves the field and $node parameters and sets the "value" property.
-    */
-    init: function(parent, field, $node) {
-        this._super(parent);
-        this.field = field;
-        this.$node = $node;
-        this.options = pyeval.py_eval(this.$node.attr("options") || '{}');
-        this.set("value", field.raw_value);
-    },
-});
-
-var FormatChar = AbstractField.extend({
-    tagName: 'span',
-    init: function(parent, field, $node) {
-        this._super.apply(this, arguments);
-        this.format_descriptor = _.extend({}, this.field, {'widget': this.$node.attr('widget')});
-    },
-    renderElement: function() {
-        this.$el.text(formats.format_value(this.field.raw_value, this.format_descriptor));
-    }
-});
-
-
-var KanbanPriority = AbstractField.extend({
-    init: function(parent, field, $node) {
-        this._super.apply(this, arguments);
-        this.name = $node.attr('name');
-    },
-    renderElement: function() {
-        this._super();
-        this.set('readonly', !!(this.field && this.field.readonly));
-    },
-    start: function() {
-        this.priority = new Priority(this, {
-            readonly: this.get('readonly'),
-            value: this.get('value'),
-            values: this.field.selection || [],
-        });
-
-        this.priority.on('update', this, function(update) {
-            var data = {};
-            data[this.name] = update.value;
-            this.trigger_up('kanban_update_record', data);
-        });
-
-        this.on('change:readonly', this, function() {
-            this.priority.readonly = this.get('readonly');
-            var $div = $('<div/>').insertAfter(this.$el);
-            this.priority.replace($div);
-            this.setElement(this.priority.$el);
-        });
-
-        var self = this;
-        return $.when(this._super(), this.priority.appendTo('<div>').done(function() {
-            self.priority.$el.addClass(self.$el.attr('class'));
-            self.replaceElement(self.priority.$el);
-        }));
-    },
-});
-
-var KanbanSelection = AbstractField.extend({
-
-    init: function(parent, field, $node) {
-        this._super.apply(this, arguments);
-        this.name = $node.attr('name');
-        this.parent = parent;
-    },
-    // Instead of making several rpc calls for each kanban card, take the values from the related fields on the tasks
-    prepare_dropdown_selection: function() {
-        var self = this;
-        var _data = [];
-        var stage_id = self.parent.values.stage_id.value[0];
-        var stage_data = {
-            id: stage_id,
-            legend_normal: self.parent.values.legend_normal ? self.parent.values.legend_normal.value : undefined,
-            legend_blocked: self.parent.values.legend_blocked ? self.parent.values.legend_blocked.value: undefined, 
-            legend_done: self.parent.values.legend_done ? self.parent.values.legend_done.value: undefined,
-        };
-        _.map(self.field.selection || [], function(res) {
-            var value = {
-                'name': res[0],
-                'tooltip': res[1],
-            };
-            if (res[0] === 'normal') {
-                value.state_name = stage_data.legend_normal ? stage_data.legend_normal : res[1];
-            } else if (res[0] === 'done') {
-                value.state_class = 'oe_kanban_status_green'; 
-                value.state_name = stage_data.legend_done ? stage_data.legend_done : res[1];
-            } else { 
-                value.state_class = 'oe_kanban_status_red'; 
-                value.state_name = stage_data.legend_blocked ? stage_data.legend_blocked : res[1];
-            }
-            _data.push(value);
-        });
-        return _data;
-    },
-    renderElement: function() {
-        var self = this;
-        self.states = this.prepare_dropdown_selection();
-
-        var current_state = _.find(this.states, function(state) {
-            return state.name === self.get('value');
-        }) || {state_class: ''};
-
-        self.$el = $(QWeb.render("KanbanSelection", {
-            current_state_class: current_state.state_class,
-            states: _.without(this.states, current_state)
-        }));
-        self.$('a').click(function (ev) {
-            ev.preventDefault();
-        });
-        self.$('a').click(self.set_kanban_selection.bind(self));
-    },
-    set_kanban_selection: function(e) {
-        e.preventDefault();
-        var self = this;
-        var $li = $(e.target).closest( "li" );
-        if ($li.length) {
-            var value = {};
-            value[self.name] = String($li.data('value'));
-            this.trigger_up('kanban_update_record', value, this);
-        }
-    },
-});
-
-var KanbanLabelSelection = AbstractField.extend({
-
-    init: function(parent, field, $node) {
-        this._super.apply(this, arguments);
-        this.classes = this.options && this.options.classes || {};
-    },
-    renderElement: function() {
-        this._super.apply(this, arguments);
-        var lbl_class = this.classes[this.field.raw_value] || 'primary';
-        this.$el.addClass('label label-' + lbl_class).text(this.field.value);
-    },
-});
-
-var KanbanAttachmentImage =  AbstractField.extend({
-    template: 'KanbanAttachmentImage',
-});
-
-
-/**
- * Kanban widgets: ProgressBar
- * parameters
- * - title: title of the gauge, displayed on top of the gauge
- * options
- * - editable: boolean if current_value is editable
- * - current_value: get the current_value from the field that must be present in the view
- * - max_value: get the max_value from the field that must be present in the view
- * - title: title of the gauge, displayed on top of the gauge --> not translated,  use parameter "title" instead
- * - on_change: action to call when cliking and setting a value
- */
-var KanbanProgressBar = AbstractField.extend({
-    events: {
-        'click': function() {
-            if(!this.readonly && this.progressbar.readonly) {
-                this.toggle_progressbar();
-            }
-        }
-    },
-
-    init: function (parent, field, node) {
-        this._super(parent, field, node);
-
-        var record = this.getParent().record;
-        this.progressbar = new ProgressBar(this, {
-            readonly: true,
-            value: record[this.options.current_value].raw_value,
-            max_value: record[this.options.max_value].raw_value,
-            title: _t(node && node[0].title || this.options.title),
-            edit_max_value: this.options.edit_max_value,
-        });
-
-        this.readonly = !this.options.editable;
-        this.on_change = this.options.on_change;
-    },
-
-    start: function () {
-        var self = this;
-
-        var def = this.progressbar.appendTo('<div>').done(function() {
-            self.progressbar.$el.addClass(self.$el.attr('class'));
-            self.replaceElement(self.progressbar.$el);
-        });
-
-        return $.when(this._super(), def).then(function() {
-            if(!self.readonly) {
-                var parent = self.getParent();
-                self.progressbar.on('update', self, function(update) {
-                    var value = update.changed_value;
-                    if(!isNaN(value)) {
-                        var data = {
-                            method: this.on_change,
-                            args: [parent.id, value],
-                            on_success: self.proxy('toggle_progressbar'),
-                        };
-                        self.trigger_up('kanban_call_method', data);
-                    } 
-                });
-            }
-        });
-    },
-
-    toggle_progressbar: function() {
-        this.progressbar.readonly = !this.progressbar.readonly;
-        var $div = $('<div/>').insertAfter(this.$el);
-        this.progressbar.replace($div);
-        this.setElement(this.progressbar.$el);
-    },
-});
-
-var KanbanMonetary = AbstractField.extend({
-    tagName: 'span',
-    init: function(parent, field, $node, data) {
-        this._super.apply(this, arguments);
-        this.record_data = data;
-    },
-    renderElement: function() {
-        this.$el.html(field_utils.format_monetary(this.field.raw_value, this.field, this.record_data, this.options));
-    }
-});
-
-var KanbanMany2One = AbstractField.extend({
-    tagName: 'span',
-    init: function(parent, field, $node, relational_data) {
-        this._super.apply(this, arguments);
-        this.m2o_value = field_utils.format_many2one(this.get('value'), this.field, undefined, {
-            relational_data: relational_data,
-        });
-    },
-    renderElement: function() {
-        this.$el.text(this.m2o_value);
-    },
-});
-
-var fields_registry = new Registry();
-
-fields_registry
-    .add('priority', KanbanPriority)
-    .add('kanban_state_selection', KanbanSelection)
-    .add("attachment_image", KanbanAttachmentImage)
-    .add('progress', KanbanProgressBar)
-    .add('float_time', FormatChar)
-    .add('monetary', KanbanMonetary)
-    .add('kanban_label_selection', KanbanLabelSelection)
-    .add('many2one', KanbanMany2One);
-
-return {
-    AbstractField: AbstractField,
-    registry: fields_registry,
-};
-
-});

web_studio/views/web_studio_templates.xml
@@ -51,7 +51,6 @@
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/kanban_quick_create.js"></script>
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/kanban_record.js"></script>
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/kanban_renderer.js"></script>
-            <script type="text/javascript" src="/web_studio/static/src/js/renderers/kanban_widgets.js"></script>
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/list_editable_renderer.js"></script>
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/list_renderer.js"></script>
             <script type="text/javascript" src="/web_studio/static/src/js/renderers/model.js"></script>

commit 865b9dac8a2f63259d8495b41abfb7c2b04a47c0

[FIX] web_studio: remove undefined widgets
Before this commit, it was not possible to load some views
because some widgets in registry was not defined.

This fixes: fd14281223b79cbd06fb2ba42d4cffdc446e4930

web_studio/static/src/js/renderers/field_widgets/field_registry.js
@@ -29,15 +29,11 @@ registry
     .add('char', basic_fields.FieldChar)
     .add('handle', basic_fields.HandleWidget)
     .add('email', basic_fields.EmailWidget)
-    .add('phone', basic_fields.FieldPhone)
     .add('url', basic_fields.UrlWidget)
     .add('image', basic_fields.FieldBinaryImage)
     .add('binary', basic_fields.FieldBinaryFile)
     .add('monetary', basic_fields.FieldMonetary)
     .add('priority', basic_fields.PriorityWidget)
-    .add('attachment_image', basic_fields.AttachmentImage)
-    .add('label_selection', basic_fields.LabelSelection)
-    .add('state_selection', basic_fields.StateSelectionWidget)
     .add('statusbar', basic_fields.StatusBar)
     .add('boolean_button', basic_fields.FieldBooleanButton)
     .add('id', basic_fields.FieldID)