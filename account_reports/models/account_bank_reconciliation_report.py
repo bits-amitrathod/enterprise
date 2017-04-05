01dd6a44f9afc9f8101a815cd424c110b6809f0c
qdp-odoo <qdp@odoo.com>
2017-04-05 16:00:30 +0200

    [FIX] account_reports: allow bank reconciliation report to exclude reversed moves.
    This commit goes along commit df755ef made on community repo.

--- account_reports/models/account_bank_reconciliation_report.py
+++ account_reports/models/account_bank_reconciliation_report.py
@@ -88,6 +88,7 @@ class account_bank_reconciliation_report(models.AbstractModel):
         move_lines = self.env['account.move.line'].search([('move_id.journal_id', '=', journal_id),
                                                            '|', ('statement_line_id', '=', False), ('statement_line_id.date', '>', self.env.context['date_to']),
                                                            ('user_type_id.type', '=', 'liquidity'),
+                                                           ('full_reconcile_id', '=', False),
                                                            ('date', '<=', self.env.context['date_to']),
                                                            ('company_id', 'in', self.env.context['company_ids'])])
         unrec_tot = 0
