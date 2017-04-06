581e15b4f25bc62f3e75af21446bd670378ae466
Cédric Snauwaert <csn@openerp.com>
2017-04-06 15:58:30 +0200

    [IMP] account_reports: add filtering by journals on financial report
    + enable that option for the P&L.
    
    Was PR #858.

--- account_reports/models/account_financial_report.py
+++ account_reports/models/account_financial_report.py
@@ -23,6 +23,7 @@ class ReportAccountFinancialReport(models.Model):
     comparison = fields.Boolean('Allow comparison', default=True, help='display the comparison filter')
     cash_basis = fields.Boolean('Use cash basis', help='if true, report will always use cash basis, if false, user can choose from filter inside the report')
     analytic = fields.Boolean('Allow analytic filter', help='display the analytic filter')
+    show_journal_filter = fields.Boolean('Allow filtering by journals', help='display the journal filter in the report')
     unfold_all_filter = fields.Boolean('Show unfold all filter', help='display the unfold all options in report')
     company_id = fields.Many2one('res.company', string='Company')
     menuitem_created = fields.Boolean("Menu Has Been Created", default=False)
@@ -57,6 +58,8 @@ class ReportAccountFinancialReport(models.Model):
             self.filter_cash_basis = None
         if self.unfold_all_filter:
             self.filter_unfold_all = False
+        if self.show_journal_filter:
+            self.filter_journals = True
         self.filter_all_entries = False
         self.filter_analytic = True if self.analytic else None
         return super(ReportAccountFinancialReport, self).get_options(previous_options)
