bits-amitrathod/enterprise

commit 0e8d1ed9511744efc0001e8520b3792451997301

[FIX] account_reports: missing trust level indicator for partner reports
* [FIX] account_reports: missing trust level indicator for partner in some reports

Aged partner balance and partner ledger should have an indicator next to the partner name showing if they are a good or bad debtor. Was PR #914

account_reports/models/account_aged_partner_balance.py
@@ -20,6 +20,7 @@ class report_account_aged_partner(models.AbstractModel):
     def get_templates(self):
         templates = super(report_account_aged_partner, self).get_templates()
         templates['main_template'] = 'account_reports.template_aged_partner_balance_report'
+        templates['line_template'] = 'account_reports.template_aged_partner_balance_line_report'
         return templates
 
     @api.model

account_reports/models/account_partner_ledger.py
@@ -103,6 +103,7 @@ class ReportPartnerLedger(models.AbstractModel):
                 'name': partner.name,
                 'columns': [{'name': v} for v in [self.format_value(debit), self.format_value(credit), self.format_value(balance)]],
                 'level': 2,
+                'trust': partner.trust,
                 'unfoldable': True,
                 'unfolded': 'partner_' + str(partner.id) in options.get('unfolded_lines') or unfold_all,
                 'colspan': 5,

account_reports/static/src/less/account_financial_report.less
@@ -71,6 +71,13 @@
     .color-green {
         color: green !important;
     }
+    .color-transparent {
+        opacity: 0 !important;
+    }
+    .trust-partner {
+        font-size: 0.8em;
+        margin-left: 5px;
+    }
     .print_only {
         display: none;
     }

account_reports/views/report_financial.xml
@@ -159,6 +159,17 @@
 
 <!-- partner and general ledger templates -->
 <template id="line_template_partner_ledger_report" inherit_id="account_reports.line_template" primary="True">
+    <xpath expr="//span[contains(@class, 'o_account_reports_caret_icon')]" position="before">
+        <t t-if="line.get('trust') == 'good'">
+            <span class="fa fa-circle color-green trust-partner"></span>
+        </t>
+        <t t-if="line.get('trust') == 'bad'">
+            <span class="fa fa-circle color-red trust-partner"></span>
+        </t>
+        <t t-if="line.get('trust') == 'normal'">
+            <span class="fa fa-circle color-transparent trust-partner"></span>
+        </t>
+    </xpath>
     <xpath expr="//ul[contains(@class, 'o_account_reports_domain_dropdown')]/li[last()]" position="before">
         <li t-if="line['caret_options'] != 'account.tax'" role="presentation"><a role="menuitem" tabindex="-1" t-att-data-id="line['id']" action="open_document">View Journal Entry</a></li>
     </xpath>
@@ -208,9 +219,28 @@
 </template>
 
 <!-- Aged partner balance templates -->
+<template id="template_aged_partner_balance_line_report" inherit_id="account_reports.line_template" primary="True">
+    <xpath expr="//span[contains(@class, 'o_account_reports_caret_icon')]" position="before">
+        <t t-if="line.get('trust') == 'good'">
+            <span class="fa fa-circle color-green trust-partner"></span>
+        </t>
+        <t t-if="line.get('trust') == 'bad'">
+            <span class="fa fa-circle color-red trust-partner"></span>
+        </t>
+        <t t-if="line.get('trust') == 'normal'">
+            <span class="fa fa-circle color-transparent trust-partner"></span>
+        </t>
+    </xpath>
+</template>
+
 <template id="template_aged_partner_balance_report" inherit_id="account_reports.main_template" primary="True">
     <xpath expr="//table" position="attributes">
         <attribute name="class">o_account_reports_table table-striped</attribute>
+    </xpath>
+    <xpath expr="//tbody" position="replace">
+        <t t-if="lines.get('lines')">
+            <t t-call="account_reports.template_aged_partner_balance_line_report" />
+        </t>
     </xpath>
 </template>
 

commit 9457e97770d4f4eca9da8d5987165035514faad0

[FIX] account_reports: add a try except for aged partner balance to prevent error on non-updated instances
Commit 0e8d1ed9511744efc0001e8520b3792451997301 has introduced a breaking change for user that did not upgrade their
account_reports module, this commit fix the problem by checking that the template is existing in Odoo (module updated)
or not.

account_reports/models/account_aged_partner_balance.py
@@ -20,7 +20,11 @@ class report_account_aged_partner(models.AbstractModel):
     def get_templates(self):
         templates = super(report_account_aged_partner, self).get_templates()
         templates['main_template'] = 'account_reports.template_aged_partner_balance_report'
-        templates['line_template'] = 'account_reports.template_aged_partner_balance_line_report'
+        try:
+            self.env['ir.ui.view'].get_view_id('account_reports.template_aged_partner_balance_line_report')
+            templates['line_template'] = 'account_reports.template_aged_partner_balance_line_report'
+        except ValueError:
+            pass
         return templates
 
     @api.model