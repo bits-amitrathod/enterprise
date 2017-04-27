9f16bee98659fe324f23b250dff6ef7b2bba2114
Aaron Bohy <aab@odoo.com>
2017-04-27 10:52:20 +0200

    [FIX] web_enterprise: Form statusbar buttons in mobile
    Some tweaks are done for the status buttons in form views to
    improve the UX in mobile. Those tweaks haven't been re-implemented
    in the new views. This is now done.

--- web_enterprise/static/src/js/views/form_view.js
+++ web_enterprise/static/src/js/views/form_view.js
@@ -1,42 +1,31 @@
-// FIXME: all this should be done directly in community
+odoo.define('web_enterprise.FormView', function (require) {
+"use strict";
 
-// odoo.define('web_enterprise.FormView', function (require) {
-// "use strict";
+/**
+ * The purpose of this file is to make the FormView use a special FormRenderer
+ * in mobile, implementing some tweaks to improve the UX in mobile.
+ */
 
-// var config = require('web.config');
-// var FormRenderingEngineMobile = require('web_enterprise.FormRenderingEngineMobile');
-// var FormView = require('web.FormView');
+var config = require('web.config');
+var FormRenderer = require('web.FormRenderer');
+var FormView = require('web.FormView');
+var MobileFormRenderer = require('web_enterprise.MobileFormRenderer');
 
-// FormView.include({
-//     defaults: _.extend({}, FormView.prototype.defaults, {
-//         disable_autofocus: config.device.touch,
-//     }),
-//     init: function () {
-//         this._super.apply(this, arguments);
-//         if (config.device.size_class <= config.device.SIZES.XS) {
-//             this.rendering_engine = new FormRenderingEngineMobile(this);
-//         }
-//     },
-// });
+FormView.include({
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
 
-// });
+    /**
+     * Overrides to select the MobileFormRenderer in mobile instead of the
+     * FormRenderer.
+     *
+     * @override
+     */
+    getRenderer: function () {
+        this.config.Renderer = config.isMobile ? MobileFormRenderer : FormRenderer;
+        return this._super.apply(this, arguments);
+    }
+});
 
-// odoo.define('web_enterprise.FormRenderingEngineMobile', function (require) {
-// "use strict";
-
-// var FormRenderingEngine = require('web.FormRenderingEngine');
-
-// return FormRenderingEngine.extend({
-//     fill_statusbar_buttons: function ($statusbar_buttons, $buttons) {
-//         if(!$buttons.length) {
-//             return;
-//         }
-//         var $statusbar_buttons_dropdown = this.render_element('FormRenderingStatusBar_DropDown', {});
-//         $buttons.each(function(i, el) {
-//             $statusbar_buttons_dropdown.find('.dropdown-menu').append($('<li/>').append(el));
-//         });
-//         $statusbar_buttons.append($statusbar_buttons_dropdown);
-//     },
-// });
-
-// });
+});
