74541c5a84f7825a59d0c035eebc1d4c547974a9
Xavier Morel <xmo@odoo.com>
2017-04-28 09:54:00 +0200

    [FIX] hr_contract_salary: poopy imports

--- hr_contract_salary/models/__init__.py
+++ hr_contract_salary/models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import hr_applicant
-import hr_contract
-import hr_employee
-import hr_job
-import hr_recruitment_config_settings
+from . import hr_applicant
+from . import hr_contract
+from . import hr_employee
+from . import hr_job
+from . import hr_recruitment_config_settings
