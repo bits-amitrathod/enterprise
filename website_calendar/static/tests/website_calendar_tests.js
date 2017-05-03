f65e466eb3ece890aebfd1eecea925c76f97fcfb
Denis Vermylen <dve@odoo.com>
2017-05-03 15:02:43 +0200

    [IMP] website_calendar: display direct employee links
    Use the direct employee link to book an appointment with a specific
    employee.
    Useful for employees to send out a personal appointment booking url.
    
    This feature was already implemented but hidden.
    
    This commit adds and applies a widget capable of displaying the url
    in the employee list view present in the appointment type form view.

---
+++ website_calendar/static/tests/website_calendar_tests.js
@@ -0,0 +1,76 @@
+odoo.define('website_calendar.tests', function (require) {
+"use strict";
+
+var FormView = require('web.FormView');
+var testUtils = require('web.test_utils');
+
+var createView = testUtils.createView;
+
+QUnit.module('website_calendar', {
+    beforeEach: function () {
+        this.data = {
+            'calendar.appointment.type': {
+                fields: {
+                    name: {type: 'char'},
+                    website_url: {type: 'char'},
+                    employee_ids: {type: 'many2many', relation: 'hr.employee'},
+                },
+                records: [{
+                    id: 1,
+                    name: 'Very Interdesting Meeting',
+                    website_url: '/website/calendar/schedule-a-demo-1/appointment',
+                    employee_ids: [214],
+                }],
+            },
+            'hr.employee': {
+                fields: {
+                    id: {type: 'integer'},
+                    name: {type: 'char'},
+                },
+                records: [{
+                    id: 214,
+                    name: 'Denis Ledur',
+                }],
+            },
+        };
+    },
+}, function () {
+
+    QUnit.test("empty previous_order widget", function (assert) {
+        assert.expect(2);
+
+        var form = createView({
+            View: FormView,
+            arch: '<form>' +
+                    '<field name="website_url" invisible="1"/>' +
+                    '<sheet>' +
+                        '<field name="employee_ids">' +
+                            '<tree string="Employees">' +
+                                '<field name="name"/>' +
+                                '<field name="id" widget="appointment_employee_url" string="Individual Appointment Link" context="{\'url\': parent.website_url}" readonly="1"/>' +
+                            '</tree>' +
+                            '<form string="Employees">' +
+                                '<group>' +
+                                    '<field name="name" class="avoid_me"/>' +
+                                '</group>' +
+                            '</form>' +
+                        '</field>' +
+                    '</sheet>' +
+                  '</form>',
+            data: this.data,
+            res_id: 1,
+            model: 'calendar.appointment.type',
+        });
+
+        assert.strictEqual(form.$('.o_form_uri').attr('href'),
+            '/website/calendar/schedule-a-demo-1?employee_id=214',
+            'Wrong employee url copied.');
+
+        form.$('.o_website_calendar_copy_icon').click();
+        // ensure we didn't open the form view
+        assert.ok($('.avoid_me').length === 0);
+
+        form.destroy();
+    });
+});
+});
