596b002cdbf916e4bd406e7af0db8443bdf91cdd
Elkasitu <adt@odoo.com>
2017-05-17 09:47:36 +0200

    [ADD] Create website_quote_subscription module to avoid losing features from the website_quote module without having the need for a hard dependency
    [FIX] website_subscription: unused imports in sale_order.py

---
+++ website_quote_subscription/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Online Subscription Quotes",
+    'auto-install': True,
+    'depends': ['website_subscription', 'website_quote'],
+
+    'summary': """
+        Quotes for your online subscriptions""",
+
+    'description': "",
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
--- website_quote_subscription/models/sale_order.py
+++ website_quote_subscription/models/sale_order.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import api, fields, models
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
