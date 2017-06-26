e56f6eee50bbff7f5caf63ef4c7ebc819367e7ad
Dharmang Soni <dpr@odoo.com>
2017-06-26 10:11:21 +0200

    [ADD] web_mobile: allow to create shortcuts in home screen
    The user is now able to create a shortcut to an action.
    This shortcut is then available in his mobile home screen.
    
    task-29451

--- web_mobile/static/src/js/user_menu.js
+++ web_mobile/static/src/js/user_menu.js
@@ -1,9 +1,13 @@
 odoo.define('web_mobile.user_menu', function (require) {
 "use strict";
 
+var core = require('web.core');
 var UserMenu = require('web.UserMenu');
+var web_client = require('web.web_client');
 
 var mobile = require('web_mobile.rpc');
+
+var _t = core._t;
 
 // Hide the logout link in mobile
 UserMenu.include({
@@ -15,6 +19,9 @@ UserMenu.include({
             this.$('a[data-menu="logout"]').addClass('hidden');
             this.$('a[data-menu="account"]').addClass('hidden');
             this.$('a[data-menu="switch"]').removeClass('hidden');
+        }
+        if (mobile.methods.addHomeShortcut) {
+            this.$('a[data-menu="shortcut"]').removeClass('hidden');
         }
         return this._super();
     },
@@ -29,6 +36,27 @@ UserMenu.include({
     _onMenuSwitch: function () {
         mobile.methods.switchAccount();
     },
+    /**
+     * @private
+     */
+    _onMenuShortcut: function () {
+        var urlData = $.bbq.getState();
+        if (urlData.menu_id) {
+            var menus = web_client.menu_data;
+            var menu = _.filter(menus.children, function (child) {
+                return child.id === parseInt(urlData.menu_id);
+            });
+            mobile.methods.addHomeShortcut({
+                'title': document.title,
+                'shortcut_url': document.URL,
+                'web_icon': menu && menu[0].web_icon_data
+            });
+         } else {
+             mobile.methods.showToast({
+                 "message": _t("No shortcut for App Switcher")
+             });
+         }
+         },
 });
 
 });
