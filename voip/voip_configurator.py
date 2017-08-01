f21f66c9d2079a30170d1507cb9f8a280471d543
Josse Colpaert <jco@openerp.com>
2017-08-01 15:06:47 +0200

    [FIX] voip: not working on standard user accounts
    When a user, like most normal users, does not have access to the
    Settings menu, it has no access to the ir config parameters of
    the voip, so he will not be able to dial (in production mode).
    
    That is why we add a sudo in the function that gets these parameters.
    That function returns those values to the web client however, so we need
    to check  that the user is not a portal user.  (has the group
    base.group_user)
    
    opw-751585

--- voip/voip_configurator.py
+++ voip/voip_configurator.py
@@ -2,6 +2,7 @@
 
 from openerp import models, fields, api
 from openerp.tools.translate import _
+from openerp.exceptions import AccessDenied
 import openerp
 
 # add sip_password to the fields that only users who can modify the user (incl. the user herself) see their real contents
@@ -18,14 +19,17 @@ class voip_configurator(models.Model):
 
     @api.model
     def get_pbx_config(self):
-        return {'pbx_ip': self.env['ir.config_parameter'].get_param('crm.voip.pbx_ip', default='localhost'),
-                'wsServer': self.env['ir.config_parameter'].get_param('crm.voip.wsServer', default='ws://localhost'),
+        if not self.env.user.has_group('base.group_user'):
+            raise AccessDenied()
+        ICP = self.env['ir.config_parameter'].sudo()
+        return {'pbx_ip': ICP.get_param('crm.voip.pbx_ip', default='localhost'),
+                'wsServer': ICP.get_param('crm.voip.wsServer', default='ws://localhost'),
                 'login': self.env.user[0].sip_login,
                 'password': self.env.user[0].sip_password,
                 'external_phone': self.env.user[0].sip_external_phone,
                 'always_transfer': self.env.user[0].sip_always_transfer,
                 'ring_number': self.env.user[0].sip_ring_number or 6,
-                'mode': self.env['ir.config_parameter'].get_param('crm.voip.mode', default="demo"),
+                'mode': ICP.get_param('crm.voip.mode', default="demo"),
                 }
 
     #not deleted yet waiting to be sure about the error management.
