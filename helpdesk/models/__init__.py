7667e0d734a6bdad58d4545fb57ad35e21bcfc79
Thibault Delavallée <tde@odoo.com>
2017-08-18 13:33:29 +0200

    [MOV] helpdesk: split models to ease reading
    Purpose is to avoid having one big file. We choose to separate ticket
    models from other models.

--- helpdesk/models/__init__.py
+++ helpdesk/models/__init__.py
@@ -2,5 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import helpdesk
+from . import helpdesk_ticket
 from . import res_users
 from . import res_partner
