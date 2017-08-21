5fa6cd47ee83a52ddc94318f3d9dda09bf2a2e8f
Thibault Delavallée <tde@odoo.com>
2017-08-21 14:35:32 +0200

    [CLN] website_subscription: clean code
     * clean strange imports probably due to some Odoo internals -> standard
       code move;
     * remove dead JS code about token now that new payment form has been
       merged;
     * remove dead LESS code used in customer portal;
    
    This commit does not imply any functional code. It just remove unnecessary
    code to ease understanding and future commits.

--- website_subscription/__init__.py
+++ website_subscription/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from odoo.release import version_info
-try:
-    from . import models       # noqa
-    from . import controllers  # noqa
-except ImportError:
-    if version_info >= (8, 'saas~6'):
-        raise
+
+from . import models
+from . import controllers
