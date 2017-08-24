81ad3d8724396f892ee9766fef5ce5764c49a710
Christophe Simonis <chs@odoo.com>
2017-08-24 16:01:35 +0200

    [FIX] quality: access products through move lines

--- quality/models/stock_picking.py
+++ quality/models/stock_picking.py
@@ -57,7 +57,7 @@ class StockPicking(models.Model):
     @api.multi
     def do_transfer(self):
         # Do the check before transferring
-        product_to_check = self.pack_operation_product_ids.filtered(lambda x: x.qty_done != 0).mapped('product_id')
+        product_to_check = self.move_line_ids.filtered(lambda x: x.qty_done != 0).mapped('product_id')
         if self.mapped('check_ids').filtered(lambda x: x.quality_state == 'none' and x.product_id in product_to_check):
             raise UserError(_('You still need to do the quality checks!'))
         return super(StockPicking, self).do_transfer()
