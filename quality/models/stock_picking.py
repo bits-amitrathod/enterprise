398a57e0432fe28c4438d967a18f456c7a75d167
Josse Colpaert <jco@openerp.com>
2017-08-25 18:44:43 +0200

    [FIX] quality: forward-port pack_operation_product_ids should be move_line_ids

--- quality/models/stock_picking.py
+++ quality/models/stock_picking.py
@@ -55,9 +55,9 @@ class StockPicking(models.Model):
         return res
 
     @api.multi
-    def do_transfer(self):
+    def action_done(self):
         # Do the check before transferring
-        product_to_check = self.pack_operation_product_ids.filtered(lambda x: x.qty_done != 0).mapped('product_id')
+        product_to_check = self.move_line_ids.filtered(lambda x: x.qty_done != 0).mapped('product_id')
         if self.mapped('check_ids').filtered(lambda x: x.quality_state == 'none' and x.product_id in product_to_check):
             raise UserError(_('You still need to do the quality checks!'))
         return super(StockPicking, self).do_transfer()
