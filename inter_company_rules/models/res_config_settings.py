07cd4ef781568151947fcffbfaef5da8a8a67413
Yannick Tivisse <yti@odoo.com>
2017-09-04 12:49:05 +0200

    [IMP] settings: Remove redefinition of `company_id` in settings.
    On the other hand, create a new field for the configuration of the inter company rules.

--- inter_company_rules/models/res_config_settings.py
+++ inter_company_rules/models/res_config_settings.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    company_id = fields.Many2one('res.company', string='Select Company',
+    rules_company_id = fields.Many2one('res.company', string='Select Company',
         help='Select company to setup Inter company rules.')
     rule_type = fields.Selection([('so_and_po', 'Send Sales & Purchase Orders'),
         ('invoice_and_refunds', 'Send Invoices & Credit Notes')],
@@ -24,25 +24,25 @@ class ResConfigSettings(models.TransientModel):
         if self.rule_type == 'so_and_po':
             self.invoice_and_refunds = False
 
-    @api.onchange('company_id')
-    def onchange_company_id(self):
-        if self.company_id:
+    @api.onchange('rules_company_id')
+    def onchange_rules_company_id(self):
+        if self.rules_company_id:
             rule_type = False
-            if self.company_id.so_from_po or self.company_id.po_from_so or self.company_id.auto_validation:
+            if self.rules_company_id.so_from_po or self.rules_company_id.po_from_so or self.rules_company_id.auto_validation:
                 rule_type = 'so_and_po'
-            elif self.company_id.auto_generate_invoices:
+            elif self.rules_company_id.auto_generate_invoices:
                 rule_type = 'invoice_and_refunds'
 
             self.rule_type = rule_type
-            self.so_from_po = self.company_id.so_from_po
-            self.po_from_so = self.company_id.po_from_so
-            self.auto_validation = self.company_id.auto_validation
-            self.warehouse_id = self.company_id.warehouse_id.id
+            self.so_from_po = self.rules_company_id.so_from_po
+            self.po_from_so = self.rules_company_id.po_from_so
+            self.auto_validation = self.rules_company_id.auto_validation
+            self.warehouse_id = self.rules_company_id.warehouse_id.id
 
     # YTI FIXME: Could define related fields instead
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        if self.company_id:
+        if self.rules_company_id:
             vals = {
                 'so_from_po': self.so_from_po if self.rule_type == 'so_and_po' else False,
                 'po_from_so': self.po_from_so if self.rule_type == 'so_and_po' else False,
@@ -50,4 +50,4 @@ class ResConfigSettings(models.TransientModel):
                 'auto_generate_invoices': True if self.rule_type == 'invoice_and_refunds' else False,
                 'warehouse_id': self.warehouse_id.id
             }
-            self.company_id.write(vals)
+            self.rules_company_id.write(vals)
