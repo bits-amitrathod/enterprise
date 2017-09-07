21134d75a18613e50fc19b10503bc3a237eaf49c
Nhomar Hernandez <nhomar@vauxoo.com>
2017-09-07 16:29:15 +0200

    [IMP] currency_rate_live: Update currency using bank of México (Banxico).
        Banxico WebService - http://www.banxico.org.mx/DgieWSWeb/DgieWS?WSDL
    
        - The date of the currency is extracted from WebService (not current date) because banxico change USD currency after 12:00pm but other currencies is not
          updated yet, then the dates are different.
    
        Was PR#1351. Courtesy of Vauxoo

--- currency_rate_live/models/res_config_settings.py
+++ currency_rate_live/models/res_config_settings.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from lxml import etree
+from lxml import etree, objectify
 import json
 from dateutil.relativedelta import relativedelta
 import re
 
 import requests
+import suds.client
 
 from odoo import api, fields, models
 from odoo.addons.web.controllers.main import xml2json_from_elementtree
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+
+BANXICO_DATE_FORMAT = '%Y-%m-%d'
 
 
 class ResCompany(models.Model):
@@ -24,14 +28,22 @@ class ResCompany(models.Model):
         ('monthly', 'Monthly')],
         default='manually', string='Interval Unit')
     currency_next_execution_date = fields.Date(string="Next Execution Date")
-    currency_provider = fields.Selection([('yahoo', 'Yahoo'), ('ecb', 'European Central Bank'), ('fta', 'Federal Tax Administration (Switzerland)')], default='ecb', string='Service Provider')
+    currency_provider = fields.Selection([
+        ('yahoo', 'Yahoo'),
+        ('ecb', 'European Central Bank'),
+        ('fta', 'Federal Tax Administration (Switzerland)'),
+        ('banxico', 'Mexican Bank'),
+    ], default='ecb', string='Service Provider')
     last_currency_sync_date = fields.Date(string="Last Sync Date", readonly=True)
 
     @api.model
     def create(self, vals):
         ''' Change the default provider depending on the company data.'''
-        if vals.get('country_id') and 'currency_provider' not in vals and self.env['res.country'].browse(vals['country_id']).code.upper() == 'CH':
-            vals['currency_provider'] = 'fta'
+        if vals.get('country_id') and 'currency_provider' not in vals:
+            if self.env['res.country'].browse(vals['country_id']).code.upper() == 'CH':
+                vals['currency_provider'] = 'fta'
+            elif self.env['res.country'].browse(vals['country_id']).code.upper() == 'MX':
+                vals['currency_provider'] = 'banxico'
         return super(ResCompany, self).create(vals)
 
     @api.model
@@ -42,6 +54,9 @@ class ResCompany(models.Model):
             if company.country_id.code == 'CH':
                 # Sets FTA as the default provider for every swiss company that was already installed
                 company.currency_provider = 'fta'
+            elif company.country_id.code == 'MX':
+                # Sets Banxico as the default provider for every mexican company that was already installed
+                company.currency_provider = 'banxico'
             else:
                 company.currency_provider = 'ecb'
 
@@ -56,11 +71,12 @@ class ResCompany(models.Model):
                 res = company._update_currency_ecb()
             elif company.currency_provider == 'fta':
                 res = company._update_currency_fta()
+            elif company.currency_provider == 'banxico':
+                res = company._update_currency_banxico()
             if not res:
                 raise UserError(_('Unable to connect to the online exchange rate platform. The web service may be temporary down. Please try again in a moment.'))
             elif company.currency_provider:
                 company.last_currency_sync_date = fields.Date.today()
-
 
     def _update_currency_fta(self):
         ''' This method is used to update the currency rates using Switzerland's
@@ -115,7 +131,6 @@ class ResCompany(models.Model):
                             break
 
                     rates_dict[currency_code] = float(re.search('\d+',currency_xml).group()) / float(rate_xml)
-
         return rates_dict
 
     def _update_currency_ecb(self):
@@ -190,6 +205,77 @@ class ResCompany(models.Model):
                         CurrencyRate.create({'currency_id': currency.id, 'rate': rate['Rate'], 'name': fields.Date.today(), 'company_id': company.id})
             if company.currency_id.rate != 1.0:
                 CurrencyRate.create({'currency_id': company.currency_id.id, 'rate': 1.0, 'name': fields.Date.today(), 'company_id': company.id})
+        return True
+
+    def _update_currency_banxico(self):
+        """Update the USD exchange against the MXN forcing using Banxico.
+        * With basement in legal topics in Mexico the rate must be **one** per day and it is equal to the rate known the
+        day immediate before the rate is gotten, it means the rate for 02/Feb is the one at 31/jan.
+        * The base currency is always MXN but with the inverse 1/rate.
+        * The official institution is Banxico.
+        * The webservice returns the following currency rates (same order):
+            1) USD SAT - Officially used from SAT institution
+            2) USD Fixed
+            3) EUR
+            4) CAD
+            5) JPY
+            6) GAB
+        Source: http://www.banxico.org.mx/portal-mercado-cambiario/
+        """
+        def update_rate(currency, rate, date):
+            #  Deleting current rate values because we can only have one
+            currency.rate_ids.filtered(lambda r: date == r.name).unlink()
+            currency.rate_ids.create({
+                'rate': rate,
+                'currency_id': currency.id,
+                'name': date,
+            })
+            # Update cached rate field
+            currency._compute_current_rate()
+        try:
+            client = suds.client.Client('http://www.banxico.org.mx/DgieWSWeb/DgieWS?WSDL', cache=None)
+            xml_str = client.service.tiposDeCambioBanxico().encode('utf-8')
+        except:
+            return False
+        xml = objectify.fromstring(xml_str)
+        ns = xml.nsmap
+        # nsmap don't support "None" key then deleting
+        ns.pop(None, None)
+        serie = xml.xpath("bm:DataSet/bm:Series[1]/bm:Obs", namespaces=ns)[0]
+        usd_mxn = float(serie.get('OBS_VALUE'))
+        date = datetime.datetime.strptime(
+            serie.get('TIME_PERIOD'), BANXICO_DATE_FORMAT).strftime(DEFAULT_SERVER_DATE_FORMAT)
+        mxn = self.env.ref('base.MXN')
+        usd = self.env.ref('base.USD')
+        base_currency = mxn
+        currency_to_update = usd
+        rate = 1.0 / usd_mxn
+        if mxn.rate != 1 and usd.rate == 1:
+            # Most of the time Mexico use USD.rate=1 and company.currency=MXN
+            base_currency = usd
+            currency_to_update = mxn
+            rate = usd_mxn
+        else:
+            # Force MXN.rate=1 to get a valid base
+            update_rate(mxn, 1, date)
+        update_rate(currency_to_update, rate, date)
+        base_mxn_rate = base_currency.compute(1, mxn, round=False)
+        foreigns = {
+            # position order of the rates from webservices
+            3: self.env.ref('base.EUR'),
+            4: self.env.ref('base.CAD'),
+            5: self.env.ref('base.JPY'),
+            6: self.env.ref('base.GBP'),
+        }
+        for index, currency in foreigns.items():
+            serie = xml.xpath("bm:DataSet/bm:Series[%d]/bm:Obs" % index, namespaces=ns)[0]
+            try:
+                foreign_mxn_rate = float(serie.get('OBS_VALUE'))
+            except ValueError:
+                continue
+            foreign_rate_date = datetime.datetime.strptime(
+                serie.get('TIME_PERIOD'), BANXICO_DATE_FORMAT).strftime(DEFAULT_SERVER_DATE_FORMAT)
+            update_rate(currency, base_mxn_rate / foreign_mxn_rate, foreign_rate_date)
         return True
 
     @api.model
--- currency_rate_live/tests/test_live_currency_update.py
+++ currency_rate_live/tests/test_live_currency_update.py
@@ -9,6 +9,7 @@ class CurrencyTestCase(TransactionCase):
         company_ecb = self.env['res.company'].create({'name': 'TEST ECB', 'currency_provider': 'ecb'})
         company_yah = self.env['res.company'].create({'name': 'TEST YAH', 'currency_provider': 'yahoo'})
         company_fta = self.env['res.company'].create({'name': 'TEST FTA', 'currency_provider': 'fta'})
+        company_banxico = self.env['res.company'].create({'name': 'TEST BANXICO', 'currency_provider': 'banxico'})
         # testing Swiss Federal Tax Administration requires that Franc Suisse can be found
         # which is not the case in runbot/demo data as l10n_ch is not always installed
         self.env.ref('base.CHF').write({'active': True})
@@ -17,11 +18,12 @@ class CurrencyTestCase(TransactionCase):
         self.currency_usd = self.env.ref('base.USD')
         rates_number = len(self.currency_usd.rate_ids)
 
-        #get the live rate for both companies, each one with a different method
+        #get the live rate for all companies, each one with a different method
         res_ecb = company_ecb._update_currency_ecb()
         res_yah = company_yah._update_currency_yahoo()
         res_fta = company_fta._update_currency_fta()
+        res_banxico = company_banxico._update_currency_banxico()
 
-        #Check that both company call to company_ecb.update_currency_rates() has created a new rate for the USD (only if the request was a success)
+        #Check that all companies call to _update_currency_rates() has created a new rate for the USD (only if the request was a success)
         rates_number_again = len(self.currency_usd.rate_ids)
-        self.assertEqual(rates_number + int(res_ecb) + int(res_yah) + int(res_fta), rates_number_again)
+        self.assertEqual(rates_number + int(res_ecb) + int(res_yah) + int(res_fta) + int(res_banxico), rates_number_again)
