17257f03745291e05cf336210bcfcac22b9f6637
Martin Geubelle <mge@odoo.com>
2017-09-26 15:16:54 +0200

    [FIX] web_studio: edit a field in Search Editor
    Before this rev., clicking on a field in the Search Editor was causing a traceback.
    
    As the search view is not really a view like the others, there is no attribute
    `state` on it.
    
    This lookup was however not useful as the variable is not used when editing the
    search view.

--- web_studio/static/src/js/view_editor_manager.js
+++ web_studio/static/src/js/view_editor_manager.js
@@ -1185,8 +1185,8 @@ var ViewEditorManager = Widget.extend({
         // edit or create the subviews
         var fields_view = this.x2mField ? this._getX2mFieldsView() : this.fields_view;
         var field = fields_view.fields[node.attrs.name];
-        var attrs = this.editor.state.fieldsInfo[this.editor.state.viewType][node.attrs.name];
         if (this.view_type === 'form' && field) {
+            var attrs = this.editor.state.fieldsInfo[this.editor.state.viewType][node.attrs.name];
             var isX2Many = _.contains(['one2many','many2many'], field.type);
             var notEditableWidgets = ['many2many_tags', 'hr_org_chart'];
             if (isX2Many && !_.contains(notEditableWidgets, attrs.widget)) {
