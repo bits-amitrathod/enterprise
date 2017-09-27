d84e9b8031b7422c8c043d709ded9745f70b1a75
Simon Lejeune <sle@openerp.com>
2017-09-27 14:26:45 +0200

    [REM] mrp_barcode
    This module is broken since saas-15 and not integrated with the new
    tablet view based on quality checks. We will make a proper barcode
    integration in a task later on.

--- mrp_barcode/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import inherited_mrp_production
-from . import inherited_mrp_workorder
-from . import inherited_stock_move
-from . import inherited_stock_picking_type
