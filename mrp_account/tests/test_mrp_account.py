1fb4699e5b64ac68e06b8e3ae8088d0bd81d2e2a
amoyaux <arm@odoo.com>
2017-09-27 18:57:32 +0200

    [FIX] mrp_account: fix tests after demo data refafctoring in mrp
    The tracking mehtod on some products in demo data have been changed and
    some other products have been added to a bom. So the cost computed in
    tests has changed

--- mrp_account/tests/test_mrp_account.py
+++ mrp_account/tests/test_mrp_account.py
@@ -22,7 +22,9 @@ class TestMrpAccount(common.TransactionCase):
         self.product_bolt = self.env.ref('mrp.product_product_computer_desk_bolt')
         self.product_bolt.categ_id = self.categ_standard.id
         self.source_location_id = self.ref('stock.stock_location_14')
-    
+        self.product_screw = self.env.ref('mrp.product_product_computer_desk_screw')
+        self.product_screw.categ_id = self.categ_standard.id
+
     def test_00_production_order_with_accounting(self):
         self.product_table_sheet.standard_price = 20.0
         self.product_table_leg.standard_price = 15.0
@@ -45,7 +47,13 @@ class TestMrpAccount(common.TransactionCase):
                 'product_uom_id': self.product_bolt.uom_id.id,
                 'product_qty': 20,
                 'location_id': self.source_location_id
-            })]
+            }), (0, 0, {
+                'product_id': self.product_screw.id,
+                'product_uom_id': self.product_screw.uom_id.id,
+                'product_qty': 200000,
+                'location_id': self.source_location_id
+            }),
+            ]
         })
         inventory.action_done()
         self.env.ref('mrp.mrp_bom_desk').routing_id = False # TODO: extend the test later with the necessary operations
@@ -65,10 +73,11 @@ class TestMrpAccount(common.TransactionCase):
         produce_wizard.do_produce()
         production_table.post_inventory()
         move_value = production_table.move_finished_ids.filtered(lambda x: x.state == "done").value
-        
-        # Real price of the head (quant: 20) + avg price leg (product: 4*15) + standard price bolt (product: 4*10)
-        self.assertEqual(move_value, 120, 'Thing should have the correct price')
-        
+
+        # Real price of the head (quant: 20) + standard price screw (product: 10*10) + standard price bolt (product: 4*10).
+        # We didn't provide a SN/LOT to the wizard, thus they were not used
+        self.assertEqual(move_value, 140, 'Thing should have the correct price')
+
         produce_wizard = self.env['mrp.product.produce'].with_context({
             'active_id': production_table.id,
             'active_ids': [production_table.id],
@@ -78,5 +87,5 @@ class TestMrpAccount(common.TransactionCase):
         produce_wizard.do_produce()
         production_table.post_inventory()
         move_value = production_table.move_finished_ids.filtered(lambda x: x.state == "done" and x.product_qty == 2.0).value
-        # 2 * Real price of the head (quant: 20) + avg price leg (product: 4*15) + standard price bolt (product: 4*10)
-        self.assertEqual(move_value, 240, 'Thing should have the correct price')
+        # 2 * Real price of the head (quant: 20) + standard price screw (product: 20*10) + standard price bolt (product: 8*10)
+        self.assertEqual(move_value, 280, 'Thing should have the correct price')
