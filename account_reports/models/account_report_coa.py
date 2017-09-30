bits-amitrathod/enterprise

commit 535b69ab122241f836b17bbd82af9ee5904c2f04

[FIX] l10n_mx_reports: show debit/credit total in trial balance
Was PR #1370.

account_reports/models/account_report_coa.py
@@ -124,6 +124,8 @@ class report_account_coa(models.AbstractModel):
                 if account not in grouped_accounts.keys():
                     grouped_accounts[account] = [{'balance': 0, 'debit': 0, 'credit': 0} for p in comparison_table]
                 grouped_accounts[account][period_number]['balance'] = res[account]['balance'] - res[account]['initial_bal']['balance']
+                grouped_accounts[account][period_number]['debit'] = res[account]['debit']
+                grouped_accounts[account][period_number]['credit'] = res[account]['credit']
             period_number += 1
 
         #make intermediary sums and build the report

l10n_mx_reports/models/trial_balance.py
@@ -46,6 +46,7 @@ class MxReportAccountTrial(models.AbstractModel):
         if self.env.user.company_id.country_id.code.upper() == 'MX':
             # TODO: something like: and all([c.country_id.code == 'MX' for c in options.companies]):
             afrl_obj = self.env['account.financial.html.report.line']
+
             lines = []
             afr_lines = afrl_obj.search([
                 ('parent_id', '=', False),
@@ -127,7 +128,8 @@ class MxReportAccountTrial(models.AbstractModel):
                 for period in range(len(comparison_table)):
                     amount = grouped_accounts[account][period]['balance']
                     total_periods += amount
-                    cols += [amount > 0 and self.format_value(amount) or '', amount < 0 and self.format_value(-amount) or '']
+                    cols += [self.format_value(grouped_accounts[account][period]['debit']),
+                             self.format_value(grouped_accounts[account][period]['credit'])]
                 cols += [self.format_value(initial_balances.get(account, 0.0) + total_periods)]
             lines.append({
                 'id': account.id,

commit cbd4af87ba7f76e473b60e2232e983679d55805e

[FIX] l10n_mx_reports: fix bad debit/credit in trial balance.
An error introduced in https://github.com/odoo/enterprise/commit/535b69ab122241f836b17bbd82af9ee5904c2f04 was always including the initial balance in debit/credit columns.

account_reports/models/account_report_coa.py
@@ -124,8 +124,8 @@ class report_account_coa(models.AbstractModel):
                 if account not in grouped_accounts.keys():
                     grouped_accounts[account] = [{'balance': 0, 'debit': 0, 'credit': 0} for p in comparison_table]
                 grouped_accounts[account][period_number]['balance'] = res[account]['balance'] - res[account]['initial_bal']['balance']
-                grouped_accounts[account][period_number]['debit'] = res[account]['debit']
-                grouped_accounts[account][period_number]['credit'] = res[account]['credit']
+                grouped_accounts[account][period_number]['debit'] = res[account]['debit'] - res[account]['initial_bal']['debit']
+                grouped_accounts[account][period_number]['credit'] = res[account]['credit'] - res[account]['initial_bal']['credit']
             period_number += 1
 
         #make intermediary sums and build the report