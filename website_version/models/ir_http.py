34faa559380128532a24e2eda875955859d82584
rde <rde@odoo.com>
2017-09-30 11:50:26 +0200

    [REM] website: remove unused method get_page_key
    call to this method got removed on #f015746549e1eb380bfbda8c8f47d1f1e5d0cfbc
    Commit linked to community commit #977d16d4129fd69c21ee8c82d2a20ad363dee6c9

--- website_version/models/ir_http.py
+++ website_version/models/ir_http.py
@@ -15,12 +15,3 @@ class ir_http(orm.AbstractModel):
             data=json.dumps(request.context['website_version_experiment'], ensure_ascii=False)
             x.set_cookie('website_version_experiment', data)
         return x
-
-    @classmethod
-    def get_page_key(cls):
-        key = super(ir_http, cls).get_page_key()
-        if hasattr(request, 'website'):
-            key += (request.website.id,)
-        seq_ver = [int(ver) for ver in request.context.get('website_version_experiment', {}).values()]
-        key += (str(sorted(seq_ver)),)
-        return key
