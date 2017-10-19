bits-amitrathod/enterprise

commit fd4eeb38c6dbe3096102076fc259308ef4a5f7a0

[IMP] web_studio: binary fields should keep the file name
Previously, downloading a file from a Studio-created binary field
yielded a file whose name was that of the associated field, without any
extension. This caused a serious usability issue, as it was the user's
responsibility to select the right software to open the file.

This commit ensures that a binary field is always dropped with an
associated char field (whose role is to contain the file name with its
correct extension). As a result, files uploaded via a Studio binary
field keep their names when they are subsequently downloaded.

web_studio/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from copy import deepcopy
 from lxml import etree
 from odoo import http, models, _
 from odoo.http import content_disposition, request
@@ -554,6 +555,35 @@ class WebStudioController(http.Controller):
         parser = etree.XMLParser(remove_blank_text=True)
         arch = etree.fromstring(studio_view_arch, parser=parser)
         model = view.model
+
+        # Determine whether an operation is associated with
+        # the creation of a binary field
+        def create_binary_field(op):
+            if op['node'].get('tag') == 'field' and op['node'].get('field_description'):
+                ttype = op['node']['field_description'].get('ttype')
+                is_image = op['node']['attrs'].get('widget') == 'image'
+                return ttype == 'binary' and not is_image
+            return False
+
+        # Every time the creation of a binary field is requested,
+        # we also create an invisible char field meant to contain the filename.
+        # The char field is then associated with the binary field
+        # via the 'filename' attribute of the latter.
+        for op in [op for op in operations if create_binary_field(op)]:
+            filename = op['node']['field_description']['name'] + '_filename'
+
+            # Create an operation adding an additional char field
+            char_op = deepcopy(op);
+            char_op['node']['field_description'].update({
+                'name': filename,
+                'ttype': 'char',
+                'field_description': _('Filename for %s') % op['node']['field_description']['name'],
+            })
+            char_op['node']['attrs']['invisible'] = '1'
+            operations.append(char_op)
+
+            op['node']['attrs']['filename'] = filename
+
         for op in operations:
             # create a new field if it does not exist
             if 'node' in op:

commit a4c3e5b921b38bb53ad52e99f301b4745edae76c

[FIX] web_studio: traceback when removing a field
https://github.com/odoo/enterprise/commit/fd4eeb38c6dbe3096102076fc259308ef4a5f7a0
introduced a traceback when deleting a field from a view.

The traceback arises from the fact that the above commit wrongly assumes
that every operation has a node which is not the case.

web_studio/controllers/main.py
@@ -563,9 +563,10 @@ class WebStudioController(http.Controller):
         # Determine whether an operation is associated with
         # the creation of a binary field
         def create_binary_field(op):
-            if op['node'].get('tag') == 'field' and op['node'].get('field_description'):
-                ttype = op['node']['field_description'].get('ttype')
-                is_image = op['node']['attrs'].get('widget') == 'image'
+            node = op.get('node')
+            if node and node.get('tag') == 'field' and node.get('field_description'):
+                ttype = node['field_description'].get('ttype')
+                is_image = node['attrs'].get('widget') == 'image'
                 return ttype == 'binary' and not is_image
             return False
 