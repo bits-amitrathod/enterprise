bits-amitrathod/enterprise

commit 2a425c76461a7ce67cfb6f91d568843e996ffe5d

[FIX] web_studio: properly anchor template fields
Before this commit, the normalization algorithm would
anchor template elements directly inside the kanban element,
which wouldn't make it show up in the actual interface
because in order for it to show up in the interface it
has to be inside the templates element.

This is fixed by checking if the template element is an
ancestor of the current element being examined during
the xpath expr generation, and appending "templates//"
to the generated expr.

web_studio/models/ir_ui_view.py
@@ -334,7 +334,7 @@ class View(models.Model):
         if target_node.tag == 'attribute':
             target_node = target_node.getparent().getparent()
 
-        if target_node.attrib.get('name'):
+        if target_node.get('name'):
             expr = '//%s' % self._identify_node(target_node)
 
         else:
@@ -343,7 +343,8 @@ class View(models.Model):
                 for n in target_node.iterancestors()
                 if n.getparent() is not None
             ]
-            expr = '//%s/%s' % ('/'.join(reversed(ancestors)), self._identify_node(target_node))
+            expr = '//%s/%s' % ('/'.join(reversed(ancestors)),
+                                self._identify_node(target_node))
 
         return expr
 
@@ -360,7 +361,20 @@ class View(models.Model):
                 node.tag,
                 len(list(node.itersiblings(tag=node.tag, preceding=True))) + 1
             )
+
+        if self._is_templates_node(node):
+            node_str = 'templates//' + node_str
+
         return node_str
+
+    def _is_templates_node(self, node):
+        """
+        Checks if the node in the view is inside a templates element
+        so that //templates might be prepended to the xpath generated in
+        _closest_node_to_xpath
+        """
+        ancestors = node.iterancestors()
+        return len([x for x in ancestors if x.tag == 'templates']) > 0
 
     def _closest_node_to_xpath(self, node, old_view):
         """

web_studio/tests/test_view_normalization.py
@@ -84,6 +84,13 @@ class TestViewNormalization(TransactionCase):
                                     <field name="state_id"/>
                                     <field name="image"/>
                                     <field name="lang"/>
+                                    <templates>
+                                        <t t-name="kanban-box">
+                                            <div class="oe_kanban_details">
+                                                <field name="name"/>
+                                            </div>
+                                        </t>
+                                    </templates>
                                 </kanban>
                             </field>
                         </page>
@@ -651,6 +658,32 @@ class TestViewNormalization(TransactionCase):
               <xpath expr="//field[@name='email']" position="replace"/>
               <xpath expr="//field[@name='phone']" position="replace">
                 <field name="email"/>
+              </xpath>
+            </data>
+        """)
+
+    # template fields are appended to the templates, not to the kanban itself
+    def test_view_normalization_21(self):
+        self._test_view_normalization("""
+            <data>
+              <xpath expr="//templates//field[@name='name']" position="after">
+                <field name="phone"/>
+              </xpath>
+              <xpath expr="//templates//field[@name='phone']" position="after">
+                <field name="mobile"/>
+              </xpath>
+              <xpath expr="//templates//field[@name='name']" position="before">
+                <field name="color"/>
+              </xpath>
+            </data>
+        """, """
+            <data>
+              <xpath expr="//templates//field[@name='name']" position="before">
+                <field name="color"/>
+              </xpath>
+              <xpath expr="//templates//field[@name='name']" position="after">
+                <field name="phone"/>
+                <field name="mobile"/>
               </xpath>
             </data>
         """)

commit a3f4f0f843191936b4de399e50425d670a1e9c83

[FIX] web_studio: don't touch abs xpath
This fixes a bug introduced in rev 2a425c7

Since absolute xpaths are, well, absolute, there
is absolutely (hehe) no need to manually modify
the expr, thus we only add templates// to
relative expr when needed.

web_studio/models/ir_ui_view.py
@@ -355,15 +355,14 @@ class View(models.Model):
         sibling elements (absolute identifier)
         """
         if node.get('name'):
-            node_str = '%s[@name=\'%s\']' % (node.tag, node.get('name'))
+            node_str = 'templates//' if self._is_templates_node(node) else ''
+            node_str += '%s[@name=\'%s\']' % (node.tag, node.get('name'))
         else:
             node_str = '%s[%s]' % (
                 node.tag,
                 len(list(node.itersiblings(tag=node.tag, preceding=True))) + 1
             )
 
-        if self._is_templates_node(node):
-            node_str = 'templates//' + node_str
 
         return node_str
 

web_studio/tests/test_view_normalization.py
@@ -6,6 +6,8 @@ from odoo.addons.web_studio.controllers.main import WebStudioController
 
 class TestViewNormalization(TransactionCase):
 
+    maxDiff = None
+
     def setUp(self):
         super(TestViewNormalization, self).setUp()
         _request_stack.push(self)
@@ -83,7 +85,6 @@ class TestViewNormalization(TransactionCase):
                                     <field name="mobile"/>
                                     <field name="state_id"/>
                                     <field name="image"/>
-                                    <field name="lang"/>
                                     <templates>
                                         <t t-name="kanban-box">
                                             <div class="oe_kanban_details">
@@ -684,6 +685,60 @@ class TestViewNormalization(TransactionCase):
               <xpath expr="//templates//field[@name='name']" position="after">
                 <field name="phone"/>
                 <field name="mobile"/>
+              </xpath>
+            </data>
+        """)
+
+    # adding kanban and template fields while using absolute xpaths
+    def test_view_normalization_22(self):
+        self._test_view_normalization("""
+            <data>
+              <xpath expr="//templates" position="before">
+                <field name="lang"/>
+              </xpath>
+              <xpath expr="//templates//div" position="inside">
+                <div class="o_dropdown_kanban dropdown">
+                            <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">
+                                <span class="fa fa-bars fa-lg"/>
+                            </a>
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                                <t t-if="widget.editable"><li><a type="edit">Edit</a></li></t>
+                                <t t-if="widget.deletable"><li><a type="delete">Delete</a></li></t>
+                                <li><ul class="oe_kanban_colorpicker" data-field="lang"/></li>
+                            </ul>
+                        </div>
+              </xpath>
+              <xpath expr="//templates//div" position="attributes">
+                <attribute name="color">lang</attribute>
+              </xpath>
+            </data>
+        """, """
+            <data>
+              <xpath expr="//field[@name='image']" position="after">
+                <field name="lang"/>
+              </xpath>
+              <xpath expr="//form[1]/sheet[1]/notebook[1]/page[1]/field[@name='child_ids']/kanban[1]/templates[1]/t[1]/div[1]" position="attributes">
+                <attribute name="color">lang</attribute>
+              </xpath>
+              <xpath expr="//templates//field[@name='name']" position="after">
+                <div class="o_dropdown_kanban dropdown">
+                  <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">
+                    <span class="fa fa-bars fa-lg"/>
+                  </a>
+                  <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                    <t t-if="widget.editable">
+                      <li>
+                        <a type="edit">Edit</a>
+                        <a type="delete">Delete</a>
+                        <ul class="oe_kanban_colorpicker" data-field="lang"/>
+                      </li>
+                    </t>
+                    <t t-if="widget.deletable">
+                      <li/>
+                    </t>
+                    <li/>
+                  </ul>
+                </div>
               </xpath>
             </data>
         """)