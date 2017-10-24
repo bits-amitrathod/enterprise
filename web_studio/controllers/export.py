bd376af667757e01bbda2f4bba7dd65f15112e51
Martin Geubelle <mge@odoo.com>
2017-10-24 14:06:03 +0200

    [FIX] web_studio: export custom reports
    This is a complement to odoo/enterprise@3da7f065.
    
    All fields linked to the model `ir.values` have been deleted so
    no need to specify `ir_values_id` for the exported reports.

--- web_studio/controllers/export.py
+++ web_studio/controllers/export.py
@@ -72,7 +72,6 @@ FIELDS_TO_EXPORT = {
 # list of relational fields to NOT export, by model
 FIELDS_NOT_TO_EXPORT = {
     'base.automation': ['trg_date_calendar_id'],
-    'ir.actions.report': ['ir_values_id'],
     'ir.actions.server': ['channel_ids', 'fields_lines', 'partner_ids'],
     'ir.filter': ['user_id'],
     'mail.template': ['attachment_ids', 'mail_server_id', 'ref_ir_value'],
