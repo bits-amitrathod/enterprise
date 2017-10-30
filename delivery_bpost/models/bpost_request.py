bits-amitrathod/enterprise

commit 0bf32cd8b14fbed11d981eb68aa147cbf05841e2

[FIX] delivery_bpost: prevent crashing on authentication errors

delivery_bpost/models/bpost_request.py
@@ -4,6 +4,7 @@ from binascii import a2b_base64
 import logging
 import re
 import requests
+from lxml import html
 from xml.etree import ElementTree as etree
 from werkzeug.urls import url_join
 
@@ -87,7 +88,13 @@ class BpostRequest():
         # Surprisingly, bpost does not require to send other data while asking for prices;
         # they simply return a price grid for all activated products for this account.
         code, response = self._send_request('rate', None, carrier)
-        xml_response = etree.fromstring(response)
+        if code != 201 and response:
+            # If the authentication fails, the server returns plain HTML instead of XML
+            error_page = html.fromstring(response)
+            error_message = error_page.body.text_content()
+            raise UserError(_("Authentication error -- wrong credentials\n(Detailed error: %s)") % error_message)
+        else:
+            xml_response = etree.fromstring(response)
 
         # Find price by product and country
         price = 0.0

commit 164d3c01abe7113ff1ec7d03208d3b03873c6cc0

[FIX] delivery_bpost: typo introduced in 0bf32cd8b14fbed11d981eb68aa147cbf05841e2

delivery_bpost/models/bpost_request.py
@@ -88,7 +88,7 @@ class BpostRequest():
         # Surprisingly, bpost does not require to send other data while asking for prices;
         # they simply return a price grid for all activated products for this account.
         code, response = self._send_request('rate', None, carrier)
-        if code != 201 and response:
+        if code == 401 and response:
             # If the authentication fails, the server returns plain HTML instead of XML
             error_page = html.fromstring(response)
             error_message = error_page.body.text_content()