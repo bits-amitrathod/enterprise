52bbb03a9ea3eb19ea048399701b134527f33cc8
Christophe Monniez <moc@odoo.com>
2017-11-07 17:06:49 +0100

    [IMP] currency_rate_live: separate test methods
    When the test for the currency rate update fails, it's difficult to find
    which currency provider failed. It's also difficult to know if the field
    update failed or the connection to the provider failed.
    This commit separate the tests with a test method for each provider.

--- currency_rate_live/tests/test_live_currency_update.py
+++ currency_rate_live/tests/test_live_currency_update.py
@@ -2,28 +2,42 @@ import unittest
 from odoo.tests.common import TransactionCase
 
 
+@unittest.skip("Currency rate live test disabled as it requires to contact external servers")
 class CurrencyTestCase(TransactionCase):
 
-    @unittest.skip("Currency rate live test disabled as it requires to contact external servers")
-    def test_live_currency_update(self):
-        company_ecb = self.env['res.company'].create({'name': 'TEST ECB', 'currency_provider': 'ecb'})
-        company_yah = self.env['res.company'].create({'name': 'TEST YAH', 'currency_provider': 'yahoo'})
-        company_fta = self.env['res.company'].create({'name': 'TEST FTA', 'currency_provider': 'fta'})
-        company_banxico = self.env['res.company'].create({'name': 'TEST BANXICO', 'currency_provider': 'banxico'})
+    def setUp(self):
+        super(CurrencyTestCase, self).setUp()
+        self.test_company = self.env['res.company'].create({'name': 'Test Company'})
+        # Each test will check the number of rates for USD
+        self.currency_usd = self.env.ref('base.USD')
+
+    def test_live_currency_update_ecb(self):
+        self.test_company.currency_provider = 'ecb'
+        rates_count = len(self.currency_usd.rate_ids)
+        res = self.test_company._update_currency_ecb()
+        self.assertTrue(res)
+        self.assertEqual(len(self.currency_usd.rate_ids), rates_count + 1)
+
+    def test_live_currency_update_yahoo(self):
+        self.test_company.currency_provider = 'yahoo'
+        rates_count = len(self.currency_usd.rate_ids)
+        res = self.test_company._update_currency_yahoo()
+        self.assertTrue(res)
+        self.assertEqual(len(self.currency_usd.rate_ids), rates_count + 1)
+
+    def test_live_currency_update_fta(self):
+        self.test_company.currency_provider = 'fta'
         # testing Swiss Federal Tax Administration requires that Franc Suisse can be found
         # which is not the case in runbot/demo data as l10n_ch is not always installed
         self.env.ref('base.CHF').write({'active': True})
+        rates_count = len(self.currency_usd.rate_ids)
+        res = self.test_company._update_currency_fta()
+        self.assertTrue(res)
+        self.assertEqual(len(self.currency_usd.rate_ids), rates_count + 1)
 
-        #check the number of rates for USD
-        self.currency_usd = self.env.ref('base.USD')
-        rates_number = len(self.currency_usd.rate_ids)
-
-        #get the live rate for all companies, each one with a different method
-        res_ecb = company_ecb._update_currency_ecb()
-        res_yah = company_yah._update_currency_yahoo()
-        res_fta = company_fta._update_currency_fta()
-        res_banxico = company_banxico._update_currency_banxico()
-
-        #Check that all companies call to _update_currency_rates() has created a new rate for the USD (only if the request was a success)
-        rates_number_again = len(self.currency_usd.rate_ids)
-        self.assertEqual(rates_number + int(res_ecb) + int(res_yah) + int(res_fta) + int(res_banxico), rates_number_again)
+    def test_live_currency_update_banxico(self):
+        self.test_company.currency_provider = 'banxico'
+        rates_count = len(self.currency_usd.rate_ids)
+        res = self.test_company._update_currency_banxico()
+        self.assertTrue(res)
+        self.assertEqual(len(self.currency_usd.rate_ids), rates_count + 1)
