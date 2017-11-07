46f4d13b2f5b43f2d0bbb9a1eae733e4cd22cccc
Richard Mathot <rim@odoo.com>
2017-11-07 14:17:40 +0100

    [IMP] timesheet_grid,web_grid: perfect onboarding
    Task:
    https://www.odoo.com/web?debug#id=38477&view_type=form&model=project.task&action=333&active_id=965&menu_id=4720

--- timesheet_grid/static/src/js/tour.js
+++ timesheet_grid/static/src/js/tour.js
@@ -1,5 +1,5 @@
-odoo.define('timesheet.tour', function(require) {
 "use strict";
+odoo.define('timesheet.tour', function(require) {
 
 var core = require('web.core');
 var tour = require('web_tour.tour');
@@ -10,13 +10,12 @@ tour.register('timesheet_tour', {
     url: "/web",
 }, [tour.STEPS.MENU_MORE, {
     trigger: '.o_app[data-menu-xmlid="hr_timesheet.timesheet_menu_root"], .oe_menu_toggler[data-menu-xmlid="hr_timesheet.timesheet_menu_root"]',
-    content: _t('Track your timesheets to invoice easily, or control costs. <i>It starts here.</i>'),
+    content: _t('Track the time spent on your projects. <i>It starts here.</i>'),
     position: 'bottom',
 }, {
     trigger: '.o_grid_button_add',
-    content: _t('Let\'s create your first timesheet entry.'),
-    position: 'right',
-    width: 200,
+    content: _t('Timesheets are done on tasks, click here to create your first one.'),
+    position: 'bottom',
 }, {
     trigger: ".modal-body .o_timesheet_tour_project_name",
     content: _t('Choose a <b>project name</b>. (e.g. name of a customer, or product)'),
@@ -33,15 +32,11 @@ tour.register('timesheet_tour', {
     },
 }, {
     trigger: '.o_grid_input',
-    content: _t('Set the number of hours done on this project, for every day of the week. (e.g. 1.5 or 1:30)'),
+    content: _t('Set the number of hours done on this task, for every day of the week. (e.g. 1.5 or 1:30)'),
     position: 'top',
     run: function (actions) {
         actions.text("4", this.$anchor);
     },
-}, {
-    trigger: '.grid_arrow_range[data-name="month"]',
-    content: _t('Easily switch to the monthly or daily view to control your time'),
-    position: 'right',
 }]);
 
 });
