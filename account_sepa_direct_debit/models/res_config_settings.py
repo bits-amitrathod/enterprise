03595ee41278c97b1c709dfed8266ce8cbefd0c9
Martin Trigaux <mat@odoo.com>
2017-11-08 13:01:14 +0100

    [FIX] account_sepa_direct_debit,timesheet_grid: correct English issues
    Was unclear or mistakes
    
    Courtesy of the translator team

--- account_sepa_direct_debit/models/res_config_settings.py
+++ account_sepa_direct_debit/models/res_config_settings.py
@@ -6,4 +6,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    sdd_creditor_identifier = fields.Char(related='company_id.sdd_creditor_identifier', string='Creditor identifier', help='Creditor identifier of your company withing SEPA scheme.')
+    sdd_creditor_identifier = fields.Char(related='company_id.sdd_creditor_identifier', string='Creditor identifier',
+        help='Creditor identifier of your company within SEPA scheme.')
