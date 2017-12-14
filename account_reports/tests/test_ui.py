7c2e12e360a4a43ffa5e3c0dbabe49de72f312e3
Aaron Bohy <aab@odoo.com>
2017-12-14 09:03:49 +0100

    [FIX] account_reports: tours should not be in assets_common
    Those tours are executed in the backend...

--- account_reports/tests/test_ui.py
+++ account_reports/tests/test_ui.py
@@ -3,9 +3,10 @@
 
 import odoo.tests
 
+
 @odoo.tests.common.at_install(False)
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
     def test_ui(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('account_reports_widgets', 'test')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_reports_widgets", login='admin')
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('account_followup_reports_widgets', 'test')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_followup_reports_widgets", login='admin')
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('account_reports_widgets')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_reports_widgets.ready", login='admin')
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('account_followup_reports_widgets')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_followup_reports_widgets.ready", login='admin')
