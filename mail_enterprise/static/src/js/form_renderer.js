76ba043255e62b4a34501177b24dfaeccf11e4f4
Siddharth Gajjar <sga@odoo.com>
2018-02-02 11:24:05 +0530

    [ADD] mail_enterprise:  display attachment preview in form view
    This commit allows to display a preview of the chatter attachment on side of
    form view if that form view in opened in large displays.
    Note that it will only preview pdf and image attachments.
    
    How it works:
    * to avoid extra rpc call to fetch attachments, we are using attachment data
      available in the chatter
    * we triggered attachment data from thread_field widget, and this trigger caught
      by FormRenderer and then renders attachment preview
    * another issue fixed by this approach is, when new attachment added from the
      chatter, it was not displaying attachment in preview because adding new message
      in chatter does not reload form view state
    * for preview we exchange the position of chatter and attachment preview node.
      With this we can manage separate scroll bars for form sheet+chatter and
      attachment preview
    
    To use this, simply add a div with class o_attachment_preview in form view arch
    to display attachment preview. Some options can be passed to change its behavior.
    
    Options:
        types: ['image', 'pdf']
        order: 'asc' or 'desc'
    
    e.g. if you want to display only pdf type atttachment and the latest one then use
    <div class="o_attachment_preview" options="{'types':['pdf'], 'order': 'desc'}"/>
    
    Task 40663
    
    Co-authored-by: Martin Geubelle <mge@odoo.com>

---
+++ mail_enterprise/static/src/js/form_renderer.js
@@ -0,0 +1,170 @@
+odoo.define('mail_enterprise.form_renderer', function (require) {
+"use strict";
+
+var config = require('web.config');
+var core = require('web.core');
+var pyeval = require('web.pyeval');
+var FormRenderer = require('web.FormRenderer');
+
+var QWeb = core.qweb;
+
+/**
+ * Display attachment preview on side of form view for large screen devices.
+ *
+ * To use this simply add div with class o_attachment_preview in format
+ *     <div class="o_attachment_preview"/>
+ *
+ * Some options can be passed to change its behavior:
+ *     types: ['image', 'pdf']
+ *     order: 'asc' or 'desc'
+ *
+ * For example, if you want to display only pdf type attachment and the latest
+ * one then use:
+ *     <div class="o_attachment_preview" options="{'types': ['pdf'], 'order': 'desc'}"/>
+**/
+
+FormRenderer.include({
+    custom_events: _.extend({}, FormRenderer.prototype.custom_events, {
+        preview_attachment: '_onAttachmentPreview'
+    }),
+
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+
+        this.$attachmentPreview = undefined;
+        this.attachmentPreviewID = undefined;
+        this.attachmentPreviewResID = undefined;
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Interchange the position of the chatter and the attachment preview.
+     *
+     * @private
+     * @param {boolean} enablePreview
+     */
+    _interchangeChatter: function (enablePreview) {
+        if (config.device.size_class < config.device.SIZES.LG) {
+            return;
+        }
+        if (!this.$attachmentPreview) {
+            return;
+        }
+        var $sheet = this.$('.o_form_sheet_bg');
+
+        if (enablePreview) {
+            this.chatter.$el.appendTo($sheet);
+            this.$attachmentPreview.insertAfter($sheet);
+        } else {
+            this.chatter.$el.insertAfter($sheet);
+            this.$attachmentPreview.appendTo($sheet);
+        }
+    },
+    /**
+     * Overrides the function that renders the nodes to return the preview's $el
+     * for the `o_attachment_preview` div node.
+     *
+     * @private
+     * @override
+     */
+    _renderNode: function (node) {
+        if (node.tag === 'div' && node.attrs.class === 'o_attachment_preview') {
+            this.attachmentPreviewID = undefined;
+            this.$attachmentPreview = $('<div>', {class: 'o_attachment_preview'});
+            this._handleAttributes(this.$attachmentPreview, node);
+            this._registerModifiers(node, this.state, this.$attachmentPreview);
+            if (node.attrs.options) {
+                this.$attachmentPreview.data(pyeval.py_eval(node.attrs.options));
+            }
+            if (this.attachmentPreviewWidth) {
+                this.$attachmentPreview.css('width', this.attachmentPreviewWidth);
+            }
+            return this.$attachmentPreview;
+        } else {
+            return this._super.apply(this, arguments);
+        }
+    },
+    /**
+     * Overrides the function to interchange the chatter and the preview once
+     * the chatter is in the dom.
+     *
+     * @private
+     * @override
+     */
+    _renderView: function () {
+        var self = this;
+        return this._super.apply(this, arguments).then(function () {
+            // for cached messages, `preview_attachment` will be triggered
+            // before the view rendering where the chatter is replaced ; in this
+            // case, we need to interchange its position if needed
+            var enablePreview = self.attachmentPreviewResID &&
+                self.attachmentPreviewResID === self.state.res_id &&
+                self.$attachmentPreview &&
+                !self.$attachmentPreview.hasClass('o_invisible_modifier');
+            self._interchangeChatter(enablePreview);
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Triggered from the mail chatter, send attachments data for preview
+     *
+     * @private
+     * @param {OdooEvent} ev
+     */
+    _onAttachmentPreview: function (ev) {
+        if (config.device.size_class < config.device.SIZES.LG) {
+            return;
+        }
+        if (!this.$attachmentPreview) {
+            return;
+        }
+        var self = this;
+        var options = _.defaults(this.$attachmentPreview.data(), {
+            types: ['pdf', 'image'],
+            order: 'asc'
+        });
+        var attachments = ev.data.attachments.slice();  // clone array
+        attachments = _.filter(attachments, function (attachment) {
+            var match = attachment.mimetype.match("(image|pdf)");
+            attachment.type = match ? match[0] : false;
+            return attachment.type;
+        });
+        if (options.order === 'desc') {
+            attachments.reverse();
+        }
+        var attachment = _.find(attachments, function (attachment) {
+            return _.contains(options.types, attachment.type);
+        });
+        if (attachment && attachment.id !== this.attachmentPreviewID) {
+            this.attachmentPreviewID = attachment.id;
+            this.attachmentPreviewResID = this.state.res_id;
+            // need to remove and append because resizer adds its element
+            this.$attachmentPreview.find('.o_attachment_preview_container').remove();
+            this.$attachmentPreview.append(QWeb.render('AttachmentPreview', {
+                attachment: attachment,
+            }));
+            this.$attachmentPreview.resizable({
+                handles: 'w',
+                minWidth: 400,
+                maxWidth: 900,
+                resize: function (event, ui) {
+                    self.attachmentPreviewWidth = ui.size.width;
+                },
+            });
+            this._interchangeChatter(!this.$attachmentPreview.hasClass('o_invisible_modifier'));
+        }
+    },
+
+});
+
+});
---
+++ mail_enterprise/static/tests/attachment_preview_tests.js
@@ -0,0 +1,184 @@
+odoo.define('mail_enterprise.attachment_side_preview_tests', function (require) {
+"use strict";
+
+var ChatManager = require('mail.ChatManager');
+var mailTestUtils = require('mail.testUtils');
+var config = require('web.config');
+var FormView = require('web.FormView');
+var testUtils = require('web.test_utils');
+
+var createBusService = mailTestUtils.createBusService;
+var createView = testUtils.createView;
+
+QUnit.module('MailAttachmentOnSide', {
+
+    beforeEach: function () {
+        this.data = {
+            partner: {
+                fields: {
+                    display_name: { string: "Displayed name", type: "char" },
+                    foo: {string: "Foo", type: "char", default: "My little Foo Value"},
+                    message_ids: {
+                        string: "messages",
+                        type: "one2many",
+                        relation: 'mail.message',
+                        relation_field: "res_id",
+                    },
+                },
+                records: [{
+                    id: 2,
+                    display_name: "first partner",
+                    foo: "HELLO",
+                    message_ids: [],
+                }]
+            }
+        };
+    }
+
+}, function () {
+
+    QUnit.test('Attachment on side', function (assert) {
+        assert.expect(7);
+
+        var count = 0;
+        this.data.partner.records[0].message_ids = [1];
+        var messages = [{
+            attachment_ids: [{
+                filename: 'image1.jpg',
+                id:1,
+                mimetype: 'image/jpeg',
+                name: 'Test Image 1',
+                url: '/web/content/1?download=true',
+            }],
+            author_id: ["1", "Kamlesh Sulochan"],
+            body: "Attachment viewer test",
+            date: "2016-12-20 09:35:40",
+            displayed_author: "Kamlesh Sulochan",
+            id: 1,
+            is_note: false,
+            is_discussion: true,
+            is_starred: false,
+            model: 'partner',
+            res_id: 2,
+        }];
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch: '<form string="Partners">' +
+                    '<sheet>' +
+                        '<field name="foo"/>' +
+                    '</sheet>' +
+                    '<div class="o_attachment_preview" options="{\'order\':\'desc\'}"></div>' +
+                    '<div class="oe_chatter">' +
+                        '<field name="message_ids" widget="mail_thread" options="{\'display_log_button\': True}"/>' +
+                    '</div>' +
+                '</form>',
+            res_id: 2,
+            config: {
+                device: {
+                    size_class: config.device.SIZES.LG,
+                },
+            },
+            mockRPC: function (route, args) {
+                if (args.method === 'message_format') {
+                    var requested_msgs = _.filter(messages, function (msg) {
+                        return _.contains(args.args[0], msg.id);
+                    });
+                    return $.when(requested_msgs);
+                }
+                if (args.method === 'message_get_suggested_recipients') {
+                    return $.when({2: []});
+                }
+                if (_.str.contains(route, '/web/static/lib/pdfjs/web/viewer.html')){
+                    var canvas = document.createElement('canvas');
+                    return $.when(canvas.toDataURL());
+                }
+                if (args.method === 'message_post') {
+                    messages.push({
+                        attachment_ids: [{
+                            filename: 'invoice.pdf',
+                            id: 2,
+                            mimetype: 'application/pdf',
+                            name: 'INV007/2018',
+                            url: '/web/content/1?download=true',
+                        }],
+                        author_id: ["5", "Bhallaldeva"],
+                        body: args.kwargs.body,
+                        date: "2016-12-20 10:35:40",
+                        displayed_author: "Bhallaldeva",
+                        id: 5,
+                        is_note: false,
+                        is_discussion: true,
+                        is_starred: false,
+                        model: 'partner',
+                        res_id: 2,
+                    });
+                    return $.when(5);
+                }
+                return this._super.apply(this, arguments);
+            },
+            intercepts: {
+                preview_attachment: function (event) {
+                    if (count === 0) {
+                        assert.strictEqual(event.data.attachments[0].id, 1, "Chatter should trigger existing image attachment data for preview");
+                    } else if (count === 1) {
+                        assert.strictEqual(event.data.attachments[1].id, 2, "Chatter should trigger new posted pdf attachment data for preview");
+                    }
+                    count++;
+                },
+            },
+            services: [ChatManager, createBusService()],
+        });
+
+        assert.strictEqual(form.$('.o_attachment_preview_img > img').length, 1, "There should be an image for attachment preview");
+        assert.strictEqual(form.$('.o_form_sheet_bg > .o_chatter').length, 1, "Chatter should moved inside sheet");
+        assert.strictEqual(form.$('.o_form_sheet_bg + .o_attachment_preview').length, 1, "Attachment preview should be next sibling to .o_form_sheet_bg");
+
+        // send a message with attached PDF file
+        form.$('.o_chatter_button_new_message').click();
+        form.$('.oe_chatter .o_composer_text_field:first()').text("Attached the pdf file");
+        form.$('.oe_chatter .o_composer_button_send').click();
+
+        assert.strictEqual(form.$('.o_attachment_preview_img > img').length, 0, "Preview image should be removed");
+        assert.strictEqual(form.$('.o_attachment_preview_container > iframe').length, 1, "There should be iframe for pdf viewer");
+        form.destroy();
+    });
+
+    QUnit.test('Attachment on side on new record', function (assert) {
+        assert.expect(3);
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch: '<form string="Partners">' +
+                    '<sheet>' +
+                        '<field name="foo"/>' +
+                    '</sheet>' +
+                    '<div class="o_attachment_preview" options="{\'order\':\'desc\'}"></div>' +
+                    '<div class="oe_chatter">' +
+                        '<field name="message_ids" widget="mail_thread" options="{\'display_log_button\': True}"/>' +
+                    '</div>' +
+                '</form>',
+            config: {
+                device: {
+                    size_class: config.device.SIZES.LG,
+                },
+            },
+            services: [ChatManager, createBusService()],
+        });
+
+        assert.strictEqual(form.$('.o_form_sheet_bg .o_attachment_preview').length, 1, "the preview should not be displayed");
+        assert.strictEqual(form.$('.o_form_sheet_bg .o_attachment_preview').children().length, 0, "the preview should be empty");
+        assert.strictEqual(form.$('.o_form_sheet_bg + .o_chatter').length, 1, "chatter should not have been moved");
+
+        form.destroy();
+    });
+
+
+});
+
+
+});
