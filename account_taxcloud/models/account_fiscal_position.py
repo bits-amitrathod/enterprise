5deeae056d8a729162ffc195a71b81e9dd4d02d3
Yannick Tivisse <yti@odoo.com>
2018-02-19 11:05:21 +0100

    [IMP] taxcloud: Remove useless fields
    Purpose
    =======
    
    Since e0a97bd we don't magic guess the taxes from what's stored on the fiscal position.
    We don't store taxcloud results on the fiscal position too.
    
    As this change has been done in 10.0, we couldn't remove the fields definition on the fiscal position models.
    
    Specification
    =============
    
    Remove all the unused fields definitions and views.

--- account_taxcloud/models/account_fiscal_position.py
+++ account_taxcloud/models/account_fiscal_position.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
-
+from odoo import fields, models
 
 
 class AccountFiscalPositionTemplate(models.Model):
@@ -14,32 +13,3 @@ class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
     is_taxcloud = fields.Boolean(string='Use TaxCloud API')
-
-    @api.multi
-    def map_tax(self, taxes, product=None, partner=None):
-
-        # Do not put taxes if the fiscal position is using taxcloud
-        # It will be compute on the SO or Inv confirmation
-        if self.is_taxcloud:
-            return self.env['account.tax'].browse()
-        else:
-            return super(AccountFiscalPosition, self).map_tax(taxes, product=product, partner=partner)
-        tic_category = self.env['product.tic.category']
-
-
-
-class AccountFiscalPositionTaxTemplate(models.Model):
-    _inherit = 'account.fiscal.position.tax.template'
-
-    tic_category_ids = fields.Many2many('product.tic.category', string="TIC Category")
-    state_ids = fields.Many2many('res.country.state', string="Federal States")
-    zip_codes = fields.Char("Zip")
-
-
-class AccountFiscalPositionTax(models.Model):
-    _inherit = 'account.fiscal.position.tax'
-
-    # TO REMOVE IN MASTER
-    tic_category_ids = fields.Many2many('product.tic.category', string="TIC Category")
-    state_ids = fields.Many2many('res.country.state', string="Federal States")
-    zip_codes = fields.Char("Zip")
