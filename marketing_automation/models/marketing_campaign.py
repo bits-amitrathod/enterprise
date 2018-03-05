bf37b0f1056ce4ab2fb412a56636f35fda7aec3d
Christophe Matthieu <chm@odoo.com>
2018-03-05 15:15:18 +0100

    [FIX] marketing_automation: allow to Generate Participants
    Must create the browse record and read.
    
    Issue:
    Create a Campaigns with target model equal "Lead/Opportunity" and Unique
    Field equal "Email (crm.lead)"
    Click on "Start" then click on "Generate Participants"
    Generate log warning fields '1', '56'... does not exists on crm.lead.
    
    opw-1819218

--- marketing_automation/models/marketing_campaign.py
+++ marketing_automation/models/marketing_campaign.py
@@ -178,7 +178,7 @@ class MarketingCampaign(models.Model):
             # check uniqueness based on 'unique_field_id' field
             if campaign.unique_field_id and campaign.unique_field_id.name != 'id':
                 # Don't use browse maybe record is deleted
-                existing_records = RecordModel.read(existing_rec_ids, [campaign.unique_field_id.name])
+                existing_records = RecordModel.browse(existing_rec_ids).read([campaign.unique_field_id.name])
                 unique_field_vals = list({rec[campaign.unique_field_id.name] for rec in existing_records})
                 unique_domain = [(campaign.unique_field_id.name, 'not in', unique_field_vals)]
                 record_domain = expression.AND([unique_domain, record_domain])
