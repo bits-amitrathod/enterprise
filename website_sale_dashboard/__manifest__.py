aed7fff9b23e37c6085c17092186a538054553cb
Mathieu Duckerts-Antoine <dam@odoo.com>
2018-03-30 08:15:18 +0200

    [ADD] website_sale_dashboard: new website dashboard
    This rev. adds a new module defining a Dashboard view for
    website_sale (i.e. when both website and sale modules are
    installed). This dashboard is the main view of the website app.

---
+++ website_sale_dashboard/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Website Sale Dashboard',
+    'category': 'Website',
+    'sequence': 55,
+    'summary': 'Get a new dashboard view in the Website App',
+    'version': '1.0',
+    'description': """
+
+This module adds a new dashboard view in the Website application.
+This new type of view contains some basic statistics, a graph, and
+a pivot subview that allow you to get a quick overview of your online sales.
+It also provides new tools to analyse your data
+
+""",
+    'depends': ['website_sale', 'web_dashboard'],
+    'data': [
+        'views/dashboard_view.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'auto_install': True,
+}
---
+++ website_sale_dashboard/models/sale_report.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    order_id = fields.Many2one(string="Order ID", comodel_name='sale.report', readonly=True)
+    is_abandoned_cart = fields.Boolean(string="Abandoned Cart", readonly=True)
+
+    def _select(self):
+        select_term = """
+            , s.id as order_id
+            , s.date_order <= (timezone('utc', now()) - ((COALESCE(config.value, '1.0') || ' hour')::INTERVAL))
+            AND team.team_type = 'website'
+            AND s.state = 'draft'
+            AND s.partner_id != %s
+            AS is_abandoned_cart
+        """ % self.env.ref('base.public_partner').id
+        return super(SaleReport, self)._select() + select_term
+
+    def _from(self):
+        from_term = """
+            left join crm_team team on team.id = s.team_id
+            left join ir_config_parameter config on config.key = 'website_sale.cart_abandoned_delay'
+        """
+        return super(SaleReport, self)._from() + from_term
+
+    def _group_by(self):
+        group_by_term = """
+            , s.id
+            , config.value
+            , team.team_type
+            """
+        return super(SaleReport, self)._group_by() + group_by_term
