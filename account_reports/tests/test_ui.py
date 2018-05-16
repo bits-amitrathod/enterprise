ad88c9a45a17a034d12fed0575c14917bf4cc25f
Jeremy Kersten <jke@odoo.com>
2018-05-16 12:46:22 +0200

    [FIX] account_reports, account_accountant: fix fwd-port 1218538
    Remove leftover call to test 'account_reports_widgets'
    
    Commit ccb4d82 move the test from account_reports to account_accountant, but during fwd port
    the call from account_accountant has been left by error.
    
    In case you install account_reports without account_accountant (eg: design-theme branch), you
    will call a tour that is not registered and branch will be red.

--- account_reports/tests/test_ui.py
+++ account_reports/tests/test_ui.py
@@ -7,5 +7,4 @@ import odoo.tests
 @odoo.tests.tagged('post_install','-at_install')
 class TestUi(odoo.tests.HttpCase):
     def test_ui(self):
-        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('account_reports_widgets')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_reports_widgets.ready", login='admin')
         self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('account_followup_reports_widgets')", "odoo.__DEBUG__.services['web_tour.tour'].tours.account_followup_reports_widgets.ready", login='admin')
