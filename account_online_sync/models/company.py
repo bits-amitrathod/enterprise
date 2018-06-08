2374f6e8403831d78f6f7ab2523257c5f2c89f89
Olivier Colson <oco@odoo.com>
2018-06-08 22:31:27 +0200

    [IMP] account_online_sync: setup bar: bank account setup: Display error message when trying to setup bank account when no bank journal exists, instead of creating one by default
    This makes sense, since a bank journal is always created by the installation of the CoA. So, this situation means no CoA is installed yet.

--- account_online_sync/models/company.py
+++ account_online_sync/models/company.py
@@ -2,6 +2,8 @@
 
 from odoo import api, models, fields, _
 
+from odoo.exceptions import UserError
+
 class ResCompany(models.Model):
     _inherit = "res.company"
 
@@ -9,16 +11,13 @@ class ResCompany(models.Model):
     def setting_init_bank_account_action(self):
         """ Setup bar function, overridden to call the online synchronization wizard
         allowing to setup bank account instead of the default wizard used in community.
-        If no bank journal exists yet, we create one first."""
+        If no bank journal exists yet, we trigger an error message asking to install
+        a CoA, which will create the journal."""
         company = self.env.user.company_id
 
         bank_journal = self.env['account.journal'].search([('company_id','=', company.id), ('type','=','bank')], limit=1)
 
         if not bank_journal:
-            bank_journal = self.env['account.journal'].create({
-                'company_id': company.id,
-                'type': 'bank',
-                'name': _('Bank Journal'),
-            })
+            raise UserError(_("No bank journal could be found! Please install a chart of accounts first, in order to create one."))
 
         return bank_journal.action_choose_institution()
