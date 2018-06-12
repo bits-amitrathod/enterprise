bits-amitrathod/enterprise

commit 39e24cfaae2f6928409e4852d709850d68d1f3cd

[FIX] account_reports: do not display full_reconcile liquidity lines
Make one Payment
Reverse the entry of this payment.

Before this commit, only the lines in the receivable were reconciled.
The lines in the liquidity were'nt, leaving the two entries appearing
in the reconciliation report

After this commit, the four lines are reconciled two by two, even in the liquidity
account.
The report do not display those lines anymore.

OPW 1816641

ref odoo/odoo#25026
closes #2234

account_reports/models/account_bank_reconciliation_report.py
@@ -91,7 +91,8 @@ class account_bank_reconciliation_report(models.AbstractModel):
         # Un-reconcilied bank statement lines
         move_lines = self.env['account.move.line'].search([('move_id.journal_id', '=', self.env.context['journal_id'].id),
                                                            '|', ('move_id.statement_line_id', '=', False), ('move_id.statement_line_id.date', '>', self.env.context['date_to']),
-                                                           ('user_type_id.type', '!=', 'liquidity'),
+                                                           ('user_type_id.type', '=', 'liquidity'),
+                                                           ('full_reconcile_id', '=', False),
                                                            ('date', '<=', self.env.context['date_to']),
                                                            ('company_id', 'in', self.env.context['company_ids'])])
         unrec_tot = 0

commit 7b046547417bc4dd10fffcbee01dbd017ac2f010

[FIX] account_reports: bank reconciliation report: use correct signs to list unreconciled payments
This is actually a fix of an error introduced by commit https://github.com/odoo/enterprise/commit/39e24cfaae2f6928409e4852d709850d68d1f3cd . This commit changed the domain when searching for payment move lines, so that we only consider the lines made on accounts of type 'bank'. But doing so, we ended out selecting the lines balancing the lines we did select before, causing a sign inversion on the balance.

account_reports/models/account_bank_reconciliation_report.py
@@ -107,10 +107,10 @@ class account_bank_reconciliation_report(models.AbstractModel):
                     'action': line.get_model_id_and_name(),
                     'name': line.name,
                     'footnotes': self.env.context['context_id']._get_footnotes('move_line_id', self.line_number),
-                    'columns': [line.date, line.ref, self._format(line.amount_currency if use_foreign_currency else line.balance)],
+                    'columns': [line.date, line.ref, self._format(-(line.amount_currency if use_foreign_currency else line.balance))],
                     'level': 1,
                 })
-                unrec_tot += line.amount_currency if use_foreign_currency else line.balance
+                unrec_tot -= line.amount_currency if use_foreign_currency else line.balance
             if unrec_tot > 0:
                 title = _("Plus Unreconciled Payments")
             else: