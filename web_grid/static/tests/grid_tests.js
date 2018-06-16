bits-amitrathod/enterprise

commit b3c5819b042a9bc5881095582005d95c050961b8

[FIX] web_grid: display field values in groupby by a non-relational field
When grouping by a non-relational field in a grid view (e.g. by name),
the row title would only show one letter of the field.
This is because relational field are given in the form of a couple (id, value)
whereas non-relational fields are simply given as value.
The code simply extracted the value by accessing v[1], which sadly returned "a"
for "name".
We now check the type to always return the correct value.

opw 1854736

web_grid/static/src/js/grid_renderer.js
@@ -122,6 +122,22 @@ return AbstractRenderer.extend({
     },
     /**
      * @private
+     * @param {Array[id, value] or value}
+     * @returns value
+     */
+    _field2label: function (value_to_display, field_type) {
+        if (!value_to_display){
+            return _t('Unknown');
+        }
+        if (["many2one", "many2many", "one2many"].indexOf(field_type) > -1) {
+            return value_to_display[1];
+        }
+        else {
+            return value_to_display;
+        }
+    },
+    /**
+     * @private
      * @returns {Deferred}
      */
     _render: function () {
@@ -246,7 +262,8 @@ return AbstractRenderer.extend({
             for (var i = 0; i < groupFields.length; i++) {
                 var row_field = groupFields[i];
                 var value = rows[rowIndex].values[row_field];
-                rowValues.push(value);
+                var field_type = self.fields[row_field].type;
+                rowValues.push(self._field2label(value, field_type));
             }
             var rowKey = _.map(rowValues, function (v) {
                 return v[0];
@@ -254,9 +271,8 @@ return AbstractRenderer.extend({
 
             return h('tr', {key: rowKey}, [
                 h('th', {}, [
-                    h('div', _.map(rowValues, function (v) {
-                        var label = v ? v[1] : _t('Unknown');
-                        var klass = v ? '' : 'o_grid_text_muted';
+                    h('div', _.map(rowValues, function (label) {
+                        var klass = label !== _t('Unknown') ? '' : 'o_grid_text_muted';
                         return h('div', {attrs: {title: label, class: klass}}, label);
                     }))
                 ])

web_grid/static/tests/grid_tests.js
@@ -263,6 +263,37 @@ QUnit.module('Views', {
         });
     });
 
+    QUnit.test('group by non-relational field', function (assert) {
+        assert.expect(1);
+        var done = assert.async();
+        var nbReadGridDomain = 0;
+        var nbReadGroup = 0;
+        var nbReadGrid = 0;
+
+        var grid = createView({
+            View: GridView,
+            model: 'analytic.line',
+            data: this.data,
+            arch: this.arch,
+            currentDate: "2017-01-25",
+            mockRPC: function (route, args) {
+                return this._super.apply(this, arguments);
+            },
+        });
+
+        return concurrency.delay(0).then(function () {
+            grid.update({groupBy: ["date"]});
+            debugger;
+            assert.strictEqual(grid.$('tbody th:first').text(),
+                               "January 2017",
+                               "Should be equal.");
+            return concurrency.delay(0);
+        }).then(function () {
+            grid.destroy();
+            done();
+        });
+    });
+
     QUnit.test('create analytic lines', function (assert) {
         assert.expect(7);
         var done = assert.async();

commit 49d3db34ea412dfab0fe069c45ee6e2f55ef5ce0

[FIX] web_grid: fill correct data to avoid snabbdom library crash
Commit:
https://github.com/odoo/enterprise/commit/b3c5819b042a9bc5881095582005d95c050961b8
Introduced a test to correctly handle non-relational fields.
However it moved up the conversion of the list of fields into a list of labels,
whereas the logic of the code expected that list to still be a list of fields.

opw 1858896, 1858663

web_grid/static/src/js/grid_renderer.js
@@ -259,15 +259,15 @@ return AbstractRenderer.extend({
         var self = this;
         return _.map(grid, function (row, rowIndex) {
             var rowValues = [];
+            var rowKeys = [];
             for (var i = 0; i < groupFields.length; i++) {
                 var row_field = groupFields[i];
                 var value = rows[rowIndex].values[row_field];
                 var field_type = self.fields[row_field].type;
+                rowKeys.push(value[0]);
                 rowValues.push(self._field2label(value, field_type));
             }
-            var rowKey = _.map(rowValues, function (v) {
-                return v[0];
-            }).join('|');
+            var rowKey = rowKeys.join('|');
 
             return h('tr', {key: rowKey}, [
                 h('th', {}, [

web_grid/static/tests/grid_tests.js
@@ -263,12 +263,67 @@ QUnit.module('Views', {
         });
     });
 
+    QUnit.test('DOM keys are unique', function (assert) {
+        /*Snabbdom, the virtual dom libraries, use keys to distinguish similar
+        elements (typically grid rows).
+        If these keys are not unique, and there are rows that are repeated,
+        the library might crash upon comparing the elements to refresh.
+        */
+
+        assert.expect(8);
+        var done = assert.async();
+
+        var line_records = [
+                {id: 12, project_id: 142, date: "2017-01-17", unit_amount: 0},
+                {id: 1, project_id: 31, date: "2017-01-24", unit_amount: 2.5},
+                {id: 3, project_id: 143, date: "2017-01-25", unit_amount: 5.5},
+                {id: 2, project_id: 33, date: "2017-01-25", unit_amount: 2},
+                {id: 4, project_id: 143, date: "2017-01-18", unit_amount: 0},
+                {id: 5, project_id: 142, date: "2017-01-18", unit_amount: 0},
+                {id: 10, project_id: 31, date: "2017-01-18", unit_amount: 0},
+                {id: 22, project_id: 33, date: "2017-01-19", unit_amount: 0},
+                {id: 21, project_id: 99, date: "2017-01-19", unit_amount: 0},
+        ];
+        var project_records = [
+                {id: 31, display_name: "Rem"},
+                {id: 33, display_name: "Rer"},
+                {id: 142, display_name: "Sas"},
+                {id: 143, display_name: "Sassy"},
+                {id: 99, display_name: "Sar"},
+        ];
+        this.data.project.records = project_records;
+        this.data['analytic.line'].records = line_records;
+
+        var grid = createView({
+            View: GridView,
+            model: 'analytic.line',
+            data: this.data,
+            arch: this.arch,
+            currentDate: "2017-01-25",
+        });
+
+        return concurrency.delay(0).then(function () {
+            assert.strictEqual(grid.$('tbody th:first').text(), "RerUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(1)').text(), "SassyUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(2)').text(), "RemUnknown", "Should be equal.");
+
+            grid.$buttons.find('button.grid_arrow_previous').click();
+            return concurrency.delay(0);
+        }).then(function () {
+            assert.strictEqual(grid.$('tbody th:first').text(), "SarUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(1)').text(), "RerUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(2)').text(), "RemUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(3)').text(), "SassyUnknown", "Should be equal.");
+            assert.strictEqual(grid.$('tbody th:eq(4)').text(), "SasUnknown", "Should be equal.");
+
+            grid.destroy();
+            done();
+        });
+    });
+
     QUnit.test('group by non-relational field', function (assert) {
         assert.expect(1);
         var done = assert.async();
-        var nbReadGridDomain = 0;
-        var nbReadGroup = 0;
-        var nbReadGrid = 0;
 
         var grid = createView({
             View: GridView,
@@ -283,7 +338,6 @@ QUnit.module('Views', {
 
         return concurrency.delay(0).then(function () {
             grid.update({groupBy: ["date"]});
-            debugger;
             assert.strictEqual(grid.$('tbody th:first').text(),
                                "January 2017",
                                "Should be equal.");