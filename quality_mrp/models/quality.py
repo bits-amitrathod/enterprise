fdc03302538b6541e0b4379c73dac83c9cb62671
Goffin Simon <sig@odoo.com>
2018-06-21 10:25:31 +0200

    [FIX] quality_mrp: Wrong UOM on workorder with lot
    With a BOM, the uom of a BOM line can be different of the uom of the product
    The qty_done is computed in function _create_checks on model 'mrp.workorder' with the
    uom of its relative workorder's move_raw_ids. So the component_uom_id must be in this
    uom too. As the move_line_id is computed in function _next on model 'mrp.workorder' with
    its relative workorder's move_raw_ids, the uom of move_line_id can be used.
    
    opw:1858002

--- quality_mrp/models/quality.py
+++ quality_mrp/models/quality.py
@@ -83,7 +83,7 @@ class QualityCheck(models.Model):
     # For components registration
     parent_id = fields.Many2one('quality.check', 'Parent Quality Check')
     component_id = fields.Many2one('product.product', 'Component')
-    component_uom_id = fields.Many2one(related='component_id.uom_id', readonly=True)
+    component_uom_id = fields.Many2one(related='move_line_id.product_uom_id', readonly=True)
     move_line_id = fields.Many2one('stock.move.line', 'Move Line')
     qty_done = fields.Float('Done', default=1.0, digits=dp.get_precision('Product Unit of Measure'))
     lot_id = fields.Many2one('stock.production.lot', 'Lot')
