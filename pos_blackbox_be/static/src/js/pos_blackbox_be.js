73a859218b71124b46bc2ddd244845a15fa3ce2a
Martin Trigaux <mat@odoo.com>
2018-07-12 10:04:16 +0000

    [IMP] pos_blackbox_be: update version number
    Correct the xpath on version number to be correctly displayed

--- pos_blackbox_be/static/src/js/pos_blackbox_be.js
+++ pos_blackbox_be/static/src/js/pos_blackbox_be.js
@@ -1066,7 +1066,7 @@ can no longer be modified. Please create a new line with eg. a negative quantity
             var self = this;
             return posmodel_super.connect_to_proxy.apply(this, arguments).then(function () {
                 self.proxy.message('request_serial', {}, {timeout: 5000}).then(function (response) {
-                    if (! response || "BODO001" + response.toUpperCase() != self.config.blackbox_pos_production_id.toUpperCase()) {
+                    if (! response || "BODO002" + response.toUpperCase() != self.config.blackbox_pos_production_id.toUpperCase()) {
                         self.proxy._show_could_not_connect_error();
                     } else {
                         self.chrome.ready.then(function () {
@@ -1106,7 +1106,7 @@ can no longer be modified. Please create a new line with eg. a negative quantity
                     order.blackbox_vsc_identification_number = parsed_response.vsc_identification_number;
                     order.blackbox_unique_fdm_production_number = parsed_response.fdm_unique_production_number;
                     order.blackbox_plu_hash = self._prepare_hash_for_ticket(packet.fields[packet.fields.length - 1].content);
-                    order.blackbox_pos_version = "Odoo " + self.version.server_version + "1805BE_FDM";
+                    order.blackbox_pos_version = "Odoo " + self.version.server_version + "1807BE_FDM";
                     order.blackbox_pos_production_id = self.config.blackbox_pos_production_id;
                     order.blackbox_terminal_id = self.blackbox_terminal_id;
 
