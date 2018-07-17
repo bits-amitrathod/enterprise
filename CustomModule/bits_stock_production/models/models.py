7d473b199e92c395a13234de03410607f3335790
avadhoot12 <avadhootkharpe@benchmarkitsolutions.com>
2018-07-17 17:36:00 +0530

    scrap schedular and stock production added.

---
+++ CustomModule/bits_stock_production/models/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import datetime
+
+class bitsStockProduction(models.Model):
+     _inherit = 'stock.production.lot'
+     _name = 'stock.production.lot'
+
+
