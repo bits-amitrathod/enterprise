bits-amitrathod/enterprise

commit 979b2e2f43e1af70d9af2dc1013c60cdf180d5e1

[IMP] l10n_mx_reports: Non-Creditable taxes on DIOT Report
Was PR #2273. Courtesy of Vauxoo

l10n_mx_reports/models/account_diot.py
@@ -27,7 +27,8 @@ class MxReportPartnerLedger(models.AbstractModel):
             {'name': _('Importation 16%'), 'class': 'number'},
             {'name': _('Paid 0%'), 'class': 'number'},
             {'name': _('Exempt'), 'class': 'number'},
-            {'name': _('Withheld'), 'class': 'number'}
+            {'name': _('Withheld'), 'class': 'number'},
+            {'name': _('Paid 16% - Non-Creditable'), 'class': 'number'},
         ]
 
     def do_query(self, options, line_id):
@@ -144,6 +145,7 @@ class MxReportPartnerLedger(models.AbstractModel):
         unfold_all = context.get('print_mode') and not options.get('unfolded_lines')
         tag_16 = self.env.ref('l10n_mx.tag_diot_16')
         tag_imp = self.env.ref('l10n_mx.tag_diot_16_imp')
+        tag_non_cre = self.env.ref('l10n_mx.tag_diot_16_non_cre', raise_if_not_found=False) or self.env['account.account.tag']
         tag_0 = self.env.ref('l10n_mx.tag_diot_0')
         tag_ret = self.env.ref('l10n_mx.tag_diot_ret')
         tag_exe = self.env.ref('l10n_mx.tag_diot_exento')
@@ -151,6 +153,7 @@ class MxReportPartnerLedger(models.AbstractModel):
             ('type_tax_use', '=', 'purchase')])
         tax16 = tax_ids.search([('id', 'in', tax_ids.ids),
                                 ('tag_ids', 'in', tag_16.ids)])
+        taxnoncre = tax_ids.search([('id', 'in', tax_ids.ids), ('tag_ids', 'in', tag_non_cre.ids)]) if tag_non_cre else self.env['account.tax']
         taximp = tax_ids.search([('id', 'in', tax_ids.ids),
                                 ('tag_ids', 'in', tag_imp.ids)])
         tax0 = tax_ids.search([('id', 'in', tax_ids.ids),
@@ -179,7 +182,7 @@ class MxReportPartnerLedger(models.AbstractModel):
                 partner.l10n_mx_nationality or '']
             partner_data = grouped_partners[partner]
             total_tax16 = total_taximp = 0
-            total_tax0 = 0
+            total_tax0 = total_taxnoncre = 0
             exempt = 0
             withh = 0
             for tax in tax16.ids:
@@ -196,6 +199,9 @@ class MxReportPartnerLedger(models.AbstractModel):
             withh += sum([abs(partner_data.get(ret.id, 0) / (100 / ret.amount))
                           for ret in tax_ret])
             p_columns.append(withh)
+            for tax in taxnoncre.ids:
+                total_taxnoncre += partner_data.get(tax, 0)
+            p_columns.append(self.format_value(total_taxnoncre))
             unfolded = 'partner_' + str(partner.id) in options.get('unfolded_lines') or unfold_all
             lines.append({
                 'id': 'partner_' + str(partner.id),
@@ -227,7 +233,7 @@ class MxReportPartnerLedger(models.AbstractModel):
                 columns = ['', '', '', '']
                 columns.append('')
                 total_tax16 = total_taximp = 0
-                total_tax0 = 0
+                total_tax0 = total_taxnoncre = 0
                 exempt = 0
                 withh = 0
                 total_tax16 += sum([
@@ -251,6 +257,10 @@ class MxReportPartnerLedger(models.AbstractModel):
                     for ret in tax_ret
                     if ret.id in line.tax_ids.ids])
                 columns.append(self.format_value(withh))
+                total_taxnoncre += sum([
+                    line.debit or line.credit * -1
+                    for tax in taxnoncre.ids if tax in line.tax_ids.ids])
+                columns.append(self.format_value(total_taxnoncre))
                 caret_type = 'account.move'
                 if line.invoice_id:
                     caret_type = 'account.invoice.in' if line.invoice_id.type in ('in_refund', 'in_invoice') else 'account.invoice.out'

l10n_mx_reports/data/account_financial_report_data.xml
@@ -7,7 +7,10 @@
             <field name="name">DIOT: 16% IMP</field>
             <field name="applicability">taxes</field>
         </record>
-
+        <record id="l10n_mx.tag_diot_16_non_cre" model="account.account.tag">
+            <field name="name">DIOT: 16% NO ACREDITABLE</field>
+            <field name="applicability">taxes</field>
+        </record>
 
 
         <menuitem id="account_reports_legal_mexican_statements_menu"

l10n_mx_reports/i18n/es.po
@@ -4,16 +4,50 @@
 #
 msgid ""
 msgstr ""
-"Project-Id-Version: Odoo Server 10.saas~14+e\n"
+"Project-Id-Version: Odoo Server 11.0+e\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2017-03-09 20:25+0000\n"
-"PO-Revision-Date: 2017-03-09 20:25+0000\n"
+"POT-Creation-Date: 2018-06-12 03:07+0000\n"
+"PO-Revision-Date: 2018-06-12 03:07+0000\n"
 "Last-Translator: <>\n"
 "Language-Team: \n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: \n"
 "Plural-Forms: \n"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:125
+#, python-format
+msgid "\n"
+"\n"
+"Please fill the missing information in the partners and try again."
+msgstr "\n"
+"\n"
+"Por favor, llene la información faltante en las empresas e intente de nuevo."
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:122
+#, python-format
+msgid "\n"
+"\n"
+"The following partners do not have a type of operation: \n"
+" - %s"
+msgstr "\n"
+"\n"
+"Las siguientes empresas no tienen un tipo de operación: \n"
+" - %s"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:118
+#, python-format
+msgid "\n"
+"\n"
+"The following partners do not have a valid RFC: \n"
+" - %s"
+msgstr "\n"
+"\n"
+"Las siguientes empresas no tienen un RFC válido: \n"
+" - %s"
 
 #. module: l10n_mx_reports
 #: selection:res.partner,l10n_mx_type_of_operation:0
@@ -777,14 +811,26 @@ msgid "COA"
 msgstr "COA"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:26
+#: model:ir.model,name:l10n_mx_reports.model_res_partner
+msgid "Contact"
+msgstr "Contacto"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:24
 #: model:ir.model,name:l10n_mx_reports.model_res_country
 #, python-format
 msgid "Country"
 msgstr "País"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:267
+#: code:addons/l10n_mx_reports/models/trial_balance.py:60
+#: code:addons/l10n_mx_reports/models/trial_balance.py:64
+#, python-format
+msgid "Credit"
+msgstr "Credit"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:301
 #: model:ir.actions.client,name:l10n_mx_reports.action_account_report_diot
 #: model:ir.model,name:l10n_mx_reports.model_l10n_mx_account_diot
 #, python-format
@@ -795,6 +841,13 @@ msgstr "DIOT"
 #: model:ir.ui.view,arch_db:l10n_mx_reports.res_partner_account_diot_form_oml
 msgid "DIOT Information"
 msgstr "Información DIOT"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/trial_balance.py:59
+#: code:addons/l10n_mx_reports/models/trial_balance.py:63
+#, python-format
+msgid "Debit"
+msgstr "Debit"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_res_country_demonym
@@ -809,13 +862,13 @@ msgid "Display Name"
 msgstr "Nombre a mostrar"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:31
+#: code:addons/l10n_mx_reports/models/account_diot.py:29
 #, python-format
 msgid "Exempt"
 msgstr "Exento"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/trial_balance.py:41
+#: code:addons/l10n_mx_reports/models/trial_balance.py:44
 #, python-format
 msgid "Export For SAT (XML)"
 msgstr "Exportar para SAT (XML)"
@@ -825,36 +878,47 @@ msgstr "Exportar para SAT (XML)"
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_l10n_mx_coa_report_id
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_l10n_mx_trial_report_id
 msgid "ID"
-msgstr "ID"
+msgstr "ID (identificación)"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:29
+#: code:addons/l10n_mx_reports/models/account_diot.py:27
 #, python-format
 msgid "Importation 16%"
 msgstr "Importación 16%"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,help:l10n_mx_reports.field_res_partner_l10n_mx_type_of_operation
+#: model:ir.model.fields,help:l10n_mx_reports.field_res_users_l10n_mx_type_of_operation
 msgid "Indicate the operations type that makes this supplier. Is the second column in DIOT report"
 msgstr "Indique el tipo de operaciones que realiza a este proveedor. Es la segunda columna del reporte de DIOT"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,help:l10n_mx_reports.field_res_partner_l10n_mx_type_of_third
+#: model:ir.model.fields,help:l10n_mx_reports.field_res_users_l10n_mx_type_of_third
 msgid "Indicate the type of third that is the supplier. Is the first column in DIOT report."
 msgstr "Indica el tipo de tercero que es el proveedor. Es la primera columna del reporte de DIOT."
 
 #. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/trial_balance.py:55
+#, python-format
+msgid "Initial Balance"
+msgstr "Balance Inicial"
+
+#. module: l10n_mx_reports
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_res_partner_l10n_mx_nationality
+#: model:ir.model.fields,field_description:l10n_mx_reports.field_res_users_l10n_mx_nationality
 msgid "L10N Mx Nationality"
 msgstr "Nacionalidad"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_res_partner_l10n_mx_type_of_operation
+#: model:ir.model.fields,field_description:l10n_mx_reports.field_res_users_l10n_mx_type_of_operation
 msgid "L10N Mx Type Of Operation"
 msgstr "Tipo de operación"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,field_description:l10n_mx_reports.field_res_partner_l10n_mx_type_of_third
+#: model:ir.model.fields,field_description:l10n_mx_reports.field_res_users_l10n_mx_type_of_third
 msgid "L10N Mx Type Of Third"
 msgstr "Tipo de Tercero"
 
@@ -871,66 +935,94 @@ msgid "Mexican Chart of Account Report"
 msgstr "Reporte de la Tabla de Cuentas Mexicana"
 
 #. module: l10n_mx_reports
-#: model:ir.ui.menu,name:l10n_mx_reports.account_reports_legal_mexican_statements_menu
-msgid "Mexican Statements"
-msgstr "Declaraciones de México"
-
-#. module: l10n_mx_reports
 #: model:ir.model,name:l10n_mx_reports.model_l10n_mx_trial_report
 msgid "Mexican Trial Balance Report"
 msgstr "Reporte del Balance de Comprobación de México"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:27
+#: model:ir.ui.menu,name:l10n_mx_reports.account_reports_legal_mexican_statements_menu
+msgid "Mexico"
+msgstr "Mexico"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/coa.py:73
+#, python-format
+msgid "Misconfigured Accounts"
+msgstr "Misconfigured Accounts"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:25
 #, python-format
 msgid "Nationality"
 msgstr "Nacionalidad"
 
 #. module: l10n_mx_reports
 #: model:ir.model.fields,help:l10n_mx_reports.field_res_partner_l10n_mx_nationality
+#: model:ir.model.fields,help:l10n_mx_reports.field_res_users_l10n_mx_nationality
 msgid "Nationality based in the supplier country. Is the seventh column in DIOT report"
 msgstr "Nacionalidad basada en el país proveedor. Es la séptima columna en el informe DIOT"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/coa.py:36
+#: code:addons/l10n_mx_reports/models/coa.py:38
 #, python-format
 msgid "Nature"
 msgstr "Naturaleza"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:30
+#: code:addons/l10n_mx_reports/models/account_diot.py:28
 #, python-format
 msgid "Paid 0%"
 msgstr "Pagado 0%"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:28
+#: code:addons/l10n_mx_reports/models/account_diot.py:26
 #, python-format
 msgid "Paid 16%"
 msgstr "Pagado 16%"
 
 #. module: l10n_mx_reports
-#: model:ir.model,name:l10n_mx_reports.model_res_partner
-msgid "Partner"
-msgstr "Empresa"
+#: code:addons/l10n_mx_reports/models/account_diot.py:31
+#, python-format
+msgid "Paid 16% - Non-Creditable"
+msgstr "Pagado 16% - No Acreditable"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:271
+#: code:addons/l10n_mx_reports/models/account_diot.py:305
 #, python-format
 msgid "Print DIOT (TXT)"
 msgstr "Imprimir DIOT (TXT)"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:256
+#: code:addons/l10n_mx_reports/models/account_diot.py:116
+#, python-format
+msgid "The report cannot be generated because of: "
+msgstr "Este reporte no se puede generar porque: "
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:290
 #, python-format
 msgid "There are more than 80 items in this list, click here to see all of them"
 msgstr "Hay más de 80 artículos en esta lista, haga clic aquí para verlos todos"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:248
+#: code:addons/l10n_mx_reports/models/coa.py:110
+#, python-format
+msgid "This XML could not be generated because some accounts are not correctly configured and can not be added in this report. This accounts are found in the section \"Misconfigured Accounts\", please configure your tag and try generate the report XML again."
+msgstr "Este XML podría no haberse generado porque algunas cuentas no están configuradas correctamente y no se pueden agregar a este reporte. Estas cuentas se encuentran en las sección \"Cuentas No Configuradas\", por favor, configure su etiqueta y trate de generar el reporte otra vez.
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/account_diot.py:282
+#: code:addons/l10n_mx_reports/models/trial_balance.py:65
+#: code:addons/l10n_mx_reports/models/trial_balance.py:111
 #, python-format
 msgid "Total"
 msgstr "Total"
+
+#. module: l10n_mx_reports
+#: code:addons/l10n_mx_reports/models/trial_balance.py:103
+#, python-format
+msgid "Total %s"
+msgstr "Total %s"
 
 #. module: l10n_mx_reports
 #: model:ir.ui.menu,name:l10n_mx_reports.menu_action_account_report_diot
@@ -944,25 +1036,25 @@ msgid "Trial Balance"
 msgstr "Balanza de Comprobación"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:23
+#: code:addons/l10n_mx_reports/models/account_diot.py:22
 #, python-format
 msgid "Type of Operation"
 msgstr "Tipo de Operación"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:24
+#: code:addons/l10n_mx_reports/models/account_diot.py:21
 #, python-format
 msgid "Type of Third"
 msgstr "Tipo de Tercero"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:25
+#: code:addons/l10n_mx_reports/models/account_diot.py:23
 #, python-format
 msgid "VAT"
 msgstr "RFC"
 
 #. module: l10n_mx_reports
-#: code:addons/l10n_mx_reports/models/account_diot.py:32
+#: code:addons/l10n_mx_reports/models/account_diot.py:30
 #, python-format
 msgid "Withheld"
 msgstr "Retenido"

commit a058c057881bf981faa0df1510951958891dae09

[FIX] l10n_mx_reports: DIOT type str doesn't define __round__ method (#2406)
Bug introduced in 979b2e2f43e1af70d9af2dc1013c60cdf180d5e1#diff-de22b9ba743c5922579dab8f580a4e8f
Non-Creditable taxes get formatted 2 times -> type str doesn't define __round__ method

l10n_mx_reports/models/account_diot.py
@@ -201,7 +201,7 @@ class MxReportPartnerLedger(models.AbstractModel):
             p_columns.append(withh)
             for tax in taxnoncre.ids:
                 total_taxnoncre += partner_data.get(tax, 0)
-            p_columns.append(self.format_value(total_taxnoncre))
+            p_columns.append(total_taxnoncre)
             unfolded = 'partner_' + str(partner.id) in options.get('unfolded_lines') or unfold_all
             lines.append({
                 'id': 'partner_' + str(partner.id),