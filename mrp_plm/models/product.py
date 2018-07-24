2239b9af48a499bac09e67f64185b995e0374b2a
Ankit Joshi <ajo@odoo.com>
2018-07-24 11:53:17 +0530

    [IMP] mrp_plm: Add attachment button in product.product.
    Attachments buttons should also be visible for product
    variant. Moreover it should also display the product template
    attachments with it.
    
    Task: 1857701

--- mrp_plm/models/product.py
+++ mrp_plm/models/product.py
@@ -45,3 +45,49 @@ class ProductTemplate(models.Model):
             'limit': 80,
             'context': "{'default_res_model': '%s','default_res_id': %d}" % ('product.template', self.id)
         }
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    product_attachment_count = fields.Integer('# Attachment', compute='_compute_attachments')
+
+    def _get_documents(self):
+        return [
+            '|',
+                '&',
+                    ('res_model', '=', 'product.product'),
+                    ('res_id', '=', self.id),
+                '&',
+                    ('res_model', '=', 'product.template'),
+                    ('res_id', '=', self.product_tmpl_id.id)
+        ]
+
+    @api.multi
+    def _compute_attachments(self):
+        if not self.user_has_groups('mrp.group_mrp_user'):
+            return
+        for product in self:
+            domain = self._get_documents()
+            product.product_attachment_count = self.env['mrp.document'].search_count(domain)
+
+    @api.multi
+    def action_see_attachments(self):
+        domain = self._get_documents()
+        attachment_view = self.env.ref('mrp.view_document_file_kanban_mrp')
+        return {
+            'name': _('Attachments'),
+            'domain': domain,
+            'res_model': 'mrp.document',
+            'type': 'ir.actions.act_window',
+            'view_id': attachment_view.id,
+            'views': [(attachment_view.id, 'kanban'), (False, 'form')],
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'help': _('''<p class="o_view_nocontent_smiling_face">
+                        Upload files to your product
+                    </p><p>
+                        Use this feature to store any files, like drawings or specifications.
+                    </p>'''),
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % ('product.product', self.id)
+        }
