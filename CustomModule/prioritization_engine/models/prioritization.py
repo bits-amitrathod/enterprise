9772dfc4cdbbdffcc821235986d541f3da6e71b8
amit rathod <amit@localhost.localdomain>
2018-08-08 14:44:57 +0530

    Resolve error

--- CustomModule/prioritization_engine/models/prioritization.py
+++ CustomModule/prioritization_engine/models/prioritization.py
@@ -62,13 +62,6 @@ class Customer(models.Model):
             #'target': 'new',
 
         }
-'''show_lots_m2o=self.has_tracking != 'none' and (self.picking_type_id.use_existing_lots or self.state == 'done' or self.origin_returned_move_id.id),  # able to create lots, whatever the value of ` use_create_lots`.
-                show_lots_text=self.has_tracking != 'none' and self.picking_type_id.use_create_lots and not self.picking_type_id.use_existing_lots and self.state != 'done' and not self.origin_returned_move_id.id,
-                show_source_location=self.location_id.child_ids,
-                show_destination_location=self.location_dest_id.child_ids,
-                show_package=not self.location_id.usage == 'supplier',
-                show_reserved_quantity=self.state != 'done' '''
-
     # Return Customer prioritization setting ON(True)/OFF(False).
     def get_prioritization_setting(self):
         return self.prioritization
