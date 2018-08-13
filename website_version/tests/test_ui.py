51c1bb3cca9ae02fc55cb49ba98cec2901a8e273
Jeremy Kersten <jke@odoo.com>
2018-08-13 19:36:03 +0200

    [REM] website_version: remove module
    This module can be partially replaced with the new model website_page
    introduced in v11 and the 'publish/unpublish' feature.
    You can easily create new page side by side and improve it without
    publish them and link the page to the new view to make it the master.
    
    This module conflict with multi website and is too fragile.
    It can easily break anothers inherit.
    
    AB Testing (e.g. show 1 time on 3 a special page) will be probably reimplemented
    in the near futur with a more robust solution.

--- website_version/tests/test_ui.py
+++
@@ -1,7 +0,0 @@
-import odoo.tests
-
-
-@odoo.tests.tagged('post_install','-at_install')
-class TestUi(odoo.tests.HttpCase):
-    def test_01_versioning(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('versioning')", "odoo.__DEBUG__.services['web_tour.tour'].tours.versioning.ready", login='admin')
