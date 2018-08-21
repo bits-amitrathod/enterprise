a06e6ca205baf8470e3d313d2c8f19eaabf55772
Raphael Collet <rco@odoo.com>
2018-08-21 13:45:17 +0200

    [FIX] web_studio: use registry test mode
    The former hack for running Studio tests is no longer necessary.

--- web_studio/tests/test_ui.py
+++ web_studio/tests/test_ui.py
@@ -7,24 +7,20 @@ import odoo.tests
 @odoo.tests.tagged('post_install', '-at_install')
 class TestUi(odoo.tests.HttpCase):
 
-    # As the tour creates models and fields, which updates the registry,
-    # using a test cursor like the resy of HTTPCase won't work
-    registry_test_mode = False
-
-    def test_new_app(self):
+    def test_new_app_and_report(self):
         self.phantom_js("/web?studio=app_creator",
                         "odoo.__DEBUG__.services['web_tour.tour'].run('web_studio_new_app_tour')",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.web_studio_new_app_tour.ready",
+                        login="admin")
+
+        # the report tour is based on the result of the former tour
+        self.phantom_js("/web",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('web_studio_new_report_tour')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].tours.web_studio_new_report_tour.ready",
                         login="admin")
 
     def test_rename(self):
         self.phantom_js("/web?studio=app_creator",
                         "odoo.__DEBUG__.services['web_tour.tour'].run('web_studio_tests_tour')",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.web_studio_tests_tour.ready",
-                        login="admin")
-
-    def test_report(self):
-        self.phantom_js("/web",
-                        "odoo.__DEBUG__.services['web_tour.tour'].run('web_studio_new_report_tour')",
-                        "odoo.__DEBUG__.services['web_tour.tour'].tours.web_studio_new_report_tour.ready",
                         login="admin")
