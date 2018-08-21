f49e6434c6595bcec8d2a30519c93a7d72adfc0b
Manohar <manoharbomma@benchmarkitsolutions.com>
2018-08-21 12:22:18 +0530

    removed customer-requests dependency from priotization engine module manifest file

--- CustomModule/customer-requests/__manifest__.py
+++ CustomModule/customer-requests/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'fetchmail', 'sale', 'sps'],
+    'depends': ['base', 'fetchmail', 'sale', 'prioritization_engine'],
 
     # always loaded
     'data': [
--- CustomModule/prioritization_engine/__manifest__.py
+++ CustomModule/prioritization_engine/__manifest__.py
@@ -20,7 +20,7 @@
 
     # any module necessary for this one to work correctly
 
-    'depends': ['base','product','product_manufacturer','sale_management','stock','customer-requests','stock','web_one2many_selectable'],
+    'depends': ['base','product','product_manufacturer','sale_management','stock','web_one2many_selectable'],
 
     # always loaded
     'data': [
