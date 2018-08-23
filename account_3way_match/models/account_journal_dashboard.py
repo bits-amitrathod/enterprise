6bb26d7bf7b782d2e46345ee124bdbbc0ebeccd5
Hiral Bhavsar <hbh@odoo.com>
2018-08-23 16:10:22 +0200

    [IMP] account_3_way_match: don't use 'should be paid' to compute graph data for vendor bills journal on dashboard
    It was confusing not to take every bill into account in the graph. This is a usability improvement.
    
    [IMP] account_taxcloud: better help button in settings
    
    Was task 46188
    Was PR https://github.com/odoo/enterprise/pull/1836

--- account_3way_match/models/account_journal_dashboard.py
+++ account_3way_match/models/account_journal_dashboard.py
@@ -31,19 +31,3 @@ class AccountJournal(models.Model):
                    AND state = 'open';""",
                    {'journal_id':self.id, 'today':datetime.today()})
         return super(AccountJournal, self)._get_open_bills_to_pay_query()
-
-    def _get_bar_graph_select_query(self):
-        """
-        Overriden to take the 'release_to_pay' status and 'date_due' field into account
-        when getting the vendor bills to pay's graph data (for other types of
-        journal, its result remains unchanged).
-        """
-        if self.type == 'purchase':
-            return ("""SELECT sum(residual_company_signed) as total, min(date) as aggr_date
-                      FROM account_invoice
-                      WHERE journal_id = %(journal_id)s
-                      AND (release_to_pay = 'yes'
-                          OR date_due < %(today)s)
-                      AND state = 'open'""",
-                      {'journal_id':self.id, 'today':datetime.today()})
-        return super(AccountJournal, self)._get_bar_graph_select_query()
