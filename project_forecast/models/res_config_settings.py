c90e5f5e8611a095a276cdbcc4d28ac217b57d09
jem-odoo <jem@openerp.com>
2018-08-24 11:10:25 +0200

    [FIX] project_forecast: avoid retriggering compute on saving settings
    Saving the res.config.settings will call the `create` method and
    propagate the new value on the res.company for the related
    fields, like 'forecast_uom'.
    Even the value has not changed, writing the value will be done and if
    some computed fields (like resource_time on project.forecast)
    depends on this company field, a recomputation of all the record
    will be done (which might be very slow depending of the number
    of database entries).
    
    This commit prevent writing on 'forecast_uom' field on the company by
    not giving to the create of res.config.settings.
    
    Note: the same change was already done in accounting at
    c781c1e5e2d186b1c03c5b60e522f8e730d59a0e

--- project_forecast/models/res_config_settings.py
+++ project_forecast/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -16,3 +16,15 @@ class ResConfigSettings(models.TransientModel):
         ('week', 'By week'),
         ('month', 'By month')
     ], string="Time Span", related='company_id.forecast_span', required=True, help="Encode your forecast in a table displayed by days, weeks or the whole year.")
+
+    @api.model
+    def create(self, values):
+        # Optimisation purpose, saving a res_config even without changing any values will trigger the write of all
+        # related values, including the forecast_uom field on res_company. This in turn will trigger the recomputation
+        # of account_move_line related field company_currency_id which can be slow depending on the number of entries
+        # in the database. Thus, if we do not explicitly change the forecast_uom, we should not write it on the company
+        if ('company_id' in values and 'forecast_uom' in values):
+            company = self.env['res.company'].browse(values.get('company_id'))
+            if company.forecast_uom == values.get('forecast_uom'):
+                values.pop('forecast_uom')
+        return super(ResConfigSettings, self).create(values)
