e7ea8370f89b603ec9f642be646a6da82ff6152d
AkashIngole <aksahingole@benchmarkitsolutions.com>
2018-08-24 13:48:29 +0530

    reports changes

--- CustomModule/reports/lot_history/__manifest__.py
+++ CustomModule/reports/lot_history/__manifest__.py
@@ -2,15 +2,15 @@
 {
     'name': "lot_history",
 
-    # 'summary': """
-    #     Short (1 phrase/line) summary of the module's purpose, used as
-    #     subtitle on modules listing or apps.openerp.com""",
-    #
-    # 'description': """
-    #     Long description of module's purpose
-    # """,
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
-    'author': "By Akash Ingole",
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
