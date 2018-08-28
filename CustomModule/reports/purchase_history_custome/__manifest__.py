5f6397aa5108f1149e08aeb90f7eee6408ad74ba
AkashIngole <aksahingole@benchmarkitsolutions.com>
2018-08-28 17:16:29 +0530

    app name change

--- CustomModule/reports/purchase_history_custome/__manifest__.py
+++ CustomModule/reports/purchase_history_custome/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "purchase_history_custome",
+    'name': "PurchaseHistoryCustom",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
