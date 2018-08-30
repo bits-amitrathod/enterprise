6157af8babcb8240610e2598818a49dfb680096a
Priyanka Kakadiya <pka@odoo.com>
2018-08-30 14:27:42 +0200

    [FIX] l10n_be_intrastat: removed references to group 'intrastat extended'
    The 'intrastat extended' group has lately been removed, but references to it were still existing... which caused a traceback when creating new users if l10n_be_intrastat was installed
    
    Related to Issue: 1879139

--- l10n_be_intrastat/__manifest__.py
+++ l10n_be_intrastat/__manifest__.py
@@ -12,7 +12,6 @@ Based on invoices.
     'data': [
         'data/code_region_data.xml',
         'data/intrastat_export.xml',
-        'views/res_users_views.xml',
         'views/res_config_settings.xml',
     ],
     'auto_install': True,
