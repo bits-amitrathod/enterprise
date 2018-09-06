d0645c6fee4c85e3e2b3840bd8b7418d38fafbcd
len-odoo <len@odoo.com>
2018-09-06 16:51:32 +0200

    [FIX] web_grid: apply context group_by at view load and reload
    The default search view, although given in params, was not taken into account
    when loading the view, although the search view would display the default filter
    as currently active.
    Similarly this was ignored at reload.
    
    opw 1878899

--- web_grid/static/src/js/grid_view.js
+++ web_grid/static/src/js/grid_view.js
@@ -54,6 +54,7 @@ var GridView = AbstractView.extend({
         this.loadParams.sectionField = sectionField;
         this.loadParams.colField = colField;
         this.loadParams.cellField = cellField;
+        this.loadParams.groupedBy = params.groupBy;
 
         // renderer
         this.rendererParams.canCreate = this.controllerParams.activeActions.create;
