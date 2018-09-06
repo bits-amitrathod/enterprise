e790c303da93de313f0802b51d5bc07ff46c9654
Pierre Paridans <app@odoo.com>
2018-09-06 10:35:25 +0200

    [IMP] sale_subscription: clarify the actions available on the subscription
    I don't see what to do after selecting the customer, the template and set some products: no primary action, what means the stage New? is the invoicing already ongoing?
    * Remove New stage
    * Add a button to close the subscription. It moves it to the first ending stage. Hide the button if already closed when you click, pop-up to select a close reason
    * hide "create renewal" if not in the state "to renew"
    
    Reported by FGI during saas-11.5 tests.

--- sale_subscription/wizard/sale_subscription_close_reason_wizard.py
+++ sale_subscription/wizard/sale_subscription_close_reason_wizard.py
@@ -8,11 +8,8 @@ class SaleSubscriptionCloseReasonWizard(models.TransientModel):
     close_reason_id = fields.Many2one("sale.subscription.close.reason", string="Close Reason", required=True)
 
     @api.multi
-    def set_close_cancel(self):
+    def set_close(self):
         self.ensure_one()
         subscription = self.env['sale.subscription'].browse(self.env.context.get('active_id'))
         subscription.close_reason_id = self.close_reason_id
-        if self.env.context.get('cancel'):
-            subscription.set_cancel()
-        else:
-            subscription.set_close()
+        subscription.set_close()
