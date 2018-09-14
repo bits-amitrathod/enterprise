0f05e5a584d48e988c5817a725e27af301a07b53
Arnold Moyaux <arm@odoo.com>
2018-09-14 12:01:26 +0200

    [FIX] stock_barcode: remove /0 on barcode lines

--- stock_barcode/static/src/js/client_action/lines_widget.js
+++ stock_barcode/static/src/js/client_action/lines_widget.js
@@ -411,7 +411,7 @@ var LinesWidget = Widget.extend({
         $line.parents('.o_barcode_lines').toggleClass('o_js_has_highlight', true);
 
         var isReservationProcessed;
-        if ($line.find('.o_barcode_scanner_qty').text().trim().split('/')[1].trim() === '0') {
+        if ($line.find('.o_barcode_scanner_qty').text().indexOf('/').length > -1) {
             isReservationProcessed = false;
         } else {
             isReservationProcessed = this._isReservationProcessedLine($line);
