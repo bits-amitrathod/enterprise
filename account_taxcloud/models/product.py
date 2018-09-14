37149f64839a26fd4547acdbe42bd2f261606eb2
Prakash Prajapati <ppr@odoo.com>
2018-09-14 14:00:34 +0530

    [IMP] contact, product, various: improve labels and tooltips of partner and product fields
    Purpose: improve understanding of partner and product models as well as their
    daily use. This is done by improving tooltips and labels of some fields
    according to business logic.
    
    Enterprise counterpart of https://github.com/odoo/odoo/commit/4b5265b9b99a7ded943c5692d927b3a6163d75da.
    
    This commit is related to task ID 1880696 and closes PR #2694 .
    
    Thanks to @xmo-odoo for its in-depth review.

--- account_taxcloud/models/product.py
+++ account_taxcloud/models/product.py
@@ -32,12 +32,9 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     tic_category_id = fields.Many2one('product.tic.category', string="TaxCloud Category",
-        help="TaxCloud uses Taxability Information Codes (TIC) to make sure each item in your catalog "
-             "is taxed at the right rate (or, for tax-exempt items, not taxed at all), so it's important "
-             "to make sure that each item is assigned a TIC. If you can't find the right tax category for "
-             "an item in your catalog, you can assign it to the 'General Goods and Services' TIC, 00000. "
-             "TaxCloud automatically assigns products to this TIC as a default, so unless you've changed an "
-             "item's TIC in the past, it should already be set to 00000.")
+        help="Each product falls into a category which has specific taxes predermined by the government."
+            "The system will use the Tax Cloud category set on the internal category of the product. If there"
+            "isn't any, the one on the product itself will be used. Only used in United States.")
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
