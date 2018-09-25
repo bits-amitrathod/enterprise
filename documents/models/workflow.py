bits-amitrathod/enterprise

commit f6aa721f01a7cc6c595032560f0c87e2d742f8c2

[FIX] documents: views, workflow, activity.
- fixed the previewer on kanban for binary files.

- changed the filter positions of the kanban view.

- Fixed: replaced the xpath replace with a after

- Fixed: security: consteq with input as first arg.

- Changed the style of the documents' no content helper to
make it compliant with the style guidelines. As requested by
SRI & EST.

- Took the option boolean out of the documents activity method
to make it more generic.

- The workflow remove activities now mark them as done instead of
deleting the activities.

- added base share link data.

- Fixed Tag catagory form view

- Fixed Sign template workflow creation

- Fixed user/partner workflow option

- Added tests for workflow activities

- fixed activity method of documents.

- added the create date to the action list view

- improved youtube video check to display the video icon
when the attachment is a youtube url.

Task: 1873908 ( V12 test phase & feedback. )

documents/models/workflow.py
@@ -35,7 +35,7 @@ class WorkflowActionRule(models.Model):
     create_model = fields.Selection([], string="Create")
 
     # Activity
-    remove_activities = fields.Boolean(string='Remove all Activities')
+    remove_activities = fields.Boolean(string='Mark all Activities as done')
     activity_option = fields.Boolean(string='Create a new activity')
     activity_type_id = fields.Many2one('mail.activity.type', string="Activity type")
     activity_summary = fields.Char('Summary')
@@ -80,16 +80,16 @@ class WorkflowActionRule(models.Model):
             # partner/owner/share_link/folder changes
             attachment_dict = {}
             if self.user_id:
-                attachment_dict['owner_id'] = self.user_id
+                attachment_dict['owner_id'] = self.user_id.id
             if self.partner_id:
-                attachment_dict['partner_id'] = self.partner_id
+                attachment_dict['partner_id'] = self.partner_id.id
             if self.folder_id:
                 attachment_dict['folder_id'] = self.folder_id.id
 
             attachment.write(attachment_dict)
 
             if self.remove_activities:
-                attachment.activity_ids.unlink()
+                attachment.activity_ids.action_feedback(feedback=f"completed by rule: {self.name}. {self.note or ''}")
 
             if self.activity_option and self.activity_type_id:
                 attachment.documents_set_activity(settings_model=self)

documents_product/models/workflow.py
@@ -5,7 +5,7 @@ class WorkflowActionRuleProduct(models.Model):
     _inherit = ['documents.workflow.rule']
 
     has_business_option = fields.Boolean(default=True, compute='_get_business')
-    create_model = fields.Selection(selection_add=[('product.template', "Product Product template")])
+    create_model = fields.Selection(selection_add=[('product.template', "Product template")])
 
     def create_record(self, attachments=None):
         rv = super(WorkflowActionRuleProduct, self).create_record(attachments=attachments)

documents_sign/models/workflow.py
@@ -17,8 +17,6 @@ class WorkflowActionRuleSign(models.Model):
                     'name': attachment.datas_fname[:attachment.datas_fname.rfind('.')],
                     'attachment_id': attachment.id,
                 }
-                for tag_action in self.tag_action_ids:
-                    tag_action.execute_action(new_obj)
                 if self.folder_id:
                     create_values['folder_id'] = self.folder_id.id
                 elif self.domain_folder_id:

documents/controllers/main.py
@@ -99,7 +99,7 @@ class ShareRoute(http.Controller):
             share = env['documents.share'].sudo().browse(share_id)
             if share.state == 'expired':
                 return request.not_found()
-            if consteq(share.access_token, access_token):
+            if consteq(access_token, share.access_token):
                 if share.action != 'upload':
                     attachments = False
                     if share.type == 'domain':
@@ -125,7 +125,7 @@ class ShareRoute(http.Controller):
         try:
             env = request.env
             share = env['documents.share'].sudo().browse(share_id)
-            if consteq(share.access_token, access_token):
+            if consteq(access_token, share.access_token):
                 return base64.b64decode(env['res.users'].sudo().browse(share.create_uid.id).image_small)
             else:
                 return request.not_found()
@@ -167,7 +167,7 @@ class ShareRoute(http.Controller):
         """
         env = request.env
         share = env['documents.share'].sudo().browse(share_id)
-        if consteq(share.access_token, access_token):
+        if consteq(access_token, share.access_token):
             try:
                 if share.action != 'upload' and share.state != 'expired':
                     return self._get_file_response(id, share_id=share_id, share_token=share.access_token, field='datas')
@@ -189,7 +189,7 @@ class ShareRoute(http.Controller):
         share = http.request.env['documents.share'].sudo().search([('id', '=', arg_id)])
         if 'upload' not in share.action or share.state == 'expired':
             return http.request.not_found()
-        if consteq(share.access_token, token):
+        if consteq(token, share.access_token):
             attachments = request.env['ir.attachment']
             folder = share.folder_id
             folder_id = folder.id or False
@@ -208,7 +208,8 @@ class ShareRoute(http.Controller):
                         'datas': base64.b64encode(data),
                     }
                     attachment = attachments.sudo().create(attachment_dict)
-                    attachment.documents_set_activity(settings_model=share)
+                    if share.activity_option:
+                        attachment.documents_set_activity(settings_model=share)
 
             except Exception as e:
                 logger.exception("Failed to upload attachment")
@@ -238,7 +239,7 @@ class ShareRoute(http.Controller):
                     'author': share.create_uid.name,
                 }
                 return request.render('documents.not_available', expired_options)
-            if not consteq(share.access_token, token):
+            if not consteq(token, share.access_token):
                 return request.not_found()
 
             if share.type == 'domain':

documents/models/documents.py
@@ -112,7 +112,8 @@ class IrAttachment(models.Model):
             attachments = self.env['ir.attachment'].browse([x[1] for x in rv])
             for attachment in attachments:
                 attachment.write(write_vals)
-                attachment.documents_set_activity(settings_model=share)
+                if share.activity_option:
+                    attachment.documents_set_activity(settings_model=share)
 
         return rv
 
@@ -122,22 +123,34 @@ class IrAttachment(models.Model):
         Generate an activity based on the fields of settings_model.
 
         :param settings_model: the model that contains the activity fields.
+                    settings_model.activity_type_id (required)
+                    settings_model.activity_summary
+                    settings_model.activity_note
+                    settings_model.activity_date_deadline_range
+                    settings_model.activity_date_deadline_range_type
+                    settings_model.activity_user_id
         """
-        if settings_model:
-            if settings_model.activity_option and settings_model.activity_type_id:
-                activity_vals = {
-                    'summary': settings_model.activity_summary or '',
-                    'note': settings_model.activity_note or '',
-                    'activity_type_id': settings_model.activity_type_id.id,
-                }
-                if settings_model.activity_date_deadline_range > 0:
-                    activity_vals['date_deadline'] = fields.Date.context_today(settings_model) + relativedelta(
-                        **{settings_model.activity_date_deadline_range_type: settings_model.activity_date_deadline_range})
+        if settings_model and settings_model.activity_type_id:
+            activity_vals = {
+                'activity_type_id': settings_model.activity_type_id.id,
+                'summary': settings_model.activity_summary or '',
+                'note': settings_model.activity_note or '',
+            }
+            if settings_model.activity_date_deadline_range > 0:
+                activity_vals['date_deadline'] = fields.Date.context_today(settings_model) + relativedelta(
+                    **{settings_model.activity_date_deadline_range_type: settings_model.activity_date_deadline_range})
 
-                user = settings_model.activity_user_id or settings_model.owner_id or self.env.user
-                if user:
-                    activity_vals['user_id'] = user.id
-                self.activity_schedule(**activity_vals)
+            if settings_model._fields.get('activity_user_id') and settings_model.activity_user_id:
+                user = settings_model.activity_user_id
+            elif settings_model._fields.get('user_id') and settings_model.user_id:
+                user = settings_model.user_id
+            elif settings_model._fields.get('owner_id') and settings_model.owner_id:
+                user = settings_model.owner_id
+            else:
+                user = self.env.user
+            if user:
+                activity_vals['user_id'] = user.id
+            self.activity_schedule(**activity_vals)
 
     @api.multi
     def toggle_favorited(self):

documents/static/src/xml/documents_inspector.xml
@@ -29,12 +29,15 @@
     <t t-foreach="widget.records.slice(0, 4)" t-as="record">
         <div t-attf-class="o_document_preview #{nbPreviews === 1 and 'o_documents_single_preview' or ''} #{record._isImage and 'o_documents_preview_image' or 'o_documents_preview_mimetype'}">
             <t t-set="mimetype" t-value="record.data.mimetype"/>
+            <t t-set="supportedUrlVideo"
+               t-value="record.data.url ? new RegExp('^(http(s)??\:\/\/)?(www\.)?((youtu.be\/))([a-zA-Z0-9\-_])|(youtube\.com\/watch\?v=)+$').test(record.data.url) : false"/>
             <t t-if="record._isImage">
                 <t t-set="size" t-value="nbPreviews === 1 ? '268x130' : nbPreviews === 2 ? '120x130' : '120x75'"/>
                 <img class="o_preview_available" t-attf-src="/web/image/#{record.res_id}/#{size}?upper_limit=true&amp;signature=#{record.data.checksum}" t-att-data-id="record.res_id" t-att-title="record.data.name"/>
             </t>
-            <div t-elif="record.data.url" t-attf-class="o_image o_mimetype_icon #{(record.data.url.indexOf('youtu') >= 0) and 'o_preview_available' or ''}"
-                 t-attf-data-mimetype="#{(record.data.url.indexOf('youtu') >= 0) and 'video' or mimetype}"
+            <div t-elif="record.data.url"
+                 t-attf-class="o_image o_mimetype_icon #{supportedUrlVideo and 'o_preview_available' or ''}"
+                 t-attf-data-mimetype="#{supportedUrlVideo and 'video' or mimetype}"
                  t-att-title="record.data.name"
                  t-att-data-id="record.res_id"/>
             <div t-else=""

documents/tests/test_documents.py
@@ -80,7 +80,14 @@ class TestCaseDocuments(TransactionCase):
             'domain_folder_id': self.folder_a_a.id,
             'name': 'workflow rule on f_a_a',
             'folder_id': self.folder_b.id,
-            'tag_action_ids': [(4, self.tag_action_a.id, 0)]
+            'tag_action_ids': [(4, self.tag_action_a.id, 0)],
+            'remove_activities': True,
+            'activity_option': True,
+            'activity_type_id': self.env.ref('documents.mail_documents_activity_data_Inbox').id,
+            'activity_summary': 'test workflow rule activity summary',
+            'activity_date_deadline_range': 7,
+            'activity_date_deadline_range_type': 'days',
+            'activity_note': 'activity test note',
         })
 
     def test_documents_rules(self):
@@ -91,6 +98,20 @@ class TestCaseDocuments(TransactionCase):
         self.assertTrue(self.tag_b.id in self.attachment_gif.tag_ids.ids, "failed at workflow rule add tag id")
         self.assertTrue(self.tag_b.id in self.attachment_txt.tag_ids.ids, "failed at workflow rule add tag id 2")
         self.assertEqual(len(self.attachment_gif.tag_ids.ids), 1, "failed at workflow rule add tag len")
+
+        activity_gif = self.env['mail.activity'].search(['&',
+                                                         ('res_id', '=', self.attachment_gif.id),
+                                                         ('res_model', '=', 'ir.attachment')])
+
+        self.assertEqual(len(activity_gif), 1, "failed at workflow rule activity len")
+        self.assertTrue(activity_gif.exists(), "failed at workflow rule activity exists")
+        self.assertEqual(activity_gif.summary, 'test workflow rule activity summary',
+                         "failed at activity data summary from workflow create activity")
+        self.assertEqual(activity_gif.note, '<p>activity test note</p>',
+                         "failed at activity data note from workflow create activity")
+        self.assertEqual(activity_gif.activity_type_id.id,
+                         self.env.ref('documents.mail_documents_activity_data_Inbox').id,
+                         "failed at activity data note from workflow create activity")
 
         self.assertEqual(self.attachment_gif.folder_id.id, self.folder_b.id, "failed at workflow rule set folder gif")
         self.assertEqual(self.attachment_txt.folder_id.id, self.folder_b.id, "failed at workflow rule set folder txt")

documents/views/documents_views.xml
@@ -18,21 +18,24 @@
         <field name="model">ir.attachment</field>
         <field name="inherit_id" ref="base.view_attachment_search"/>
         <field name="arch" type="xml">
+            <xpath expr="//filter[@name='binary_filter']" position="after">
+                <filter name="requested_filter" string="Requested files" domain="[('type', '=', 'empty')]"/>
+            </xpath>
             <xpath expr="//search" position="inside">
                 <field name="datas_fname"/>
                 <field name="tag_ids"/>
                 <field name="owner_id"/>
                 <field name="partner_id" string="Contact"/>
                 <separator/>
-                    <filter string="Late Activities" name="activities_overdue"
-                        domain="[('activity_ids.date_deadline', '&lt;', context_today().strftime('%Y-%m-%d'))]"
-                        help="Show all records which has next action date is before today"/>
-                    <filter string="Today Activities" name="activities_today"
-                        domain="[('activity_ids.date_deadline', '=', context_today().strftime('%Y-%m-%d'))]"/>
-                    <filter string="Future Activities" name="activities_upcoming_all"
-                        domain="[('activity_ids.date_deadline', '&gt;', context_today().strftime('%Y-%m-%d'))
-                        ]"/>
-                <filter name="requested_filter" string="Requested files" domain="[('type', '=', 'empty')]"/>
+                <filter string="Late Activities" name="activities_overdue"
+                    domain="[('activity_ids.date_deadline', '&lt;', context_today().strftime('%Y-%m-%d'))]"
+                    help="Show all records which has next action date is before today"/>
+                <filter string="Today Activities" name="activities_today"
+                    domain="[('activity_ids.date_deadline', '=', context_today().strftime('%Y-%m-%d'))]"/>
+                <filter string="Future Activities" name="activities_upcoming_all"
+                    domain="[('activity_ids.date_deadline', '&gt;', context_today().strftime('%Y-%m-%d'))
+                    ]"/>
+                <separator/>
                 <filter string="PDF" name="pdf" domain="[('mimetype', 'ilike', 'pdf')]"/>
                 <filter string="Image" name="img" domain="[('mimetype', 'ilike', 'image')]"/>
                 <filter string="Video" name="video" domain="[('mimetype', 'ilike', 'video')]"/>
@@ -80,11 +83,16 @@
                         <t t-set="fileRequest" t-value="record.type.raw_value === 'empty'"/>
                         <div t-attf-class="oe_kanban_global_area o_kanban_attachment #{fileRequest ? 'oe_file_request' : ''}">
                             <div t-attf-class="o_kanban_image #{fileRequest ? 'o_request_image' : ''}">
-                                <t t-set="webimage" t-value="new RegExp('image.*(gif|jpeg|jpg|png)').test(record.mimetype.value)"/>
-                                <t t-set="binaryPreviewable" t-value="new RegExp('(image|video|application/pdf|text)').test(record.mimetype.value) &amp;&amp; record.type === 'binary'"/>
-                                <t t-set="urlPreviewable" t-value="record.url.raw_value.length ? record.url.raw_value.indexOf('youtu') >= 0 : false"/>
-                                <div t-attf-class="o_kanban_image_wrapper #{(webimage || binaryPreviewable || urlPreviewable) ? 'oe_kanban_previewer' : ''}">
-                                    <div t-if="record.type.raw_value == 'url'" class="o_url_image fa fa-link fa-3x text-muted"/>
+                                <t t-set="webimage"
+                                   t-value="new RegExp('image.*(gif|jpeg|jpg|png)').test(record.mimetype.value)"/>
+                                <t t-set="binaryPreviewable"
+                                   t-value="new RegExp('(image|video|application/pdf|text)').test(record.mimetype.value) &amp;&amp; record.type.raw_value === 'binary'"/>
+                                <!-- should be made more generic if we support different websites for videos -->
+                                <t t-set="supportedUrlVideo"
+                                   t-value="record.url.raw_value.length ? new RegExp('^(http(s)??\:\/\/)?(www\.)?((youtu.be\/))([a-zA-Z0-9\-_])|(youtube\.com\/watch\?v=)+$').test(record.url.raw_value) : false"/>
+                                <div t-attf-class="o_kanban_image_wrapper #{(webimage || binaryPreviewable || supportedUrlVideo) ? 'oe_kanban_previewer' : ''}">
+                                    <div t-if="supportedUrlVideo" class="o_image o_image_thumbnail" data-mimetype="video"/>
+                                    <div t-elif="record.type.raw_value == 'url'" class="o_url_image fa fa-link fa-3x text-muted"/>
                                     <img t-elif="webimage" t-attf-src="/web/image/#{record.id.raw_value}?field=thumbnail&amp;signature=#{record.checksum.value}" width="100" height="100" alt="Document" class="o_attachment_image"/>
                                     <div t-elif="fileRequest" class="fa fa-download fa-3x text-muted"/>
                                     <div t-else="!webimage" class="o_image o_image_thumbnail" t-att-data-mimetype="record.mimetype.value"/>
@@ -465,6 +473,7 @@
             <tree>
                 <field name="domain_folder_id"/>
                 <field name="name"/>
+                <field name="create_date"/>
             </tree>
         </field>
     </record>
@@ -621,16 +630,18 @@
         <field name="arch" type="xml">
             <form>
                 <sheet>
+                    <div class="oe_title">
+                        <label for="name" class="oe_edit_only"/>
+                        <h1><field name="name" placeholder="E.g. Status"/></h1>
+                    </div>
                     <group>
-                        <div class="oe_title">
-                            <label for="name" class="oe_edit_only"/>
-                            <h1><field name="name" placeholder="E.g. Status"/></h1>
-                        </div>
-                        <field name="folder_id" required="1"/>
-                        <field name="tooltip"/>
-                    </group>
-                    <group>
-                        <field name="tag_ids"/>
+                        <group>
+                            <field name="folder_id" required="1"/>
+                            <field name="tooltip"/>
+                        </group>
+                        <group>
+                            <field name="tag_ids"/>
+                        </group>
                     </group>
                 </sheet>
             </form>
@@ -754,7 +765,7 @@
       <field name="view_id" ref="documents.documents_view_kanban"/>
       <field name="domain">['&amp;', ('res_model', 'not in', ['ir.ui.view', 'Country', 'ir.ui.menu', 'Contact']), ('res_field', '=', False), ('folder_id', '!=', False)]</field>
       <field name="help" type="html">
-            <p class="o_view_nocontent_smiling_face">Upload <small>a file or</small> drag <small>it here.</small></p>
+            <p class="o_view_nocontent_smiling_face">Upload <span class="font-weight-normal">a file or</span> drag <span class="font-weight-normal">it here.</span></p>
         </field>
     </record>
 

commit edd4ca8ac340a6ad1855fc59cfb37c3f57fb8a18

[FIX] documents: don't use f-strings (yet)
Odoo officially supports python 3.5, which doesn't support f-strings.

Commit f6aa721f01a7cc6c595032560f0c87e2d742f8c2 introduced an f-string,
result: registry crashes because of a SyntaxError on Python 3.5

documents/models/workflow.py
@@ -89,7 +89,9 @@ class WorkflowActionRule(models.Model):
             attachment.write(attachment_dict)
 
             if self.remove_activities:
-                attachment.activity_ids.action_feedback(feedback=f"completed by rule: {self.name}. {self.note or ''}")
+                attachment.activity_ids.action_feedback(
+                    feedback="completed by rule: %s. %s" % (self.name, self.note or '')
+                )
 
             if self.activity_option and self.activity_type_id:
                 attachment.documents_set_activity(settings_model=self)