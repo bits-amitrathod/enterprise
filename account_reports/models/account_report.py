4d1a48c135d311b6970222aedf1c1a19561a8b23
Laurent Smet <las@odoo.com>
2018-11-02 09:25:59 +0100

    [IMP] account_reports: hide irrelevant multi_company filter on tax report

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -51,6 +51,7 @@ class AccountReportFootnote(models.Model):
 class AccountReport(models.AbstractModel):
     _name = 'account.report'
 
+    filter_multi_company = True
     filter_date = None
     filter_cash_basis = None
     filter_all_entries = None
@@ -70,7 +71,7 @@ class AccountReport(models.AbstractModel):
             options[filter_name] = getattr(self, element)
 
         group_multi_company = self.env['ir.model.data'].xmlid_to_object('base.group_multi_company')
-        if self.env.user.id in group_multi_company.users.ids:
+        if options.get('multi_company') and self.env.user.id in group_multi_company.users.ids:
             # We have a user with multi-company
             options['multi_company'] = [{'id': c.id, 'name': c.name, 'selected': True if c.id == self.env.user.company_id.id else False} for c in self.env.user.company_ids]
         if options.get('journals'):
