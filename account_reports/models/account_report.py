bits-amitrathod/enterprise

commit 4d1a48c135d311b6970222aedf1c1a19561a8b23

[IMP] account_reports: hide irrelevant multi_company filter on tax report

account_reports/models/account_report.py
@@ -51,6 +51,7 @@ class AccountReportFootnote(models.Model):
 class AccountReport(models.AbstractModel):
     _name = 'account.report'
 
+    filter_multi_company = True
     filter_date = None
     filter_cash_basis = None
     filter_all_entries = None
@@ -70,7 +71,7 @@ class AccountReport(models.AbstractModel):
             options[filter_name] = getattr(self, element)
 
         group_multi_company = self.env['ir.model.data'].xmlid_to_object('base.group_multi_company')
-        if self.env.user.id in group_multi_company.users.ids:
+        if options.get('multi_company') and self.env.user.id in group_multi_company.users.ids:
             # We have a user with multi-company
             options['multi_company'] = [{'id': c.id, 'name': c.name, 'selected': True if c.id == self.env.user.company_id.id else False} for c in self.env.user.company_ids]
         if options.get('journals'):

account_reports/models/account_generic_tax_report.py
@@ -10,6 +10,7 @@ class generic_tax_report(models.AbstractModel):
     _inherit = 'account.report'
     _name = 'account.generic.tax.report'
 
+    filter_multi_company = None
     filter_date = {'date_from': '', 'date_to': '', 'filter': 'this_month'}
     filter_cash_basis = False
     filter_all_entries = False

commit dad865f1a64ed87357e553637eecf6ec94f4d08f

[FIX] account_reports: Fix traceback in single company
In case of single company mode, the user is not in the multi-company group and then,
options['multi_company'] == True due to filter_multi_company = True.

Bug introduced by:
https://github.com/odoo/enterprise/commit/4d1a48c135d311b6970222aedf1c1a19561a8b23

account_reports/models/account_report.py
@@ -71,9 +71,12 @@ class AccountReport(models.AbstractModel):
             options[filter_name] = getattr(self, element)
 
         group_multi_company = self.env['ir.model.data'].xmlid_to_object('base.group_multi_company')
-        if options.get('multi_company') and self.env.user.id in group_multi_company.users.ids:
-            # We have a user with multi-company
-            options['multi_company'] = [{'id': c.id, 'name': c.name, 'selected': True if c.id == self.env.user.company_id.id else False} for c in self.env.user.company_ids]
+        if options.get('multi_company'):
+            if self.env.user.id in group_multi_company.users.ids:
+                # We have a user with multi-company
+                options['multi_company'] = [{'id': c.id, 'name': c.name, 'selected': True if c.id == self.env.user.company_id.id else False} for c in self.env.user.company_ids]
+            else:
+                del options['multi_company']
         if options.get('journals'):
             options['journals'] = self.get_journals()
 