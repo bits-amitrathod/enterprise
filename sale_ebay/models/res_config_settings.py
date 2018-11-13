bits-amitrathod/enterprise

commit 39fb3be0ef0a95366ecd1bc5112a944de060612b

[REF] various: convert res.config.settings to use ir.config_parameter integration
This commit is linked to community update about config settings. Those
now handle config parameter like other specific config fields using a
new config_parameter attribute on fields.

Please see commit https://github.com/odoo/odoo/commit/0b32d3fe614ad2f96cc50d31b8feebb3474a1ce7 for more details.

sale_ebay/models/res_config_settings.py
@@ -9,84 +9,58 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    ebay_dev_id = fields.Char("Developer Key")
-    ebay_sandbox_token = fields.Text("Sandbox Token")
-    ebay_sandbox_app_id = fields.Char("Sandbox App Key")
-    ebay_sandbox_cert_id = fields.Char("Sandbox Cert Key")
+    ebay_dev_id = fields.Char("Developer Key", default='', config_parameter='ebay_dev_id')
+    ebay_sandbox_token = fields.Char("Sandbox Token", default='', config_parameter='ebay_sandbox_token')
+    ebay_sandbox_app_id = fields.Char("Sandbox App Key", default='', config_parameter='ebay_sandbox_app_id')
+    ebay_sandbox_cert_id = fields.Char("Sandbox Cert Key", default='', config_parameter='ebay_sandbox_cert_id')
 
-    ebay_prod_token = fields.Text("Production Token")
-    ebay_prod_app_id = fields.Char("Production App Key")
-    ebay_prod_cert_id = fields.Char("Production Cert Key")
+    ebay_prod_token = fields.Char("Production Token", default='', config_parameter='ebay_prod_token')
+    ebay_prod_app_id = fields.Char("Production App Key", default='', config_parameter='ebay_prod_app_id')
+    ebay_prod_cert_id = fields.Char("Production Cert Key", default='', config_parameter='ebay_prod_cert_id')
     ebay_domain = fields.Selection([
         ('prod', 'Production'),
         ('sand', 'Sandbox'),
-    ], string='eBay Mode', default='sand', required=True)
+    ], string='Mode', default='sand', required=True, config_parameter='ebay_domain')
     ebay_currency = fields.Many2one("res.currency", string='ebay Currency',
-                                    domain=[('ebay_available', '=', True)], required=True)
+                                    domain=[('ebay_available', '=', True)], required=True,
+                                    default=lambda self: self.env['res.currency'].search([('ebay_available', '=', True)], limit=1).id,
+                                    config_parameter='ebay_currency')
     ebay_country = fields.Many2one("res.country", domain=[('ebay_available', '=', True)],
-                                   string="Country")
-    ebay_site = fields.Many2one("ebay.site", string="eBay Website")
-    ebay_zip_code = fields.Char(string="Zip")
-    ebay_location = fields.Char(string="Location")
+                                   string="Country",
+                                   default=lambda self: self.env['res.country'].search([('ebay_available', '=', True)], limit=1).id,
+                                   config_parameter='ebay_country')
+    ebay_site = fields.Many2one("ebay.site", string="eBay Website",
+                                default=lambda self: self.env['ebay.site'].search([], limit=1).id,
+                                config_parameter='ebay_site')
+    ebay_zip_code = fields.Char(string="Zip", default='', config_parameter='ebay_zip_code')
+    ebay_location = fields.Char(string="Location", default='', config_parameter='')
     ebay_out_of_stock = fields.Boolean("Out Of Stock", default=False)
-    ebay_sales_team = fields.Many2one("crm.team", string="ebay Sales Channel")
-    ebay_gallery_plus = fields.Boolean("Gallery Plus", default=False)
+    ebay_sales_team = fields.Many2one("crm.team", string="ebay Sales Channel",
+                                      default=lambda self: self.env['crm.team'].search([('team_type', '=', 'ebay')], limit=1).id,
+                                      config_parameter='ebay_sales_team')
+    ebay_gallery_plus = fields.Boolean("Gallery Plus", default='', config_parameter='ebay_gallery_plus')
 
     @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
         set_param = self.env['ir.config_parameter'].sudo().set_param
-
-        ebay_dev_id = self.ebay_dev_id or ''
-        sandbox_token = self.ebay_sandbox_token or ''
-        sandbox_app_id = self.ebay_sandbox_app_id or ''
-        sandbox_cert_id = self.ebay_sandbox_cert_id or ''
-        prod_token = self.ebay_prod_token or ''
-        prod_app_id = self.ebay_prod_app_id or ''
-        prod_cert_id = self.ebay_prod_cert_id or ''
-
-        set_param('ebay_dev_id', ebay_dev_id)
-        set_param('ebay_sandbox_token', sandbox_token)
-        set_param('ebay_sandbox_app_id', sandbox_app_id)
-        set_param('ebay_sandbox_cert_id', sandbox_cert_id)
-        set_param('ebay_prod_token', prod_token)
-        set_param('ebay_prod_app_id', prod_app_id)
-        set_param('ebay_prod_cert_id', prod_cert_id)
-
-        ebay_sales_team = self.ebay_sales_team or self.env['crm.team'].search([('team_type', '=', 'ebay')])[0]
-        domain = self.ebay_domain or ''
-        currency = self.ebay_currency or self.env['res.currency'].search([('ebay_available', '=', True)])[0]
         # by default all currencies active field is set to False except EUR and USD
         self.ebay_currency.active = True
-        country = self.ebay_country or self.env['res.country'].search([('ebay_available', '=', True)])[0]
-        site = self.ebay_site or self.env['ebay.site'].search([])[0]
-        zip_code = self.ebay_zip_code or ''
-        location = self.ebay_location or ''
-        gallery_plus = self.ebay_gallery_plus or ''
-
-        set_param('ebay_sales_team', ebay_sales_team.id)
-        set_param('ebay_domain', domain)
-        set_param('ebay_currency', currency.id)
-        set_param('ebay_country', country.id)
-        set_param('ebay_site', site.id)
-        set_param('ebay_zip_code', zip_code)
-        set_param('ebay_location', location)
-        set_param('ebay_gallery_plus', gallery_plus)
 
         out_of_stock = self.ebay_out_of_stock or ''
         if out_of_stock != self.env['ir.config_parameter'].get_param('ebay_out_of_stock'):
             set_param('ebay_out_of_stock', out_of_stock)
 
-            if domain == 'sand':
-                if sandbox_token and sandbox_cert_id and sandbox_app_id:
+            if self.ebay_domain == 'sand':
+                if self.ebay_sandbox_token and self.ebay_sandbox_cert_id and self.ebay_sandbox_app_id:
                     ebay_api = Trading(
                         domain='api.sandbox.ebay.com',
                         config_file=None,
-                        appid=sandbox_app_id,
+                        appid=self.ebay_sandbox_app_id,
                         devid="ed74122e-6f71-4877-83d8-e0e2585bd78f",
-                        certid=sandbox_cert_id,
-                        token=sandbox_token,
-                        siteid=site.ebay_id if site else 0)
+                        certid=self.ebay_sandbox_cert_id,
+                        token=self.ebay_sandbox_token,
+                        siteid=self.site.ebay_id if self.site else 0)
                     call_data = {
                         'OutOfStockControlPreference': 'true' if out_of_stock else 'false',
                     }
@@ -95,15 +69,15 @@ class ResConfigSettings(models.TransientModel):
                     except ConnectionError:
                         pass
             else:
-                if prod_token and prod_cert_id and prod_app_id:
+                if self.ebay_prod_token and self.ebay_prod_cert_id and self.ebay_prod_app_id:
                     ebay_api = Trading(
                         domain='api.ebay.com',
                         config_file=None,
-                        appid=prod_app_id,
+                        appid=self.ebay_prod_app_id,
                         devid="ed74122e-6f71-4877-83d8-e0e2585bd78f",
-                        certid=prod_cert_id,
-                        token=prod_token,
-                        siteid=site.ebay_id if site else 0)
+                        certid=self.ebay_prod_cert_id,
+                        token=self.ebay_prod_token,
+                        siteid=self.site.ebay_id if self.site else 0)
                     call_data = {
                         'OutOfStockControlPreference': 'true' if out_of_stock else 'false',
                     }
@@ -117,22 +91,7 @@ class ResConfigSettings(models.TransientModel):
         res = super(ResConfigSettings, self).get_values()
         get_param = self.env['ir.config_parameter'].sudo().get_param
         res.update(
-            ebay_dev_id=get_param('ebay_dev_id', default=''),
-            ebay_sandbox_token=get_param('ebay_sandbox_token', default=''),
-            ebay_sandbox_app_id=get_param('ebay_sandbox_app_id', default=''),
-            ebay_sandbox_cert_id=get_param('ebay_sandbox_cert_id', default=''),
-            ebay_prod_token=get_param('ebay_prod_token', default=''),
-            ebay_prod_app_id=get_param('ebay_prod_app_id', default=''),
-            ebay_prod_cert_id=get_param('ebay_prod_cert_id', default=''),
-            ebay_domain=get_param('ebay_domain', default='sand'),
-            ebay_currency=int(get_param('ebay_currency', default=self.env.ref('base.USD'))),
-            ebay_country=int(get_param('ebay_country', default=self.env.ref('base.us'))),
-            ebay_site=int(get_param('ebay_site', default=self.env['ebay.site'].search([])[0])),
-            ebay_zip_code=get_param('ebay_zip_code'),
-            ebay_location=get_param('ebay_location'),
             ebay_out_of_stock=get_param('ebay_out_of_stock', default=False),
-            ebay_sales_team=int(get_param('ebay_sales_team', default=self.env['crm.team'].search([('team_type', '=', 'ebay')], limit=1))),
-            ebay_gallery_plus=get_param('ebay_gallery_plus'),
         )
         return res
 

account_taxcloud/models/res_config_settings.py
@@ -9,26 +9,9 @@ from .taxcloud_request import TaxCloudRequest
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    taxcloud_api_id = fields.Char(string='TaxCloud API ID')
-    taxcloud_api_key = fields.Char(string='TaxCloud API KEY')
+    taxcloud_api_id = fields.Char(string='TaxCloud API ID', default='', config_parameter='account_taxcloud.taxcloud_api_id')
+    taxcloud_api_key = fields.Char(string='TaxCloud API KEY', default='', config_parameter='account_taxcloud.taxcloud_api_key')
     tic_category_id = fields.Many2one(related='company_id.tic_category_id', string="Default TIC Code")
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        Param = self.env['ir.config_parameter'].sudo()
-        Param.set_param("account_taxcloud.taxcloud_api_id", (self.taxcloud_api_id or '').strip())
-        Param.set_param("account_taxcloud.taxcloud_api_key", (self.taxcloud_api_key or '').strip())
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            taxcloud_api_id=params.get_param('account_taxcloud.taxcloud_api_id', default=''),
-            taxcloud_api_key=params.get_param('account_taxcloud.taxcloud_api_key', default=''),
-        )
-        return res
 
     @api.multi
     def sync_taxcloud_category(self):

hr_contract_salary/models/res_config_settings.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    access_token_validity = fields.Integer(string='Default Access Token Validity Duration')
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            access_token_validity=int(params.get_param('hr_contract_salary.access_token_validity', default=30))
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param("hr_contract_salary.access_token_validity", self.access_token_validity)
+    access_token_validity = fields.Integer(string='Default Access Token Validity Duration',
+                                           default=30,
+                                           config_parameter='hr_contract_salary.access_token_validity')

mail_push/models/res_config_settings.py
@@ -1,33 +1,12 @@
 # -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    mail_push_notification = fields.Boolean('Notifications', oldname="default_mail_push_notification")
-    fcm_api_key = fields.Char('Server API Key')
-    fcm_project_id = fields.Char('Sender ID')
-
-    def get_fcm_credentials(self):
-        params = self.env['ir.config_parameter'].sudo()
-        return dict(
-            mail_push_notification=params.get_param('mail_push.mail_push_notification', default=False),
-            fcm_api_key=params.get_param('mail_push.fcm_api_key', default=''),
-            fcm_project_id=params.get_param('mail_push.fcm_project_id', default=''),
-        )
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(self.get_fcm_credentials())
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        params = self.env['ir.config_parameter'].sudo()
-        params.set_param("mail_push.mail_push_notification", self.mail_push_notification)
-        params.set_param('mail_push.fcm_api_key', self[0].fcm_api_key or '')
-        params.set_param('mail_push.fcm_project_id', self[0].fcm_project_id or '')
+    mail_push_notification = fields.Boolean('Notifications', oldname="default_mail_push_notification",
+                                            config_parameter='mail_push.mail_push_notification')
+    fcm_api_key = fields.Char('Server API Key', default='', config_parameter='mail_push.fcm_api_key')
+    fcm_project_id = fields.Char('Sender ID', default='', config_parameter='mail_push.fcm_project_id')

timesheet_grid_sale/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 from odoo.addons.timesheet_grid_sale.models.sale import DEFAULT_INVOICED_TIMESHEET
 
@@ -12,17 +12,4 @@ class ResConfigSettings(models.TransientModel):
     invoiced_timesheet = fields.Selection([
         ('all', "All recorded timesheets"),
         ('approved', "Approved timesheets only"),
-    ], default='all', string="Timesheets Invoicing")
-
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('sale.invoiced_timesheet', self.invoiced_timesheet)
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            invoiced_timesheet=params.get_param('sale.invoiced_timesheet', DEFAULT_INVOICED_TIMESHEET)
-        )
-        return res
+    ], default=DEFAULT_INVOICED_TIMESHEET, string="Timesheets Invoicing", config_parameter='sale.invoiced_timesheet')

voip/models/res_config_settings.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    wsServer = fields.Char("WebSocket", help="The URL of your WebSocket")
-    pbx_ip = fields.Char("PBX Server IP", help="The IP adress of your PBX Server")
+    wsServer = fields.Char("WebSocket", help="The URL of your WebSocket",
+                           default='ws://localhost', config_parameter='voip.wsServer')
+    pbx_ip = fields.Char("PBX Server IP", help="The IP adress of your PBX Server",
+                         default='localhost', config_parameter='voip.pbx_ip')
     mode = fields.Selection([
         ('demo', 'Demo'),
         ('prod', 'Production'),
-    ], string="Mode")
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        params = self.env['ir.config_parameter'].sudo()
-        params.set_param('voip.pbx_ip', self[0].pbx_ip)
-        params.set_param('voip.wsServer', self[0].wsServer)
-        params.set_param('voip.mode', self[0].mode)
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            pbx_ip=params.get_param('voip.pbx_ip', default='localhost'),
-            wsServer=params.get_param('voip.wsServer', default='ws://localhost'),
-            mode=params.get_param('voip.mode', default="demo"),
-        )
-        return res
+    ], string="Mode", default='demo', config_parameter='voip.mode')

voip_onsip/models/res_config_settings.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            wsServer=params.get_param('voip.wsServer', default='wss://edge.sip.onsip.com'),
-        )
-        return res
+    wsServer = fields.Char(default='wss://edge.sip.onsip.com', config_parameter='voip.wsServer')

mail_push/models/mail_channel.py
@@ -52,7 +52,12 @@ class MailChannel(models.Model):
                         method(service_identities, message)
 
     def _get_default_fcm_credentials(self):
-        return self.env['res.config.settings'].sudo().get_fcm_credentials()
+        params = self.env['ir.config_parameter'].sudo()
+        return dict(
+            mail_push_notification=params.get_param('mail_push.mail_push_notification', default=False),
+            fcm_api_key=params.get_param('mail_push.fcm_api_key', default=''),
+            fcm_project_id=params.get_param('mail_push.fcm_project_id', default=''),
+        )
 
     @api.model
     def _push_notify_fcm(self, identities, message):

commit 7aa7a9a1b31e9543874b5bcaaad88a728b8830f0

[FIX] sale_ebay: get the right parameter when configuring settings
Fine-tuning of commit 39fb3be0ef0a95366ecd1bc5112a944de060612b,
which introduced a typo in getting the ebay site field.
We also deduplicate the lines using that parameter.

opw 1904906

closes odoo/enterprise#3053

sale_ebay/models/res_config_settings.py
@@ -50,6 +50,7 @@ class ResConfigSettings(models.TransientModel):
         out_of_stock = self.ebay_out_of_stock or ''
         if out_of_stock != self.env['ir.config_parameter'].get_param('ebay_out_of_stock'):
             set_param('ebay_out_of_stock', out_of_stock)
+            siteid = self.ebay_site.ebay_id if self.ebay_site else 0
 
             if self.ebay_domain == 'sand':
                 if self.ebay_sandbox_token and self.ebay_sandbox_cert_id and self.ebay_sandbox_app_id:
@@ -60,7 +61,7 @@ class ResConfigSettings(models.TransientModel):
                         devid="ed74122e-6f71-4877-83d8-e0e2585bd78f",
                         certid=self.ebay_sandbox_cert_id,
                         token=self.ebay_sandbox_token,
-                        siteid=self.site.ebay_id if self.site else 0)
+                        siteid=siteid)
                     call_data = {
                         'OutOfStockControlPreference': 'true' if out_of_stock else 'false',
                     }
@@ -77,7 +78,7 @@ class ResConfigSettings(models.TransientModel):
                         devid="ed74122e-6f71-4877-83d8-e0e2585bd78f",
                         certid=self.ebay_prod_cert_id,
                         token=self.ebay_prod_token,
-                        siteid=self.site.ebay_id if self.site else 0)
+                        siteid=siteid)
                     call_data = {
                         'OutOfStockControlPreference': 'true' if out_of_stock else 'false',
                     }