bits-amitrathod/enterprise

commit 00e6b9380ee00e7c9ee946a260d197cb299c5670

[REF] *: renaming AppSwitcher to HomeMenu
Summary:
   AppSwitcher      => HomeMenu
   AppSwitcherMenu  => HomeStudioMenu

Details:
   application_switcher => home_menu
   application-switcher => home-menu
   AppSwitcher          => HomeMenu
   appswitcher          => home_menu
   AppSwitcherMenu      => HomeStudioMenu
   appswitcher_menu     => home_studio_menu
   app_switcher         => home_menu
   app-switcher         => home-menu
   app_switcher_menu    => home_studio_menu

web_enterprise/static/src/js/home_menu.js
@@ -1,4 +1,4 @@
-odoo.define('web_enterprise.AppSwitcher', function (require) {
+odoo.define('web_enterprise.HomeMenu', function (require) {
 "use strict";
 
 var config = require('web.config');
@@ -9,8 +9,8 @@ var Widget = require('web.Widget');
 var QWeb = core.qweb;
 var NBR_ICONS = 6;
 
-var AppSwitcher = Widget.extend({
-    template: 'AppSwitcher',
+var HomeMenu = Widget.extend({
+    template: 'HomeMenu',
     events: {
         'click .o_menuitem': '_onMenuitemClick',
         'input input.o_menu_search_input': '_onMenuSearchInput',
@@ -33,7 +33,7 @@ var AppSwitcher = Widget.extend({
     start: function () {
         this.$input = this.$('input');
         this.$menuSearch = this.$('.o_menu_search');
-        this.$mainContent = this.$('.o_application_switcher_scrollable');
+        this.$mainContent = this.$('.o_home_menu_scrollable');
         return this._super.apply(this, arguments);
     },
     /**
@@ -160,7 +160,7 @@ var AppSwitcher = Widget.extend({
      */
     _render: function () {
         this.$menuSearch.toggleClass('o_bar_hidden', !this._state.isSearching);
-        this.$mainContent.html(QWeb.render('AppSwitcher.Content', { widget: this }));
+        this.$mainContent.html(QWeb.render('HomeMenu.Content', { widget: this }));
         var $focused = this.$mainContent.find('.o_focused');
         if ($focused.length && !config.device.isMobile) {
             if (!this._state.isComposing) {
@@ -293,13 +293,13 @@ var AppSwitcher = Widget.extend({
                 break;
             case $.ui.keyCode.ESCAPE:
                 // clear text on search, hide it if no content before ESC
-                // hide app switcher if there is an inner action
+                // hide home menu if there is an inner action
                 this._state = this._getInitialState();
                 this._state.isSearching = $input.val().length > 0;
                 $input.val("");
                 this._update({focus: 0, search: $input.val()});
                 if (!this._state.isSearching) {
-                    this.trigger_up('hide_app_switcher');
+                    this.trigger_up('hide_home_menu');
                 }
                 break;
             default:
@@ -345,7 +345,7 @@ var AppSwitcher = Widget.extend({
     }
 });
 
-return AppSwitcher;
+return HomeMenu;
 
 });
 
@@ -355,12 +355,12 @@ odoo.define('web_enterprise.ExpirationPanel', function (require) {
 var core = require('web.core');
 var session = require('web.session');
 var utils = require('web.utils');
-var AppSwitcher = require('web_enterprise.AppSwitcher');
+var HomeMenu = require('web_enterprise.HomeMenu');
 
 var QWeb = core.qweb;
 
-AppSwitcher.include({
-    events: _.extend(AppSwitcher.prototype.events, {
+HomeMenu.include({
+    events: _.extend(HomeMenu.prototype.events, {
         'click .oe_instance_buy': '_onEnterpriseBuy',
         'click .oe_instance_renew': '_onEnterpriseRenew',
         'click .oe_instance_upsell': '_onEnterpriseUpsell',

web_studio/static/src/js/home_menu.js
@@ -1,11 +1,11 @@
-odoo.define('web_studio.AppSwitcher', function (require) {
+odoo.define('web_studio.HomeMenu', function (require) {
 "use strict";
 
 var core = require('web.core');
 var session = require('web.session');
 var WebClient = require('web.WebClient');
 var web_client = require('web.web_client');
-var AppSwitcher = require('web_enterprise.AppSwitcher');
+var HomeMenu = require('web_enterprise.HomeMenu');
 
 var bus = require('web_studio.bus');
 
@@ -13,12 +13,12 @@ var QWeb = core.qweb;
 
 /*
  * Notice:
- *  some features (like seeing the appswitcher background) are available
+ *  some features (like seeing the home menu background) are available
  *  even the user is not a system user, this is why there are two different
  *  includes in this file.
  */
 
-AppSwitcher.include({
+HomeMenu.include({
     /**
      * @override
      */
@@ -41,7 +41,7 @@ AppSwitcher.include({
         return this._super.apply(this, arguments);
     },
     /**
-     * Put the appswitcher background as the cover of current `$el`.
+     * Put the home menu background as the cover of current `$el`.
      *
      * @private
      */
@@ -62,14 +62,14 @@ AppSwitcher.include({
 
 WebClient.include({
     /**
-     * Adds a class on the webclient on top of the o_app_switcher_background
-     * class to inform that the appswitcher is customized.
+     * Adds a class on the webclient on top of the o_home_menu_background
+     * class to inform that the home menu is customized.
      *
      * @override
      */
-    toggle_app_switcher: function (display) {
+    toggle_home_menu: function (display) {
         this._super.apply(this, arguments);
-        this.$el.toggleClass('o_app_switcher_background_custom', display && !!this.menu_data.background_image);
+        this.$el.toggleClass('o_home_menu_background_custom', display && !!this.menu_data.background_image);
     },
 });
 
@@ -77,8 +77,8 @@ if (!session.is_system) {
     return;
 }
 
-AppSwitcher.include({
-    events: _.extend(AppSwitcher.prototype.events, {
+HomeMenu.include({
+    events: _.extend(HomeMenu.prototype.events, {
         'click .o_web_studio_new_app': '_onNewApp',
     }),
     /**
@@ -121,7 +121,7 @@ AppSwitcher.include({
             return;
         }
         if (display) {
-            this.on_detach_callback();  // de-bind hanlders on appswitcher
+            this.on_detach_callback();  // de-bind hanlders on home menu
             this.in_DOM = true;  // avoid effect of on_detach_callback
             this._renderNewApp();
         } else {

event_barcode/static/src/xml/event_barcode.xml
@@ -3,7 +3,7 @@
 <templates xml:space="preserve">
 
     <t t-name="event_barcode_template">
-        <div class="o_event_barcode_bg o_app_switcher_background">
+        <div class="o_event_barcode_bg o_home_menu_background">
             <div class="o_event_barcode_detail text-center mt32">
                 <div class="col-md-3 col-xs-12 o_event_date">
                     <span class="o_event_day mt24"><t t-esc="widget.data.date"/></span>

stock_barcode/static/src/xml/stock_barcode.xml
@@ -3,7 +3,7 @@
 <templates xml:space="preserve">
 
     <t t-name="main_menu">
-        <div class="o_stock_barcode_main_menu_container o_app_switcher_background">
+        <div class="o_stock_barcode_main_menu_container o_home_menu_background">
             <div class="o_stock_barcode_main_menu">
                 <h1>Barcode Scanning</h1>
                 <div t-if="widget.message_demo_barcodes" class="message_demo_barcodes alert alert-info alert-dismissible" role="alert">

voip/static/src/xml/dialing_panel.xml
@@ -10,7 +10,7 @@
 </t>
 
 <t t-name="voip.DialingPanel">
-  <div class="o_dial o_in_appswitcher">
+  <div class="o_dial o_in_home_menu">
     <div class="o_dial_fold text-right">
       <span>
         <a href="#" class="o_dial_window_close fa fa-close"/>

web_enterprise/static/src/js/menu.js
@@ -16,7 +16,7 @@ var Menu = Widget.extend({
     template: 'Menu',
     menusTemplate: 'Menu.sections',
     events: {
-        'click .o_menu_toggle': '_onToggleAppSwitcher',
+        'click .o_menu_toggle': '_onToggleHomeMenu',
         'mouseover .o_menu_sections > li:not(.open)': '_onMouseOverMenu',
         'click .o_menu_brand': '_onAppNameClicked',
     },
@@ -24,7 +24,7 @@ var Menu = Widget.extend({
     init: function (parent, menu_data) {
         var self = this;
         this._super.apply(this, arguments);
-        this.appswitcher_displayed = true;
+        this.home_menu_displayed = true;
         this.backbutton_displayed = false;
 
         this.$menu_sections = {};
@@ -72,20 +72,20 @@ var Menu = Widget.extend({
 
         return this._super.apply(this, arguments);
     },
-    toggle_mode: function (appswitcher, overapp) {
-        this.appswitcher_displayed = !!appswitcher;
-        this.backbutton_displayed = this.appswitcher_displayed && !!overapp;
+    toggle_mode: function (home_menu, overapp) {
+        this.home_menu_displayed = !!home_menu;
+        this.backbutton_displayed = this.home_menu_displayed && !!overapp;
 
-        this.$menu_toggle.toggleClass('fa-chevron-left', this.appswitcher_displayed)
-                         .toggleClass('fa-th', !this.appswitcher_displayed);
-        if (this.appswitcher_displayed && !this.backbutton_displayed) {
+        this.$menu_toggle.toggleClass('fa-chevron-left', this.home_menu_displayed)
+                         .toggleClass('fa-th', !this.home_menu_displayed);
+        if (this.home_menu_displayed && !this.backbutton_displayed) {
             this.$menu_toggle.removeAttr('accesskey');
         } else {
             this.$menu_toggle.attr('accesskey', 'h');
         }
-        this.$menu_toggle.toggleClass('hidden', this.appswitcher_displayed && !this.backbutton_displayed);
-        this.$menu_brand_placeholder.toggleClass('hidden', this.appswitcher_displayed);
-        this.$section_placeholder.toggleClass('hidden', this.appswitcher_displayed);
+        this.$menu_toggle.toggleClass('hidden', this.home_menu_displayed && !this.backbutton_displayed);
+        this.$menu_brand_placeholder.toggleClass('hidden', this.home_menu_displayed);
+        this.$section_placeholder.toggleClass('hidden', this.home_menu_displayed);
     },
     change_menu_section: function (primary_menu_id) {
         if (!this.$menu_sections[primary_menu_id]) {
@@ -232,9 +232,9 @@ var Menu = Widget.extend({
      * @private
      * @param {MouseEvent} ev
      */
-    _onToggleAppSwitcher: function (ev) {
+    _onToggleHomeMenu: function (ev) {
         ev.preventDefault();
-        this.trigger_up(this.appswitcher_displayed ? 'hide_app_switcher' : 'show_app_switcher');
+        this.trigger_up(this.home_menu_displayed ? 'hide_home_menu' : 'show_home_menu');
         this.$el.parent().removeClass('o_mobile_menu_opened');
     },
 });

web_enterprise/static/src/js/menu_mobile.js
@@ -121,7 +121,7 @@ Menu.include({
 
         // update the burger menu content: either display the submenus (if we
         // are in an app, and if it contains submenus) or the user menu
-        var displaySubMenus = !this.appswitcher_displayed;
+        var displaySubMenus = !this.home_menu_displayed;
         if (displaySubMenus) {
             var app = _.findWhere(this.menu_data.children, {id: this.current_primary_menu});
             displaySubMenus = !!(app && app.children.length);

commit abcb3cc9e3a540ca97309742de9163daa640ea8d

[FIX] web_enterprise: crash when searching on the home menu
- The forward port 00e6b9380ee00e7c9ee946a260d197cb299c5670 has introduced a bug
  where searching on the home menu could display a javascript error.

  This commit fixes the bug.

closes odoo/enterprise#3270

web_enterprise/static/src/js/home_menu.js
@@ -311,7 +311,7 @@ var HomeMenu = Widget.extend({
             case 67: // c
             case 88: // x
                 // keep focus and selection on keyboard copy and cut
-                if (event.ctrlKey || event.metaKey) {
+                if (ev.ctrlKey || ev.metaKey) {
                     break;
                 }
             default: