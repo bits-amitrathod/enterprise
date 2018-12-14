03a6ed642c1024c10315fd518de5cf5611d38b1f
Denis Ledoux <dle@odoo.com>
2018-12-14 11:03:20 +0000

    [IMP] stock_barcode: get_all_products_by_barcode performance
    - By using a dict to get the product of packages,
      instead of looping on all the products until
      finding the good one. The list of product can be
      quite huge, and using a dict is more efficient
    - By reading missing products (not having a barcode)
      all at once,instead of one by one.
    
    opw-1914064
    
    closes odoo/enterprise#3269

--- stock_barcode/models/product_product.py
+++ stock_barcode/models/product_product.py
@@ -19,11 +19,15 @@ class Product(models.Model):
         )
         # for each packaging, grab the corresponding product data
         to_add = []
+        to_read = []
+        products_by_id = {product['id']: product for product in products}
         for packaging in packagings:
-            for product in products:
-                if packaging['product_id'] == product['id']:
-                    to_add.append(dict(product, **{'qty': packaging['qty']}))
-                    break
+            if products_by_id.get(packaging['product_id']):
+                product = products_by_id[packaging['product_id']]
+                to_add.append(dict(product, **{'qty': packaging['qty']}))
             # if the product doesn't have a barcode, you need to read it directly in the DB
-            to_add.append(dict(packaging, **self.env['product.product'].browse(packaging['product_id'][0]).sudo().read(['display_name', 'uom_id', 'tracking'])[0]))
+            to_read.append((packaging, packaging['product_id'][0]))
+        products_to_read = self.env['product.product'].browse(list(set(t[1] for t in to_read))).sudo().read(['display_name', 'uom_id', 'tracking'])
+        products_to_read = {product['id']: product for product in products_to_read}
+        to_add.extend([dict(t[0], **products_to_read[t[1]]) for t in to_read])
         return {product.pop('barcode'): product for product in products + to_add}
