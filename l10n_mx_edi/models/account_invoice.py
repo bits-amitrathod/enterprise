fee4740b69a7e247a8c7708073abd523fc42654f
Luis Torres <luis_t@vauxoo.com>
2018-12-14 00:02:11 +0000

    [IMP] l10n_mx_edi: Support cases with taxes in amount fixed
    In some cases, the IEPS must be configured like amount fixed, example in
    drinks the SAT indicate that must be applied 1 peso per litre.
    
    https://www.sifei.com.mx/blog/noticias-sifei-1/post/tienes-idea-de-que-es-el-ieps-87
    
    Odoo support this cases and now also the Mexican localization.
    
    Was improved the way like get the taxes amount to IEPS with amount fix.
    
    closes odoo/enterprise#3311

--- l10n_mx_edi/models/account_invoice.py
+++ l10n_mx_edi/models/account_invoice.py
@@ -673,12 +673,13 @@ class AccountInvoice(models.Model):
                 tax_dict = tax_line.get(tax.id, {})
                 amount = round(abs(tax_dict.get(
                     'amount', tax.amount / 100 * float("%.2f" % line.price_subtotal))), 2)
+                rate = round(abs(tax.amount), 2)
                 if tax.amount not in taxes:
                     taxes.update({tax.amount: {
                         'name': (tax.tag_ids[0].name
                                  if tax.tag_ids else tax.name).upper(),
                         'amount': amount,
-                        'rate': round(abs(tax.amount), 2),
+                        'rate': rate if tax.amount_type == 'fixed' else rate / 100.0,
                         'type': tax.l10n_mx_cfdi_tax_type,
                         'tax_amount': tax_dict.get('amount', tax.amount),
                     }})
