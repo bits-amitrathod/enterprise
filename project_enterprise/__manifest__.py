Raniani-lab/enterpriise

[ADD] project_enterprise: rename settings labels when enterprise
Timesheet feature has been moved in enterprise (naming
differently). "Task logs" is the community label, but is
still visible when enteprise is installed. This commit
provides a module to rename this into "Timesheets", to
avoid user confusion.

Task-1893021

project_enterprise/__manifest__.py
@@ -1,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Project Enterprise",
+    'summary': """Bridge module for mail and enterprise""",
+    'description': """
+Bridge module for mail and enterprise
+    """,
+    'category': 'Project',
+    'version': '1.0',
+    'depends': ['project'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'auto_intall': True,
+    'license': 'OEEL-1',
+}

bits-amitrathod/enterprise

Essentially a backport of 9af941428880af8e1810dfd9feef494d7
opw 1931173
closes odoo/enterprise#3517

project_enterprise/__manifest__.py
@@ -1,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Project Enterprise",
+    'summary': """Bridge module for project and enterprise""",
+    'description': """
+Bridge module for project and enterprise
+    """,
+    'category': 'Project',
+    'version': '1.0',
+    'depends': ['project'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'OEEL-1',
+}