e33f229f89668de8772ea4813a8967d1b0565a4f
Lucas Perais (lpe) <lpe@odoo.com>
2019-02-05 10:56:43 +0000

    [FIX] account_reports: general ledger search string
    In the general ledger, search for a string in the accounts
    
    Before this commit:
    - the JS searched anywhere in the name of the account, while python
    searched only with startswith
    - It created a discrepancy causing the deletion of a line when clicking on it
    
    After this commit:
    - the search is harmonized between python and JS which is:
    case insensitive, only searching at the beginning of each part of the account name
    - hence, there is no discrepancy  between the server and the client, so the case
    where clicking on an account to unfold makes that line disappear doesn't occur anymore
    
    OPW 1932048
    
    closes odoo/enterprise#3584

--- account_reports/models/account_general_ledger.py
+++ account_reports/models/account_general_ledger.py
@@ -297,7 +297,7 @@ class report_account_general_ledger(models.AbstractModel):
             display_name = account.code + " " + account.name
             if options.get('filter_accounts'):
                 #skip all accounts where both the code and the name don't start with the given filtering string
-                if not any([display_name_part.startswith(options.get('filter_accounts')) for display_name_part in display_name.split(' ')]):
+                if not any([display_name_part.lower().startswith(options['filter_accounts'].lower()) for display_name_part in display_name.split(' ')]):
                     continue
             debit = grouped_accounts[account]['debit']
             credit = grouped_accounts[account]['credit']
