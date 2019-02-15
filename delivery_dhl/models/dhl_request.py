347131b165dbf882eb4c1fe8c38ee5dde983fc8c
sbbaro <sbb@odoo.com>
2019-02-15 15:21:27 +0000

    [FIX] delivery_dhl: add company name to label
    If the partner used for a delivery has a parent company,
    we use it to fill DHL's "CompanyName" field.
    
    opw 1924201
    
    closes odoo/enterprise#3662
    
    Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

--- delivery_dhl/models/dhl_request.py
+++ delivery_dhl/models/dhl_request.py
@@ -306,7 +306,10 @@ class DHLProvider():
             etree.SubElement(billing_node, "DutyPaymentType").text = "S"
 
         consignee_node = etree.SubElement(root, "Consignee")
-        etree.SubElement(consignee_node, "CompanyName").text = self._remove_accents(param["recipient_partner"].name)
+        if param["recipient_partner"].parent_id:
+            etree.SubElement(consignee_node, "CompanyName").text = self._remove_accents(param["recipient_partner"].parent_id.name)
+        else:
+            etree.SubElement(consignee_node, "CompanyName").text = self._remove_accents(param["recipient_partner"].name)
         etree.SubElement(consignee_node, "AddressLine").text = self._remove_accents(param["recipient_streetLines"])
         etree.SubElement(consignee_node, "City").text = self._remove_accents(param["recipient_partner"].city)
 
