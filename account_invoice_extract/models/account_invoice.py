1bb360b20ea5368f91572f4a153e3d63fc084162
Nans Lefebvre <len@odoo.com>
2019-04-04 07:22:15 +0000

    [FIX] account_invoice_extract: fix show OCR button dependence
    The _compute_show_send/resend_button methods did not actually depend
    on message_ids, but on message_main_attachment_id.
    As a side-effect, the two were essentially the same as long as only attachments
    in the thread were used.
    Since the series of commit to use all the attachments linked to the record,
    it makes sense to actually use the correct dependence
    (since it can be modified at attachment upload and not only at message_post).
    
    opw 1950403

--- account_invoice_extract/models/account_invoice.py
+++ account_invoice_extract/models/account_invoice.py
@@ -65,14 +65,14 @@ class AccountInvoice(models.Model):
             can_show = False
         return can_show
 
-    @api.depends('state', 'extract_state', 'message_ids')
+    @api.depends('state', 'extract_state', 'message_main_attachment_id')
     def _compute_show_resend_button(self):
         for record in self:
             record.extract_can_show_resend_button = self._compute_can_show_send_resend(record)
             if record.extract_state not in ['error_status', 'not_enough_credit', 'module_not_up_to_date']:
                 record.extract_can_show_resend_button = False
 
-    @api.depends('state', 'extract_state', 'message_ids')
+    @api.depends('state', 'extract_state', 'message_main_attachment_id')
     def _compute_show_send_button(self):
         for record in self:
             record.extract_can_show_send_button = self._compute_can_show_send_resend(record)
