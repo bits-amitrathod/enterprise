12710b1451ac2a584c84ee57a1f5819f11499320
Lucas Perais (lpe) <lpe@odoo.com>
2019-04-16 07:59:14 +0000

    [FIX] sale_contract: keep end date set by user when starting a subscription
    Create a subscription, set an end date on it.
    Start the subscription
    
    Before this commit, the end date was set to false. Obviously it was writable afterwards
    
    After this commit, we keep the end date that the user set before starting the subscription
    
    OPW 1925915
    
    closes odoo/enterprise#4113
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- sale_contract/models/sale_subscription.py
+++ sale_contract/models/sale_subscription.py
@@ -162,7 +162,7 @@ class SaleSubscription(models.Model):
 
     @api.multi
     def set_open(self):
-        return self.write({'state': 'open', 'date': False})
+        return self.write({'state': 'open'})
 
     @api.multi
     def set_pending(self):
