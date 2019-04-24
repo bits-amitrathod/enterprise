3d4fcdda3e063dcaf1d4a65567c6cefc88b5c33d
Goffin Simon <sig@odoo.com>
2019-04-24 13:53:02 +0000

    [FIX] sale_subscription: Subtotal on Subscription Lines only computed after Saving
    Steps to reproduce the bug:
    - Create a user A with subscription Manager access rights
    - Create a subscription and add lines
    
    Bug:
    
    The subtotal was only computed after clicking on save
    
    Technically, when adding a product on a subscription line, a new record in cache is created.
    When making a sudo in fuction _compute_price_subtotal, the environment is changed
    So the environments of variable line and sudo_line are not the same and checking
    the pricelist with line and applying it on subtotal with sudo_line is not consistent
    as this is not the same environment. In this case, the currency of the pricelist was defined
    with line but not with sudo_line and then the price_subtotal was set to 0.0
    
    Forward-port up to saas-11.3
    
    opw:1962988
    
    closes odoo/enterprise#4189
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- sale_subscription/models/sale_subscription.py
+++ sale_subscription/models/sale_subscription.py
@@ -670,7 +670,7 @@ class SaleSubscriptionLine(models.Model):
             line_sudo = line.sudo()
             price = line.env['account.tax']._fix_tax_included_price(line.price_unit, line_sudo.product_id.taxes_id, [])
             line.price_subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
-            if line.analytic_account_id.pricelist_id:
+            if line_sudo.analytic_account_id.pricelist_id.currency_id:
                 line.price_subtotal = line_sudo.analytic_account_id.pricelist_id.currency_id.round(line.price_subtotal)
 
     @api.onchange('product_id')
