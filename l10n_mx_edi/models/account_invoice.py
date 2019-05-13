74187d582421037051a88801e70f95020f5d0846
Gabriela Mogollon <gmogollon@vauxoo.com>
2019-05-13 17:07:38 +0000

    [FIX] l10n_mx_edi: disable after commit if there is a transaction payment
    This fix tries to avoid the raise error `Some of the invoices that will
    be paid with this record are not signed, and the UUID is required to
    indicate the invoices that are paid with this CFDI`, when an invoice with
    transactions payment is validated.
    
    Currently, to link payments to invoice the invoice needs to be signed
    but the signed service is called after commit to avoid sign an invoice
    two times in case of errors. But to avoid no be able to validate
    invoices from e-commerce sales we are disabling the after commit process
    only for this kind of invoice.
    
    Note: If the signing service fails, the raise error will be shown so
    it'll be more difficult to know what kind of error the process has (this
    needs to be improved in the master version).
    
    closes odoo/enterprise#4347
    
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- l10n_mx_edi/models/account_invoice.py
+++ l10n_mx_edi/models/account_invoice.py
@@ -983,6 +983,7 @@ class AccountInvoice(models.Model):
         '''Generates the cfdi attachments for mexican companies when validated.'''
         result = super(AccountInvoice, self).invoice_validate()
         version = self.l10n_mx_edi_get_pac_version()
+        trans_field = 'transaction_ids' in self._fields
         for record in self.filtered(lambda r: r.l10n_mx_edi_is_required()):
             if record.type == 'out_refund' and (
                 record.refund_invoice_id and not record.refund_invoice_id.l10n_mx_edi_cfdi_uuid):
@@ -997,7 +998,7 @@ class AccountInvoice(models.Model):
             subscription = 'subscription_id' in record.invoice_line_ids._fields and record.invoice_line_ids.filtered(
                 'subscription_id')
             ctx = {}
-            if subscription:
+            if subscription or (trans_field and record.mapped('transaction_ids.payment_id')):
                 ctx = {'disable_after_commit': True}
             record.with_context(**ctx)._l10n_mx_edi_retry()
         return result
