020589947a3c97e3878ef83eab27fb4ba3dc40fd
Antoine Prieels <anp@odoo.com>
2019-05-23 12:25:28 +0000

    [FIX] iot: stopPolling not aborting requests
    When adding a listener to the longpolling, we call `stopPolling` to
    abort all requests currently working. Then, we start a new request with
    `startPolling`.
    
    `stopPolling` was not working as expected and previous requests were
    not killed, resulting in a huge number of useless requests still
    running.
    
    The problem was that, when we got an answer for the current request, we
    first sent the new request before removing the reference to the old
    one. Instead of setting the reference to the old request to `false`, we
    overrode the reference to the new one, that was still running. The
    stopPolling method was then not able to kill the request since it
    didn't have its reference.
    
    closes odoo/enterprise#4415
    
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>

--- iot/static/src/js/iot.js
+++ iot/static/src/js/iot.js
@@ -457,6 +457,7 @@ var IoTLongpolling = BusService.extend({
         // The backend has a maximum cycle time of 50 seconds so give +10 seconds
         this._rpcIoT(iot_ip, this.POLL_ROUTE, data, options)
             .then(function (result) {
+                self._listeners[iot_ip].rpc = false;
                 if (result.result) {
                     if (self._session_id === result.result.session_id) {
                         self._onSuccess(iot_ip, result.result);
@@ -464,7 +465,6 @@ var IoTLongpolling = BusService.extend({
                 } else {
                     self._onError();
                 }
-                self._listeners[iot_ip].rpc = false;
             }).fail(function (jqXHR, textStatus) {
                 if (textStatus === 'error' && self.parseURL.protocol === 'https:') {
                     self._doWarnCertificate(iot_ip);
