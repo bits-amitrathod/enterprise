f8e32839dd324fb6c44c167243bce9094fbbb396
Nans Lefebvre <len@odoo.com>
2019-06-05 09:15:50 +0000

    [FIX] sale_ebay: replace deprecated api.one from stock methods by api.multi
    Changing the api.multi of a method into api.one implies a change of behaviour.
    There is absolutely no reason for such a thing to happen; the override of stock
    methods in this module exist only to add a synchronisation, so it should not
    touch the api. This change persisted because it was simply never updated.
    
    opw 2004727
    
    closes odoo/enterprise#4515
    
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- sale_ebay/models/stock_picking.py
+++ sale_ebay/models/stock_picking.py
@@ -7,26 +7,27 @@ from odoo import models, api
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.one
+    @api.multi
     def action_done(self):
         result = super(StockPicking, self).action_done()
         self._ebay_update_carrier(transfered=True)
         return result
 
-    @api.one
+    @api.multi
     def _ebay_update_carrier(self, transfered=False):
-        so = self.env['sale.order'].search([('name', '=', self.origin), ('origin', 'like', 'eBay')])
-        if so.order_line.filtered(lambda line: line.product_id.product_tmpl_id.ebay_use):
-            call_data = {
-                'OrderLineItemID': so.client_order_ref,
-            }
-            if transfered:
-                call_data['Shipped'] = True
-            if self.carrier_tracking_ref and self.carrier_id:
-                call_data['Shipment'] = {
-                    'ShipmentTrackingDetails': {
-                        'ShipmentTrackingNumber': self.carrier_tracking_ref,
-                        'ShippingCarrierUsed': self.carrier_id.name,
-                    },
+        for picking in self:
+            so = self.env['sale.order'].search([('name', '=', picking.origin), ('origin', 'like', 'eBay')])
+            if so.order_line.filtered(lambda line: line.product_id.product_tmpl_id.ebay_use):
+                call_data = {
+                    'OrderLineItemID': so.client_order_ref,
                 }
-            self.env['product.template'].ebay_execute("CompleteSale", call_data)
+                if transfered:
+                    call_data['Shipped'] = True
+                if picking.carrier_tracking_ref and picking.carrier_id:
+                    call_data['Shipment'] = {
+                        'ShipmentTrackingDetails': {
+                            'ShipmentTrackingNumber': picking.carrier_tracking_ref,
+                            'ShippingCarrierUsed': picking.carrier_id.name,
+                        },
+                    }
+                self.env['product.template'].ebay_execute("CompleteSale", call_data)
