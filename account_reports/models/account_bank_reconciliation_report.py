619185423189be6b8bdf81e22c576d26da17268f
Olivier Colson <oco@odoo.com>
2019-07-17 12:07:16 +0000

    [FIX] account_reports: bank reconciliation report: avoid crash when no data to display
    In SQL, SUM(...) of empty stuff returns NULL, not 0.
    
    closes odoo/enterprise#4872
    
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- account_reports/models/account_bank_reconciliation_report.py
+++ account_reports/models/account_bank_reconciliation_report.py
@@ -91,8 +91,8 @@ class account_bank_reconciliation_report(models.AbstractModel):
 
         if account_ids:
             self._cr.execute('''
-                SELECT SUM(line.''' + ('amount_currency' if use_foreign_currency else 'balance') + ''') FROM account_move_line line
-                WHERE line.account_id IN %s AND line.date <= %s AND line.company_id IN %s 
+                SELECT COALESCE(SUM(line.''' + ('amount_currency' if use_foreign_currency else 'balance') + '''), 0) FROM account_move_line line
+                WHERE line.account_id IN %s AND line.date <= %s AND line.company_id IN %s
             ''', [
                 tuple(account_ids),
                 self.env.context['date_to'],
