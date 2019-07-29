e17c9ac0e36218900264176c58f61277c027b858
Alex Tuyls <alt@odoo.com>
2019-07-29 13:50:05 +0000

    [FIX] helpdesk: access rights issue for helpdesk user
    When a Helpdesk user tries to post a message on a ticket
    where there is a helpdesk team defined, and if that user
    does not belong to that team, an access right error is
    raised because he does not have access to it. He should
    be able to post a message anyway.
    
    opw:2039256
    
    closes odoo/enterprise#4957
    
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- helpdesk/models/helpdesk_ticket.py
+++ helpdesk/models/helpdesk_ticket.py
@@ -440,7 +440,7 @@ class HelpdeskTicket(models.Model):
     @api.multi
     def _notify_get_reply_to(self, default=None, records=None, company=None, doc_names=None):
         """ Override to set alias of tickets to their team if any. """
-        aliases = self.mapped('team_id')._notify_get_reply_to(default=default, records=None, company=company, doc_names=None)
+        aliases = self.mapped('team_id').sudo()._notify_get_reply_to(default=default, records=None, company=company, doc_names=None)
         res = {ticket.id: aliases.get(ticket.team_id.id) for ticket in self}
         leftover = self.filtered(lambda rec: not rec.team_id)
         if leftover:
