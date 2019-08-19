bits-amitrathod/enterprise

commit 332d35b56e67119ce55f3260501227ad9b84ba14

[FIX] website_enterprise: prevent multiple spinners on main nav-bar
Currently, if user clicks on 'Applications' icon on main nav-bar
of website to navigate to back-end, a spinner is added to indicate
initiation of the action. However, if user quickly clicks this icon
multiple times, more than one spinners are added for each click
(because event is being listened for every click, which also delays
the actual action).

This commit fixes the issue by listening to the event only
once when 'Applications' icon is clicked for the first time.

Task: 1963329
Closes: #4096

Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

website_enterprise/static/src/js/website_enterprise.js
@@ -23,7 +23,7 @@ odoo.define('website.app_switcher', function (require) {
             ev.preventDefault();
 
             // We add a spinner for the user to understand the loading.
-            $(ev.currentTarget).removeClass('fa fa-th').append($('<span/>', {'class': 'fa fa-spin fa-spinner'}));
+            $(ev.currentTarget).removeClass('fa fa-th o_menu_toggle').append($('<span/>', {'class': 'fa fa-spin fa-spinner'}));
             var url = '/web#home';
             window.location.href = session.debug ? $.param.querystring(url, {debug: session.debug}) : url;
         },

commit 15dc0a97cea0a45ce7080ed2610aeb974969e3bf

[FIX] website_enterprise: restore design of go-to-backend button
The design of the button (when the user clicks on it) was broken by
https://github.com/odoo/odoo/commit/332d35b56e67119ce55f3260501227ad9b84ba14

Indeed the CSS class should never have been removed. This commit solves
the issue by restoring the class and solves the original goal of the
mentioned commit by redirecting to backend only if the button is not
already in its loading state.

task-2041973

closes odoo/enterprise#4962

Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>


Co-authored-by: Jérémy Kersten (jke) <jke@openerp.com>
Co-authored-by: qsm-odoo <qsm@odoo.com>

website_enterprise/static/src/js/website_enterprise.js
@@ -23,7 +23,11 @@ odoo.define('website.app_switcher', function (require) {
             ev.preventDefault();
 
             // We add a spinner for the user to understand the loading.
-            $(ev.currentTarget).removeClass('fa fa-th o_menu_toggle').append($('<span/>', {'class': 'fa fa-spin fa-spinner'}));
+            var $button = $(ev.currentTarget);
+            if (!$button.hasClass('fa')) {
+                return;
+            }
+            $button.removeClass('fa fa-th').append($('<span/>', {'class': 'fa fa-spin fa-spinner'}));
             var url = '/web#home';
             window.location.href = session.debug ? $.param.querystring(url, {debug: session.debug}) : url;
         },