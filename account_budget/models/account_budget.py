6402108b29e9d24e03a1068f25f7515e215f04d7
Julien Castiaux <juc@odoo.com>
2019-08-29 09:31:22 +0000

    [FIX] account_budget: pivot on computed field
    Show the pivot view of the account budget report. Traceback.
    
    The pivot view shows a sum of each column using a `{field}:sum`
    read_group field name but as the field is not stored it is not possible
    to compute it hence the traceback. As the read_group overrided in the
    budjet model does not handle aggregate function, they have been removed.
    
    closes odoo/enterprise#5373
    
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- account_budget/models/account_budget.py
+++ account_budget/models/account_budget.py
@@ -114,10 +114,23 @@ class CrossoveredBudgetLines(models.Model):
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         # overrides the default read_group in order to compute the computed fields manually for the group
 
-        result = super(CrossoveredBudgetLines, self).read_group(domain, fields, groupby, offset=offset, limit=limit,
-                                                                orderby=orderby, lazy=lazy)
-        fields_list = ['practical_amount', 'theoritical_amount', 'percentage']
-        if any(x in fields for x in fields_list):
+        fields_list = {'practical_amount', 'theoritical_amount', 'percentage'}
+
+        # Not any of the fields_list support aggregate function like :sum
+        def truncate_aggr(field):
+            field_no_aggr = field.split(':', 1)[0]
+            if field_no_aggr in fields_list:
+                return field_no_aggr
+            return field
+        fields = {truncate_aggr(field) for field in fields}
+
+        # Read non fields_list fields
+        result = super(CrossoveredBudgetLines, self).read_group(
+            domain, list(fields - fields_list), groupby, offset=offset,
+            limit=limit, orderby=orderby, lazy=lazy)
+
+        # Populate result with fields_list values
+        if fields & fields_list:
             for group_line in result:
 
                 # initialise fields to compute to 0 if they are requested
