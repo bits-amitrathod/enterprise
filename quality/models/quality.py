fe0519fd9f9ce12d1ee00453839a40bcc1b8f600
Nans Lefebvre <len@odoo.com>
2019-09-20 08:25:05 +0000

    [FIX] quality: get team from the right company when creating checks
    Use quality in multicompany.
    The default team for checks is searched without check on the company.
    As a result, workorders can be created with a team_id being in another company.
    It follows that MOs with such workorders cannot be planned or validated.
    Instead, we raise to avoid the user getting stuck and needing support's help.
    
    opw 2072587
    
    closes odoo/enterprise#5690
    
    Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

--- quality/models/quality.py
+++ quality/models/quality.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class TestType(models.Model):
@@ -22,7 +23,10 @@ class QualityPoint(models.Model):
     _order = "sequence, id"
 
     def __get_default_team_id(self):
-        return self.env['quality.alert.team'].search([], limit=1).id
+        # rename in 12.5 (__ -> _)
+        company_id = self.company_id.id or self.env.context.get('default_company_id', self.env.user.company_id.id)
+        domain = ['|', ('company_id', '=', company_id), ('company_id', '=', False)]
+        return self.team_id._get_quality_team(domain)
 
     def _get_default_test_type_id(self):
         domain = self._get_type_default_domain()
@@ -107,6 +111,15 @@ class QualityAlertTeam(models.Model):
         alert_result = dict((data['team_id'][0], data['team_id_count']) for data in alert_data)
         for team in self:
             team.alert_count = alert_result.get(team.id, 0)
+
+    @api.model
+    def _get_quality_team(self, domain):
+        team_id = self.env['quality.alert.team'].search(domain, limit=1).id
+        if team_id:
+            return team_id
+        else:
+            raise UserError(_("No quality team found for this company.\n"
+                              "Please go to configuration and create one first."))
 
 
 class QualityReason(models.Model):
@@ -206,6 +219,11 @@ class QualityAlert(models.Model):
     _description = "Quality Alert"
     _inherit = ['mail.thread']
 
+    def _get_default_team_id(self):
+        company_id = self.company_id.id or self.env.context.get('default_company_id', self.env.user.company_id.id)
+        domain = ['|', ('company_id', '=', company_id), ('company_id', '=', False)]
+        return self.team_id._get_quality_team(domain)
+
     name = fields.Char('Name', default=lambda self: _('New'))
     description = fields.Html('Description')
     stage_id = fields.Many2one('quality.alert.stage', 'Stage', ondelete='restrict',
@@ -222,7 +240,7 @@ class QualityAlert(models.Model):
     user_id = fields.Many2one('res.users', 'Responsible', track_visibility='onchange', default=lambda self: self.env.user)
     team_id = fields.Many2one(
         'quality.alert.team', 'Team', required=True,
-        default=lambda x: x.env['quality.alert.team'].search([], limit=1))
+        default=lambda x: x._get_default_team_id())
     partner_id = fields.Many2one('res.partner', 'Vendor')
     check_id = fields.Many2one('quality.check', 'Check')
     product_tmpl_id = fields.Many2one('product.template', 'Product')
